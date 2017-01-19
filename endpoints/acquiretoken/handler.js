0612b7011f048b06b710f71d9ea3eb14c3948e42
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-01-19 14:48:12 -0800

    Function acquiretoken endpoint
    Former-commit-id: 7002d25db2fb7dcd66487c0bea58b3652a818941

--- endpoints/acquiretoken/handler.js
+++ endpoints/acquiretoken/handler.js
@@ -2,17 +2,18 @@
 const jwt = require('jsonwebtoken');
 var AWS = require("aws-sdk");
 const _ = require("underscore");
+
 var lr = require('../../lib/lambda-response.js');
 var timestamp = require('../../lib/timestamp.js');
 
 module.exports.acquiretoken = (event, context, callback) => {
 	
-	var timestamp = timestamp.createTimestampSeconds + (60 * 60);
+	var _timestamp = timestamp.createTimestampSeconds() + (60 * 60);
 
 	var payload = {
 		body: JSON.stringify({}),
-		iat: timestamp,
-		exp: timestamp
+		iat: _timestamp,
+		exp: _timestamp
 	}
 	
 	var created_token = jwt.sign(payload, process.env.site_secret_jwt_key);
