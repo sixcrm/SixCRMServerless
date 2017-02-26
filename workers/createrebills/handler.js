ceb0b98244672d47077756ec8bfa7fdc8cdb0400
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-02-25 18:08:31 -0800

    Functional CreateRebills Worker
    Former-commit-id: efc46b973c205b2d44456a32557df17a8981518d

---
+++ workers/createrebills/handler.js
@@ -0,0 +1,33 @@
+'use strict';
+var LambdaResponse = require('../../lib/lambda-response.js');
+
+var createRebillsController = require('../../controllers/workers/createRebills.js');
+
+module.exports.createrebills = (event, context, callback) => {
+	
+	createRebillsController.execute(event).then((result) => {
+		
+		if(result !== createRebillsController.messages.success && result !== createRebillsController.messages.successnoaction){
+			
+			new LambdaResponse().issueResponse(200, {
+				message: result
+			}, callback);
+			
+		}else{
+			
+			createRebillsController.createForwardMessage(event).then((forward_object) => {
+				new LambdaResponse().issueResponse(200, {
+					message: result,
+					forward: forward_object
+				}, callback);
+			});
+			
+		}
+		
+	}).catch((error) =>{
+	
+		return new LambdaResponse().issueError(error.message, 500, event, error, callback);
+		
+	});
+
+}
