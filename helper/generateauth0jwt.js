ddf45414b54afdf6632f27d4fe922bf1ba705c45
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-03-08 17:34:16 -0800

    Adding auth0 JWT generator to the helpers
    Former-commit-id: b64a631efab86b0d61c0b87c57f4145ef7518bd5

---
+++ helper/generateauth0jwt.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+const tu = require('../lib/test-utilities.js');
+const timestamp = require('../lib/timestamp.js');
+
+let site_config = yaml.safeLoad(fs.readFileSync('../config/development/site.yml', 'utf8'));
+let now = timestamp.createTimestampSeconds();
+
+let secret_key = site_config.jwt.auth0.secret_key;
+let email = process.argv[2];
+if(typeof email == 'undefined'){
+	email = 'super.user@test.com';
+}
+
+let jwt_contents = {
+	"email": "test@test.com",
+	"email_verified": true,
+	"picture": "",
+	"iss": "https://sixcrm.auth0.com/",
+	"sub": "",
+	"aud": "",
+	"exp": (now+3600),
+	"iat": now
+};
+
+console.log(tu.generateJWT(jwt_contents, secret_key));
