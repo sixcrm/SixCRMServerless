b28e392a9c8a5b1721ae3f7332af67a8934c1171
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-03-28 12:38:46 -0700

    Adding Slack Alarm (was untracked)
    Former-commit-id: d383decfd5157c0816345e933fc1f27b6d02b85e

--- deployment/dynamodb/deploy_tables.sh
+++ deployment/dynamodb/deploy_tables.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+set -e
+set -u
+
+STAGE='development'
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+  	--stage=*)
+		STAGE="${1#*=}"
+		;;
+  esac
+  shift
+done
+
 serverless dynamodb install
-serverless dynamodb executeAll --stage $CIRCLE_BRANCH
+serverless dynamodb executeAll --stage $STAGE
