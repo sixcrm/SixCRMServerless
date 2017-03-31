4918a09cb208864f9d411d4145890b9d3bea8403
Ljubomir Simin <ljubomir@coingcs.com>
2017-03-31 16:04:05 +0200

    Simplify promises.
    Former-commit-id: 298c358adde93dfbf29ab53d1b93cf8ebfbeb3cc

--- test/functional/sqs-test-utils.js
+++ test/functional/sqs-test-utils.js
@@ -16,19 +16,15 @@ class SqsTestUtils {
     }
 
     purgeAllQueues() {
-        return new Promise((resolve) => {
-            Promise.all([
-                this.purgeQueue('bill'),
-                this.purgeQueue('rebill'),
-                this.purgeQueue('recover'),
-                this.purgeQueue('hold'),
-                this.purgeQueue('shipped'),
-                this.purgeQueue('delivered'),
-                this.purgeQueue('searchindex')
-            ]).then(() => {
-                resolve();
-            });
-        });
+        return Promise.all([
+            this.purgeQueue('bill'),
+            this.purgeQueue('rebill'),
+            this.purgeQueue('recover'),
+            this.purgeQueue('hold'),
+            this.purgeQueue('shipped'),
+            this.purgeQueue('delivered'),
+            this.purgeQueue('searchindex')
+        ]);
     }
 
     messageCountInQueue(queue) {
@@ -38,28 +34,24 @@ class SqsTestUtils {
             '&AttributeName.3=ApproximateNumberOfMessagesDelayed';
 
         // Return the sum of all 3 attributes.
-        return new Promise((resolve, reject) => {
-            this.executeQuery(queue, query).then(res => {
-                let text = res.text;
-                let regexp = /<Value>(\d*)<\/Value>/g;
-                let result = 0;
+        return this.executeQuery(queue, query).then(res => {
+            let text = res.text;
+            let regexp = /<Value>(\d*)<\/Value>/g;
+            let result = 0;
 
-                let match = regexp.exec(text);
-                while (match != null) {
-                    result += Number(match[1]);
-                    match = regexp.exec(text);
-                }
+            let match = regexp.exec(text);
+            while (match != null) {
+                result += Number(match[1]);
+                match = regexp.exec(text);
+            }
 
-                resolve(result);
-            });
+            return result;
         });
     }
 
     receiveMessageFromQueue(queue) {
-        return new Promise((resolve, reject) => {
-            this.executeQuery(queue, 'Action=ReceiveMessage&AttributeName=All').then(res => {
-                resolve(res.text.match(/<Body>(.*)<\/Body>/)[1]);
-            });
+        return this.executeQuery(queue, 'Action=ReceiveMessage&AttributeName=All').then(res => {
+            return res.text.match(/<Body>(.*)<\/Body>/)[1];
         });
     }
 
