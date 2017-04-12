e381a7c6200e0e01ed747b89c78e79d784e45e25
paul_sheraton <paul.sheraton@mheducation.com>
2017-04-12 09:05:48 -0700

    limiting create_index to be syncronous or have a concurrency limit with bluebird, also bluebird was already installed as a sub-dependency of another node module
    Former-commit-id: 028dfea94a96be2d1a3794e2ed9d9e8d81361324

--- deployment/cloudsearch/deploy.js
+++ deployment/cloudsearch/deploy.js
@@ -1,13 +1,14 @@
 "use strict"
 const AWS = require("aws-sdk");
 const _ = require('underscore');
+const Promise = require ('bluebird');
 const fs = require('fs');
 const yaml = require('js-yaml');
 
 const du = require('../../lib/debug-utilities.js');
 
 let cs = new AWS.CloudSearch({
-	region: 'us-east-1', 
+	region: 'us-east-1',
 	apiVersion: '2013-01-01'
 });
 
@@ -22,88 +23,97 @@ createDomain()
 	.then(() => {
 		du.highlight('Complete');
 	}).catch((error) => {
-		throw new Error(error);
+		throw new Error(error); //This is what shows the unhandled error
 	});
 
 
 function createDomain(){
-	
+
 	du.debug('Create Domain');
-	
+
 	return new Promise((resolve, reject) => {
-		
+
 		let params = {
 			DomainName: domain_name
 		};
-		
+
 		cs.createDomain(params, (error, data) => {
-			
+
 			if(error){ return reject(error); }
-			
+
 			return resolve(data);
-			
+
 		});
-		
+
 	});
 
 }
 
 function createIndexes(){
-	
+
 	du.debug('Create Indexes');
-	
+
 	return new Promise((resolve, reject) => {
-	
+
 		getIndexObjects().then(index_objects => {
-				
+
 			let promises = [];
-			
-			for(var i = 0; i < index_objects.length; i++){
-				
-				index_objects[i]['DomainName'] = domain_name;
-				
-				let create_index_promise = createIndex(index_objects[i]).catch((error) => {  return reject(error); });
-				
-				promises.push(create_index_promise);
-				
-			}
-			
-			Promise.all(promises).then((promises) => {
-				
+
+      //Limt the concurrency rate of promises to 1 to naively avoid rate limits
+      Promise.map(index_objects, (indexObject) => {
+        indexObject.DomainName = domain_name;
+        return createIndex(indexObject);
+      }, {concurrency: 1})
+      .then((promises) => {
+        return resolve(promises);
 				du.debug('Resolved Promises: ', promises);
-				
-				return resolve(promises);
-			
-			}).catch((error) => {
-			
-				return reject(error);
-			
-			});
-		
-		});		
-		
+      })
+
+			//for(var i = 0; i < index_objects.length; i++){
+
+				//index_objects[i]['DomainName'] = domain_name;
+
+				//let create_index_promise = createIndex(index_objects[i]);
+
+				//promises.push(create_index_promise);
+
+			//}
+
+			//Promise.all(promises).then((promises) => {
+
+
+				//return resolve(promises);
+
+			//}).catch((error) => {
+
+				//return reject(error);
+
+			//});
+
+		});
+
 	});
-	
+
 }
 
 function getIndexObjects(){
-	
+
 	return new Promise((resolve, reject) => {
-		
+
 		let response_objects = [];
-		
+
 		let index_directory = __dirname+'/indexes/';
-		
+
 		fs.readdir(index_directory, (error, files) => {
-			
+
 			if(error){ return reject(error); }
-			
+
 			files.forEach(file => {
-					
+
 				du.debug('Index File: '+file);
-				
+
 				let file_contents = fs.readFileSync(index_directory+file, 'utf8');
-				
+
 				let obj = JSON.parse(file_contents);
 
 				response_objects.push(obj);
@@ -113,57 +123,57 @@ function getIndexObjects(){
 			return resolve(response_objects);
 
 		});
-	
+
 	});
-	
+
 }
 
 function createIndex(index_object){
-	
+
 	return new Promise((resolve, reject) => {
-		
-		if(!_.has(index_object, 'IndexField')){ 
-			
-			du.warning(index_object);	
+
+		if(!_.has(index_object, 'IndexField')){
+
+			du.warning(index_object);
 			return reject('Index Object lacks IndexField');
-			
+
 		}
-		
+
 		if(!_.has(index_object.IndexField, 'IndexFieldName')){
-			
-			du.warning(index_object);	
+
+			du.warning(index_object);
 			return reject('Index Object lacks IndexField.IndexFieldName');
-			
+
 		}
-	
+
 		cs.defineIndexField(index_object, (error, data) => {
-	
+
 			if(error){ return reject(error); }
-		
+
 			return resolve(data);
 
 		});
-		
+
 	});
-	
+
 }
 
 function indexDocuments(){
 
 	return new Promise((resolve, reject) => {
-	
+
 		var params = {
 			DomainName: domain_name
 		};
-		
+
 		cs.indexDocuments(params, (error, data) => {
-			
+
 			if(error){ return reject(error); }
-			
+
 			return resolve(data);
-			
+
 		});
-		
+
 	});
-	
+
 }
