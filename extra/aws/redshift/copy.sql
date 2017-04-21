4dc3435e999b431eb378ae4d88b8e87991eba202
Aldo Zelen <azelen@gmail.com>
2017-04-21 21:50:15 +0200

    Added a Redshift dir for scripts
    Former-commit-id: 1a196d3ed23a47c1b8cba86e06f23b7d33e78ff0

--- extra/aws/redshift/copy.sql
+++ extra/aws/redshift/copy.sql
@@ -5,6 +5,6 @@
 */
 
 COPY f_transactions
-FROM 's3://sixcrm-redshift-staging/test.csv' credentials 'aws_access_key_id=AKIAIP6FAI6MVLVAPRWQ;aws_secret_access_key=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'
+FROM 's3://sixcrm-redshift-staging/test.csv' credentials 'aws_access_key_id= ;aws_secret_access_key= '
 IGNOREHEADER 1
 DELIMITER ',';
