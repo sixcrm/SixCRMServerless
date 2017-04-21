4b54bdee9dc6fab8fef845b1495f5d9fdb9df7b6
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-04-21 13:22:53 -0700

    Functional Analytics Endpoint
    Former-commit-id: 8be710af158b588d13d5ec3ec705229306bb53f1

--- controllers/analytics/Analytics.js
+++ controllers/analytics/Analytics.js
@@ -1,6 +1,8 @@
 'use strict';
 const _ = require('underscore');
 const fs = require('fs');
+var validator = require('validator');
+var Validator = require('jsonschema').Validator;
 
 var timestamp = require('../../lib/timestamp.js');
 
@@ -11,64 +13,123 @@ class AnalyticsController {
 
     getTransactionSummary(parameters){
 
-      du.debug('Get Transaction Summary');
+      return new Promise((resolve, reject) => {
+        let query_name = 'transaction_summary';
 
-      return this.getQueryString('transaction_summary').then((query) => {
+        du.debug('Get Transaction Summary');
 
-        du.debug('Raw parameters:', parameters);
+        this.getQueryString(query_name).then((query) => {
 
-        parameters = ['df6a75c8-67d1-5ec2-8d54-94ae46d817b3', 'date\'3.1.2017\'', 'date\'3.31.2017\''];
+          parameters = this.appendAccount(parameters);
 
-        du.highlight('Query:', query, 'Parameters:', parameters);
+          this.validateQueryParameters(query_name, parameters).then((validated) => {
 
-        return redshiftutilities.query(query, parameters).then((results) => {
+            parameters = ['df6a75c8-67d1-5ec2-8d54-94ae46d817b3', 'date\'3.1.2017\'', 'date\'3.31.2017\''];
 
-          return Promise.resolve({
-            transactions:[
-              {
-                datetime: "2017-04-20T20:57:32.802Z",
-                byprocessorresult: [
+            du.highlight('Query:', query, 'Parameters:', parameters);
+
+            redshiftutilities.query(query, parameters).then((results) => {
+
+              du.info(results);
+              
+              return resolve({
+                transactions:[
                   {
-                    processor_result: "success",
-                    count: 14,
-                    amount: 450.99
+                    datetime: "2017-04-20T20:57:32.802Z",
+                    byprocessorresult: [
+                      {
+                        processor_result: "success",
+                        count: 14,
+                        amount: 450.99
+                      },
+                      {
+                        processor_result: "decline",
+                        count: 2,
+                        amount: 32.98
+                      },
+                      {
+                        processor_result: "error",
+                        count: 2,
+                        amount: 32.98
+                      }
+                    ]
                   },
                   {
-                    processor_result: "decline",
-                    count: 2,
-                    amount: 32.98
-                  },
-                  {
-                    processor_result: "error",
-                    count: 2,
-                    amount: 32.98
+                    datetime: "2017-04-21T17:41:41.117Z",
+                    byprocessorresult: [
+                      {
+                        processor_result: "success",
+                        count: 14,
+                        amount: 450.99
+                      },
+                      {
+                        processor_result: "decline",
+                        count: 2,
+                        amount: 32.98
+                      },
+                      {
+                        processor_result: "error",
+                        count: 2,
+                        amount: 32.98
+                      }
+                    ]
                   }
                 ]
-              },
-              {
-                datetime: "2017-04-21T17:41:41.117Z",
-                byprocessorresult: [
-                  {
-                    processor_result: "success",
-                    count: 14,
-                    amount: 450.99
-                  },
-                  {
-                    processor_result: "decline",
-                    count: 2,
-                    amount: 32.98
-                  },
-                  {
-                    processor_result: "error",
-                    count: 2,
-                    amount: 32.98
-                  }
-                ]
-              }
-            ]
+              });
+
+            });
+
+          }).catch((error) => {
+
+            return reject(error);
+
           });
 
         });
+
+      });
+
+    }
+
+    validateQueryParameters(query_name, object){
+
+      du.debug('Validating:', query_name+' query parameters', object);
+
+      return new Promise((resolve, reject) => {
+
+          return this.getQueryParameterValidationString(query_name).then((query_validation_string) => {
+
+            var v = new Validator();
+
+            var validation;
+
+            try{
+
+                v = new Validator();
+                validation = v.validate(object, query_validation_string);
+
+            }catch(e){
+
+                return reject(new Error('Unable to instantiate validator.'));
+
+            }
+
+            du.info(validation);
+
+            if(_.has(validation, "errors") && _.isArray(validation.errors) && validation.errors.length > 0){
+
+                var error = {
+                    message: 'One or more validation errors occurred.',
+                    issues: validation.errors.map((e) => { return e.message; })
+                };
+
+                return reject(error);
+
+            }
+
+            return resolve(true);
+
+          });
 
       });
 
@@ -94,6 +155,23 @@ class AnalyticsController {
 
       });
 
+    }
+
+    getQueryParameterValidationString(query_name){
+
+      du.debug('Get Query Parameter Validation String');
+
+      return new Promise((resolve, reject) => {
+
+        let query_validation_filepath = this.getQueryParameterValidationFilepath(query_name);
+
+        du.debug('Filepath: ', query_validation_filepath);
+
+        let schema = require(query_validation_filepath);
+
+        return resolve(schema);
+
+      });
 
     }
 
@@ -101,7 +179,23 @@ class AnalyticsController {
 
       du.debug('Get Query Filepath');
 
-      return __dirname+'/queries/'+query_name+'.sql';
+      return __dirname+'/queries/'+query_name+'/query.sql';
+
+    }
+
+    getQueryParameterValidationFilepath(query_name){
+
+      du.debug('Get Query Parameter Validation Filepath');
+
+      return __dirname+'/queries/'+query_name+'/parameter_validation.json';
+
+    }
+
+    appendAccount(parameters){
+
+        parameters['account'] = global.account;
+
+        return parameters;
 
     }
 
