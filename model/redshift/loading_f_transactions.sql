c45e892a8ed9394fc1e1488f2fad47827e32031a
Aldo Zelen <azelen@gmail.com>
2017-04-21 21:41:05 +0200

    Processor queries
    Former-commit-id: a23e0224f95b0106c5c219ed6d22add2b33381c6

---
+++ model/redshift/loading_f_transactions.sql
@@ -0,0 +1,5 @@
+COPY f_transactions
+FROM 's3://sixcrm-redshift-staging/test.csv' credentials 'aws_access_key_id=AKIAIP6FAI6MVLVAPRWQ;aws_secret_access_key=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' 
+IGNOREHEADER 1 
+DELIMITER ',';
+
