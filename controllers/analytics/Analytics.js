719a64e90bf62d37d0b180fec2e5e53d2140ae75
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-04-22 12:59:16 -0700

    Updating Analytics controller
    Former-commit-id: f814c23bbb868eddffead816e493c54b7704ec8c

--- controllers/analytics/Analytics.js
+++ controllers/analytics/Analytics.js
@@ -9,185 +9,194 @@ var timestamp = require('../../lib/timestamp.js');
 const du = require('../../lib/debug-utilities.js');
 const redshiftutilities = require('../../lib/redshift-utilities.js');
 
+//Technical Debt:  We need a caching strategy.
+
 class AnalyticsController {
 
     getTransactionSummary(parameters){
 
-      return new Promise((resolve, reject) => {
-        let query_name = 'transaction_summary';
+        return new Promise((resolve, reject) => {
 
-        du.debug('Get Transaction Summary');
+        //Technical Debt: pick meaningful query based on the period (difference between the start and end date) queried.
+            let query_name = 'transaction_summary';
 
-        this.getQueryString(query_name).then((query) => {
+            du.debug('Get Transaction Summary');
 
-          parameters = this.appendAccount(parameters);
+            this.getQueryString(query_name).then((query) => {
 
-          this.validateQueryParameters(query_name, parameters).then((validated) => {
+                parameters = this.appendAccount(parameters);
 
-            parameters = ['df6a75c8-67d1-5ec2-8d54-94ae46d817b3', 'date\'3.1.2017\'', 'date\'3.31.2017\''];
+                this.validateQueryParameters(query_name, parameters).then((validated) => {
 
-            du.highlight('Query:', query, 'Parameters:', parameters);
+            //Technical Debt:
+            //add additional validation: start and end adhere to some date format
+            //assume account is a UUIDv4 and the user has access to it (graph controller assures us..., I think)
 
-            redshiftutilities.query(query, parameters).then((results) => {
+                    let ordered_parameters = [parameters.account, parameters.start, parameters.end];
 
-              du.info(results);
-              
-              return resolve({
-                transactions:[
-                  {
-                    datetime: "2017-04-20T20:57:32.802Z",
-                    byprocessorresult: [
-                      {
-                        processor_result: "success",
-                        count: 14,
-                        amount: 450.99
-                      },
-                      {
-                        processor_result: "decline",
-                        count: 2,
-                        amount: 32.98
-                      },
-                      {
-                        processor_result: "error",
-                        count: 2,
-                        amount: 32.98
-                      }
-                    ]
-                  },
-                  {
-                    datetime: "2017-04-21T17:41:41.117Z",
-                    byprocessorresult: [
-                      {
-                        processor_result: "success",
-                        count: 14,
-                        amount: 450.99
-                      },
-                      {
-                        processor_result: "decline",
-                        count: 2,
-                        amount: 32.98
-                      },
-                      {
-                        processor_result: "error",
-                        count: 2,
-                        amount: 32.98
-                      }
-                    ]
-                  }
-                ]
-              });
+                    du.highlight('Query:', query, 'Parameters:', ordered_parameters);
+
+                    redshiftutilities.query(query, ordered_parameters).then((results) => {
+
+                        du.debug('Query Results:', results);
+
+              // Technical Debt: Once the query is of acceptable structure, transform structure into the following JSON structure...
+                        return resolve({
+                            transactions:[
+                                {
+                                    datetime: "2017-04-20T20:57:32.802Z",
+                                    byprocessorresult: [
+                                        {
+                                            processor_result: "success",
+                                            count: 14,
+                                            amount: 450.99
+                                        },
+                                        {
+                                            processor_result: "decline",
+                                            count: 2,
+                                            amount: 32.98
+                                        },
+                                        {
+                                            processor_result: "error",
+                                            count: 2,
+                                            amount: 32.98
+                                        }
+                                    ]
+                                },
+                                {
+                                    datetime: "2017-04-21T17:41:41.117Z",
+                                    byprocessorresult: [
+                                        {
+                                            processor_result: "success",
+                                            count: 14,
+                                            amount: 450.99
+                                        },
+                                        {
+                                            processor_result: "decline",
+                                            count: 2,
+                                            amount: 32.98
+                                        },
+                                        {
+                                            processor_result: "error",
+                                            count: 2,
+                                            amount: 32.98
+                                        }
+                                    ]
+                                }
+                            ]
+                        });
+
+                    });
+
+                }).catch((error) => {
+
+                    return reject(error);
+
+                });
 
             });
 
-          }).catch((error) => {
-
-            return reject(error);
-
-          });
-
         });
-
-      });
 
     }
 
     validateQueryParameters(query_name, object){
 
-      du.debug('Validating:', query_name+' query parameters', object);
+        du.debug('Validating:', query_name+' query parameters', object);
 
-      return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
 
-          return this.getQueryParameterValidationString(query_name).then((query_validation_string) => {
+            return this.getQueryParameterValidationString(query_name).then((query_validation_string) => {
 
-            var v = new Validator();
+                var v = new Validator();
 
-            var validation;
+                var validation;
 
-            try{
+                try{
 
-                v = new Validator();
-                validation = v.validate(object, query_validation_string);
+                    v = new Validator();
+                    validation = v.validate(object, query_validation_string);
 
-            }catch(e){
+                }catch(e){
 
-                return reject(new Error('Unable to instantiate validator.'));
+                    return reject(new Error('Unable to instantiate validator.'));
 
-            }
+                }
 
-            du.info(validation);
+                du.info(validation);
 
-            if(_.has(validation, "errors") && _.isArray(validation.errors) && validation.errors.length > 0){
+                if(_.has(validation, "errors") && _.isArray(validation.errors) && validation.errors.length > 0){
 
-                var error = {
-                    message: 'One or more validation errors occurred.',
-                    issues: validation.errors.map((e) => { return e.message; })
-                };
+                    var error = {
+                        message: 'One or more validation errors occurred.',
+                        issues: validation.errors.map((e) => { return e.message; })
+                    };
 
-                return reject(error);
+                    return reject(error);
 
-            }
+                }
 
-            return resolve(true);
+                return resolve(true);
 
-          });
+            });
 
-      });
+        });
 
     }
 
     getQueryString(query_name){
 
-      du.debug('Get Query String');
+        du.debug('Get Query String');
 
-      return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
 
-        let query_filepath = this.getQueryFilepath(query_name);
+            let query_filepath = this.getQueryFilepath(query_name);
 
-        du.debug('Filepath: ', query_filepath);
+            du.debug('Filepath: ', query_filepath);
 
-        fs.readFile(query_filepath, 'utf8', (error, data) => {
+            fs.readFile(query_filepath, 'utf8', (error, data) => {
 
-          if(error) { return reject(error); }
+                if(error) { return reject(error); }
 
-          return resolve(data);
+                return resolve(data);
+
+            });
 
         });
-
-      });
 
     }
 
     getQueryParameterValidationString(query_name){
 
-      du.debug('Get Query Parameter Validation String');
+        du.debug('Get Query Parameter Validation String');
 
-      return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
 
-        let query_validation_filepath = this.getQueryParameterValidationFilepath(query_name);
+            let query_validation_filepath = this.getQueryParameterValidationFilepath(query_name);
 
-        du.debug('Filepath: ', query_validation_filepath);
+            du.debug('Filepath: ', query_validation_filepath);
 
-        let schema = require(query_validation_filepath);
+            let schema = require(query_validation_filepath);
 
-        return resolve(schema);
+            return resolve(schema);
 
-      });
+        });
 
     }
 
     getQueryFilepath(query_name){
 
-      du.debug('Get Query Filepath');
+        du.debug('Get Query Filepath');
 
-      return __dirname+'/queries/'+query_name+'/query.sql';
+        return __dirname+'/queries/'+query_name+'/query.sql';
 
     }
 
     getQueryParameterValidationFilepath(query_name){
 
-      du.debug('Get Query Parameter Validation Filepath');
+        du.debug('Get Query Parameter Validation Filepath');
 
-      return __dirname+'/queries/'+query_name+'/parameter_validation.json';
+        return __dirname+'/queries/'+query_name+'/parameter_validation.json';
 
     }
 
--- controllers/analytics/queries/transaction_summary/query.sql
+++ controllers/analytics/queries/transaction_summary/query.sql
@@ -3,5 +3,5 @@ result, sum(amount) as sum_amount , count(*) as transaction_count
 from
 f_transactions
 where account = $1::text
-and stamp between $2::text and $3::text
+and stamp between date'3.1.2017' and date'3.31.2017'
 group by result
