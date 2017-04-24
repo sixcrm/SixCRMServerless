b6b48791b8acc526350b0dc9d316f0a3dbacfb86
Aldo Zelen <azelen@gmail.com>
2017-04-24 21:38:50 +0200

    Created depreciated folder, deleted old query structure
    Former-commit-id: 0a042a883bac00a5c35bfbf0080457cb4916c6ed

--- controllers/analytics/queries/transaction_summary/aggregation_processor_amount/query.sql
+++ controllers/analytics/queries/transaction_summary/aggregation_processor_amount/query.sql
@@ -1,16 +1,16 @@
 SELECT /* Aggregation by result and data spoofing */ rt.result,
        COALESCE(SUM(amount),0) AS sum_amount,
        COALESCE(COUNT(*),0) AS transaction_count,
-       DATE_TRUNC('day',rt.rt_stamp) AS hour
+       DATE_TRUNC('{{period}}',rt.rt_stamp) AS hour
 FROM (SELECT *
       FROM f_transactions
       WHERE account = '{{account}}') ft
   RIGHT JOIN (SELECT RESULT,
-                     DATE_TRUNC('day',stamp) rt_stamp
+                     DATE_TRUNC('{{period}}',stamp) rt_stamp
               FROM d_dates,
                    d_results
               GROUP BY RESULT,
-                       DATE_TRUNC('day',stamp) sw) rt
+                       DATE_TRUNC('{{period}}',stamp) sw) rt
           ON (ft.result = rt.result
          AND DATE_TRUNC ('day',ft.stamp) = rt_stamp)
 WHERE rt.rt_stamp BETWEEN DATE '{{start}}' AND DATE '{{end}}'
--- model/redshift/loading_f_transactions.sql
+++ model/redshift/loading_f_transactions.sql
@@ -1,10 +1,10 @@
 COPY f_transactions
-FROM 's3://sixcrm-redshift-staging/test.csv' credentials 'aws_access_key_id=AKIAIP6FAI6MVLVAPRWQ;aws_secret_access_key=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' 
+FROM 's3://sixcrm-redshift-staging/test.csv' credentials 'aws_access_key_id=;aws_secret_access_key=' 
 IGNOREHEADER 1 
 DELIMITER ',';
 
 
 COPY f_transactions
-FROM 's3://sixcrm-redshift-staging/test_24042017.csv' credentials 'aws_access_key_id=AKIAIP6FAI6MVLVAPRWQ;aws_secret_access_key=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' 
+FROM 's3://sixcrm-redshift-staging/test_24042017.csv' credentials 'aws_access_key_id=;aws_secret_access_key=' 
 IGNOREHEADER 1 
 DELIMITER ',';
