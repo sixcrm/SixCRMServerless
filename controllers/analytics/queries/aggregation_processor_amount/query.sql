681627d2d67db09a2f0f510e19d6f3332eff3685
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-04-25 11:36:51 -0700

    Formatted Query
    Former-commit-id: 4dec7c751483346f29bdec6c2cad01a5db068cc9

--- controllers/analytics/queries/aggregation_processor_amount/query.sql
+++ controllers/analytics/queries/aggregation_processor_amount/query.sql
@@ -1,21 +1,37 @@
-SELECT rt.result,
-       COALESCE(SUM(amount),0) AS sum_amount,
-       COALESCE(COUNT(*),0) AS transaction_count,
-       DATE_TRUNC('{{period}}',rt.rt_stamp) AS {{period}}
-FROM (SELECT *
-      FROM f_transactions
-      WHERE 1
+SELECT
+  rt.result,
+  COALESCE(SUM(amount),0) AS sum_amount,
+  COALESCE(COUNT(*),0) AS transaction_count,
+  DATE_TRUNC('{{period}}',rt.rt_stamp) AS {{period}}
+FROM
+  (
+    SELECT
+      *
+    FROM
+      f_transactions
+    WHERE 1
       {{filter}}
-      AND account = '{{account}}') ft
-  RIGHT JOIN (SELECT RESULT,
-                     DATE_TRUNC('{{period}}',stamp) rt_stamp
-              FROM d_dates,
-                   d_results
-              GROUP BY RESULT,
-                       DATE_TRUNC('{{period}}',stamp)) rt
-          ON (ft.result = rt.result
-         AND DATE_TRUNC ('{{period}}',ft.stamp) = rt_stamp)
-WHERE rt.rt_stamp BETWEEN DATE '{{start}}' AND DATE '{{end}}'
-GROUP BY rt.result,
-         rt_stamp
-ORDER BY {{period}}
+    AND
+      account = '{{account}}'
+  ) ft
+  RIGHT JOIN
+  (
+    SELECT
+      RESULT,
+      DATE_TRUNC('{{period}}',stamp) rt_stamp
+    FROM
+      d_dates,
+      d_results
+    GROUP BY
+      RESULT,
+      DATE_TRUNC('{{period}}',stamp)
+  ) rt
+  ON
+    (ft.result = rt.result AND DATE_TRUNC ('{{period}}',ft.stamp) = rt_stamp)
+WHERE
+  rt.rt_stamp BETWEEN DATE '{{start}}' AND DATE '{{end}}'
+GROUP BY
+  rt.result,
+  rt_stamp
+ORDER BY
+  {{period}}
