dd9f5b9155e6df80306fee3ecaa44dd5da1ea860
azelen <azelen@gmail.com>
2017-04-28 19:54:53 +0200

    Merchant processor
    Former-commit-id: 5ff0a5c3dccc4a629db9691299af445d41275fca

---
+++ controllers/analytics/queries/merchant_processor_amount/query.sql
@@ -0,0 +1,37 @@
+SELECT
+  rt.processor_result,
+  COALESCE(SUM(amount),0) AS sum_amount,
+  COALESCE(COUNT(ft.id),0) AS transaction_count,
+  DATE_TRUNC('{{period}}',rt.rt_datetime) AS {{period}}
+FROM
+  (
+    SELECT
+      *
+    FROM
+      f_transactions
+    WHERE 1
+      {{filter}}
+    AND
+      account = '{{account}}'
+  ) ft
+  RIGHT JOIN
+  (
+    SELECT
+      processor_result,
+      DATE_TRUNC('{{period}}',datetime) rt_datetime
+    FROM
+      d_datetime,
+      d_processor_result
+    GROUP BY
+      processor_result,
+      DATE_TRUNC('{{period}}',datetime)
+  ) rt
+  ON
+    (ft.processor_result = rt.processor_result AND DATE_TRUNC ('{{period}}',ft.datetime) = rt_datetime)
+WHERE
+  rt.rt_datetime BETWEEN DATE '{{start}}' AND DATE '{{end}}'
+GROUP BY
+  rt.processor_result,
+  rt_datetime
+ORDER BY
+  {{period}},processor_result
