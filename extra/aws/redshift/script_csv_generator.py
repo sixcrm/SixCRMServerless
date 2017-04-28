0f2df731ea2f107e9c064a582aafecb0100281ae
azelen <azelen@gmail.com>
2017-04-28 22:52:40 +0200

    Changed the generator function
    Former-commit-id: 98dc0f1e5fa19264e1ecdb730d85f753842366d9

--- extra/aws/redshift/script_csv_generator.py
+++ extra/aws/redshift/script_csv_generator.py
@@ -31,13 +31,30 @@ account = [uuid.uuid4() for x in range(10)]
 schedule = [uuid.uuid4() for x in range(100)]
 
 
-#g=open("test.csv","w",newline="\n", encoding="utf-8")
-g=open("test.csv","w")
-w=csv.writer(g)
-w.writerow(('id','stamp','customer','creditcard','merchprocessor','campaign','affiliate','amount','result','account','type','schedule'))
+g=open("test.csv","w",newline="\n", encoding="utf-8")
 
-for i in range(3000000):
+w=csv.writer(g)
+w.writerow(('id',
+ 'stamp',
+ 'customer',
+ 'creditcard',
+ 'merchprocessor',
+ 'campaign',
+ 'affiliate',
+ 'amount',
+ 'result',
+ 'account',
+ 'type',
+ 'schedule',
+ 'subaffiliate_1'
+,'subaffiliate_2'
+,'subaffiliate_3'
+,'subaffiliate_4'
+,'subaffiliate_5'
+))
+
+for i in range(1000000):
     w.writerow((uuid.uuid4(),random_date(d1,d2),random.choice(customers),random.choice(credit_card),
       random.choice(merch_processor),random.choice(campaign),random.choice(affiliate),random.uniform(1, 10000),
-      random.choice(["success", "decline", "error"]),random.choice(account),random.choice(["rebill", "new"]),random.choice(schedule) ))
+      random.choice(["success", "decline", "error"]),random.choice(account),random.choice(["rebill", "new"]),random.choice(schedule), '','','','',''))
 g.close()
