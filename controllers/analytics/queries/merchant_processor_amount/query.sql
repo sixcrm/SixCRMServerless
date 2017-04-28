8a1429a909b09e6bfa59a4c1c858e9bbaade1506
azelen <azelen@gmail.com>
2017-04-28 20:13:54 +0200

    Merchant processor query
    Former-commit-id: 3ee305405d6a48aee1ce1c5051a083548ce096a2

--- controllers/analytics/queries/merchant_processor_amount/query.sql
+++ controllers/analytics/queries/merchant_processor_amount/query.sql
@@ -1,37 +1,19 @@
 SELECT
-  rt.processor_result,
-  COALESCE(SUM(amount),0) AS sum_amount,
-  COALESCE(COUNT(ft.id),0) AS transaction_count,
-  DATE_TRUNC('{{period}}',rt.rt_datetime) AS {{period}}
-FROM
-  (
-    SELECT
-      *
-    FROM
-      f_transactions
-    WHERE 1
-      {{filter}}
-    AND
-      account = '{{account}}'
-  ) ft
-  RIGHT JOIN
-  (
-    SELECT
-      processor_result,
-      DATE_TRUNC('{{period}}',datetime) rt_datetime
-    FROM
-      d_datetime,
-      d_processor_result
-    GROUP BY
-      processor_result,
-      DATE_TRUNC('{{period}}',datetime)
-  ) rt
-  ON
-    (ft.processor_result = rt.processor_result AND DATE_TRUNC ('{{period}}',ft.datetime) = rt_datetime)
-WHERE
-  rt.rt_datetime BETWEEN DATE '{{start}}' AND DATE '{{end}}'
+  merchant_processor,
+  processor_result,
+  SUM(amount) AS sum_amount,
+  COUNT(*) AS transaction_count,
+  DATE_TRUNC('{{period}}', datetime) AS {{period}}
+FROM f_transactions
+WHERE 1
+  {{filter}}
+  AND account = '{{account}}'
+  AND   datetime BETWEEN DATE '{{start}}' AND DATE '{{end}}'
 GROUP BY
-  rt.processor_result,
-  rt_datetime
+  processor_result,
+  merchant_processor,
+  DATE_TRUNC('{{period}}',datetime)
 ORDER BY
-  {{period}},processor_result
+  merchant_processor,
+  processor_result,
+  {{period}};
