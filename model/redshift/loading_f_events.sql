9d54d9e2e3ac0799ec9865ac5961591db9f27b3d
azelen <azelen@gmail.com>
2017-05-04 18:28:05 +0200

    Clear keys
    Former-commit-id: 894bf861876178e54184feee0d86b6bea70d53a2

--- model/redshift/loading_f_events.sql
+++ model/redshift/loading_f_events.sql
@@ -1,7 +1,4 @@
 COPY f_events
-FROM 's3://sixcrm-redshift-staging/test_events.csv' credentials 'aws_access_key_id=AKIAIP6FAI6MVLVAPRWQ;aws_secret_access_key=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'
+FROM 's3://sixcrm-redshift-staging/test_events.csv' credentials 'aws_access_key_id=;aws_secret_access_key='
 IGNOREHEADER 1
 DELIMITER ',';
-
-select count(*) from f_events;
-
