34a8cbd9d83389c05e4d53b8efaddcf6f18c5631
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-05-04 22:01:55 -0700

    Updating Kinesis Firehose utilities
    Former-commit-id: 3588a53df474f644355a24a1b0edb51cd9f1378e

--- helper/generateeventrecord.js
+++ helper/generateeventrecord.js
@@ -9,27 +9,21 @@ setEnvironmentVariables();
 
 const kinesisfirehoseutilities = require('../lib/kinesis-firehose-utilities');
 
-function createRandomKinesisTransactionRecord(){
+function createRandomKinesisEventRecord(){
 
     let spoofed_record = {
-        id: uuidV4(),
+        session: uuidV4(),
+        type : random.selectRandomFromArray(['click', 'lead', 'order','upsell','confirm']),
         datetime: timestamp.getISO8601(),
-        customer: uuidV4(),
-        creditcard: uuidV4(),
-        merchant_processor: uuidV4(),
-        campaign: uuidV4(),
-        affiliate: uuidV4(),
-        amount: random.randomDouble(1.00, 45.00, 2),
-        processor_result: random.selectRandomFromArray(['success', 'decline', 'error']),
         account: uuidV4(),
-        transaction_type: random.selectRandomFromArray(['new', 'rebill']),
+        campaign: uuidV4(),
         product_schedule: uuidV4(),
+        affiliate: uuidV4(),
         subaffiliate_1: uuidV4(),
         subaffiliate_2: uuidV4(),
         subaffiliate_3: uuidV4(),
         subaffiliate_4: uuidV4(),
-        subaffiliate_5: uuidV4(),
-        subtype: random.selectRandomFromArray(['main', 'upsell']),
+        subaffiliate_5: uuidV4()
     };
 
     du.debug('Spoofed Record:', spoofed_record);
@@ -40,12 +34,12 @@ function createRandomKinesisTransactionRecord(){
 
 function setEnvironmentVariables(){
 
-    process.env.kinesis_firehose_transactions_stream = 'six-development-transactions';
+    process.env.kinesis_firehose_events_stream = 'sixcrm-firehose-events';
     process.env.aws_region = 'us-east-1';
 
 }
 
-return kinesisfirehoseutilities.putRecord('transactions', createRandomKinesisTransactionRecord()).then((result) => {
+return kinesisfirehoseutilities.putRecord('events', createRandomKinesisEventRecord()).then((result) => {
     du.output('Kinesis Firehose Result', result);
     return result;
 })
