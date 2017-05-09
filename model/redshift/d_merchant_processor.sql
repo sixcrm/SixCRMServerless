258dffe5bd97e77993aee3ee111135f1ecb5ffd1
azelen <azelen@gmail.com>
2017-05-09 21:26:18 +0200

    Column type change
    Former-commit-id: 36ccc237610302bcec9f90b340943757196f0c47

--- model/redshift/d_merchant_processor.sql
+++ model/redshift/d_merchant_processor.sql
@@ -2,7 +2,9 @@ DROP TABLE d_merchant_processor;
 CREATE TABLE d_merchant_processor
 (
   merchant_processor VARCHAR(36),
-  activity_flag      BOOLEAN
+  activity_flag      VARCHAR(8)
 ) DISTSTYLE ALL;
 
-COMMENT ON COLUMN d_merchant_processor.activity_flag IS 'true is for Active, false is for Non-active';
+COMMENT ON COLUMN d_merchant_processor.activity_flag IS 'Active, inactive';
+
+
