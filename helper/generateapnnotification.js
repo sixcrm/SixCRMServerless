550b4c682f0a16bf7cd4f7e28fc72e494ab2c14c
Claus Ruete <clausruete@icloud.com>
2017-05-09 17:30:56 -0700

    Working APN utilities and dispatch :)
    Former-commit-id: 7f7ed530f37a399a7d6b3d67e400f19b4a085c42

--- helper/generateapnnotification.js
+++ helper/generateapnnotification.js
@@ -1,26 +1,20 @@
 'use strict'
+
 const apnutilities = require('../lib/apn-utilities');
+const apn = require('apn');
 const du = require('../lib/debug-utilities.js');
 
-let message = process.argv[2];
-let user = process.argv[3];
+let deviceToken = process.argv[2];
 
-if (!message) {
-    du.output('Message is required');
-    process.exit();
+let message = process.argv[3];
+
+if(
+    deviceToken == undefined
+    || deviceToken == ''
+    || message == undefined
+    || message == ''
+) {
+    du.output('Usage: node generateapnnotification.js [device token] [message]');
+} else {
+    apnutilities.sendNotification(deviceToken, message)
 }
-
-if (!user) {
-    du.output('User (email) is required');
-    process.exit();
-}
-
-//Flesh out the user.  Make sure device tokens are set
-
-return apnutilities.sendNotifications(user, message).then((result) => {
-    du.output('Notification Result', result);
-    return result;
-})
-.catch((error) => {
-    du.warning('Error:', error);
-});
