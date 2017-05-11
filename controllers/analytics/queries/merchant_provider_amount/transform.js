18da2bd8dc53503a17e8d717f3a4598a9c77a0ad
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-05-11 14:36:59 -0700

    merging enable analytics -  possible damage
    Former-commit-id: 62147e943160c263089f6525b36e6864ac9e22a0

--- controllers/analytics/queries/merchant_provider_amount/transform.js
+++ controllers/analytics/queries/merchant_provider_amount/transform.js
@@ -4,10 +4,52 @@ let du = require('../../../../lib/debug-utilities.js');
 module.exports = function(results, parameters){
 
     du.debug('Transformation Function');
+
     return new Promise((resolve, reject) => {
-        du.info(results);
-        //Technical Debt:  Finish Me!
-        let return_object = {};
+
+        let return_array = [];
+
+        results.forEach((result) => {
+
+            let found = false;
+
+            return_array.forEach((return_object) => {
+
+                if(return_object.id == result.merchant_provider){
+
+                    found = true;
+
+                    return_object[result.processor_result] = {
+                        count:result.transaction_count,
+                        amount:result.sum_amount
+                    };
+
+                }
+
+            });
+
+            if(found == false){
+
+            //Technical Debt:  The name needs to be the Merchant Processor name from the database
+                let mp_array = {
+                    id: result.merchant_provider,
+                    name: result.merchant_provider
+                };
+
+                mp_array[result.processor_result] = {
+                    count:result.transaction_count,
+                    amount:result.sum_amount
+                };
+
+                return_array.push(mp_array);
+
+            }
+
+        });
+
+        let return_object = {merchant_providers: return_array};
+
+        du.info(return_object);
 
         return resolve(return_object);
 
