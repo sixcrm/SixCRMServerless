0b9be0f0bba18b44f60d0b163cc2031993090903
azelen <azelen@gmail.com>
2017-05-14 21:08:06 +0200

    Added a new table
    Former-commit-id: 76d1934f89f65855eb79fbd872804d6a00dd48d2

---
+++ model/redshift/f_activity.sql
@@ -0,0 +1,23 @@
+/*
+14.05.2017 A.Zelen Activity Fact table, anticipating accumulating fact table type
+
+*/
+
+DROP TABLE f_activity;
+
+CREATE TABLE f_activity
+(
+  id               VARCHAR(128) NOT NULL,
+  datetime         TIMESTAMP    NOT NULL,
+  actor            VARCHAR(128)  NOT NULL,
+  actor_type       VARCHAR(128),
+  action           VARCHAR(128),
+  acted_upon       VARCHAR(128),
+  acted_upon_type  VARCHAR(128),
+  associated_with  VARCHAR(128),
+  accociated_with_type VARCHAR(128)
+)
+  distkey (actor) INTERLEAVED sortkey (datetime);
+
+
+COMMENT ON TABLE f_activity Is 'Fact table builed arount';
