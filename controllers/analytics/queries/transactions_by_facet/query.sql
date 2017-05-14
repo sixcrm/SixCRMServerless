0e7e1f6d632d00fd78ec9b697d36aad60c74659c
azelen <azelen@gmail.com>
2017-05-14 18:36:47 +0200

    Added amount and total amount to results
    Former-commit-id: b9912c6a42dc6682f8bfe5072cf381523ff4a44c

--- controllers/analytics/queries/transactions_by_facet/query.sql
+++ controllers/analytics/queries/transactions_by_facet/query.sql
@@ -1,12 +1,22 @@
 SELECT
   {{facet}},
-  count(*) AS transactions_count,
-  sum(count(*))
-  OVER ( ) AS all_transactions
-FROM f_transactions
-WHERE 1
-  {{filter}}
-AND datetime BETWEEN TIMESTAMP '{{start}}' AND TIMESTAMP '{{end}}'
-GROUP BY {{facet}}
+  transactions_count,
+  all_transactions_count,
+  transactions_amount,
+  sum(transactions_amount)
+  OVER ()
+    AS all_transactions_amount
+FROM
+  (SELECT
+     {{facet}},
+     count(*)    AS transactions_count,
+     sum(count(*))
+     OVER ( )    AS all_transactions_count,
+     sum(amount) AS transactions_amount
+   FROM f_transactions
+   WHERE 1
+         {{filter}}
+         AND datetime BETWEEN TIMESTAMP '{{start}}' AND TIMESTAMP '{{end}}'
+   GROUP BY {{facet}})
 ORDER BY transactions_count {{order}}
 LIMIT {{limit}} OFFSET {{offset}}
---
+++ controllers/analytics/queries/transactions_facet_timeseries/README.md
@@ -0,0 +1,14 @@
+# Transactions facet timeseries
+
+____
+
+## Arguments:
+
+### Required
+* `account` - a UUIDv4
+* `start` - a ISO-8601 datetime
+* `end` - a ISO-8601 datetime
+
+### Optional
+
+THIS IS IN WORK !!!!
