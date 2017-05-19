24b1ec739d6dc6d7e7f80719faeda1cfc01f9bf4
azelen <azelen@gmail.com>
2017-05-19 22:39:10 +0200

    Activity python spoofer
    Former-commit-id: fd5f6fd438d0b2fd8ec86494c7c8815594a73c11

---
+++ extra/aws/redshift/script_csv_generator_activity.py
@@ -0,0 +1,69 @@
+import csv
+import uuid
+import random
+from datetime import *
+
+# 21.04.2017 Simple Python script for generating random dataset
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = random.randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+d1 = datetime.strptime('1/1/2017 10:00 AM', '%m/%d/%Y %I:%M %p')
+d2 = datetime.strptime('4/21/2017 10:00 AM', '%m/%d/%Y %I:%M %p')
+
+# Generate random number of distinct entites customers, credit_card
+
+actor = [uuid.uuid4() for x in range(10)]
+acted_upon = [uuid.uuid4() for x in range(10)]
+accociated_with = [uuid.uuid4() for x in range(10)]
+
+"""
+ID internal id
+datetime -
+actor - UUID4 or an e-mail address
+actor type - entity name - customer, user, system
+action - created a new, deleted a etcc...
+acted upon - e-mail or ID required
+acted-upon type - customer ,user, etc...
+associated with not required UUID4 or e-mail
+ACCOCIATED WITH type customer, user merchant proicessor etc...
+
+everyting by the actore everything by the acted upon
+"""
+
+
+g=open("activity.csv","w",newline="\n", encoding="utf-8")
+
+w=csv.writer(g)
+w.writerow((
+ 'id',
+ 'datetime',
+ 'actor',
+ 'actor_type',
+ 'action',
+ 'acted_upon',
+ 'acted_upon_type',
+ 'associated_with',
+ 'associated_with_type'
+))
+
+for i in range(1000000):
+    w.writerow((
+      uuid.uuid4(),
+      random_date(d1,d2),
+      random.choice(actor),
+      random.choice(["customer","merchant processor","system"]),
+      random.choice(["continued","deleted","created new"]),
+      random.choice(acted_upon),
+      random.choice(["customer","merchant processor","system"]),
+      random.choice(accociated_with),
+      random.choice(["customer","merchant processor","system"])
+      ))
+g.close()
