ba0802202b6bcc533da0cf1d9d8b62b35c59bbe3
azelen <azelen@gmail.com>
2017-05-19 18:33:21 +0200

    New events spoofers in extra/redshift
    Former-commit-id: dac02c5a4cec2e808e27e1a07a339bc28c76ca93

---
+++ extra/aws/redshift/script_json_generator_events.js
@@ -0,0 +1,78 @@
+var AWS = require('aws-sdk');
+var pg = require('pg');
+
+var s3 = new AWS.S3();
+var red = new AWS.Redshift();
+var redshiftClient = require('./redshift.js');
+
+var myBucket = 'sixcrm-redshift-staging';
+var myKey = 'test_json_gen_events.json';
+
+if (typeof Promise === 'undefined') {
+    AWS.config.setPromisesDependency(require('bluebird'));
+    console.log("Console");
+}
+
+function createGuid()
+{
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+
+        return v.toString(16);
+    });
+}
+
+function choose(choices) {
+    var index = Math.floor(Math.random() * choices.length);
+
+    return choices[index];
+}
+
+
+function generateJson() {
+    var jsonArr =  [];
+
+    for (var i = 0; i < 200; i++) {
+        jsonArr.push({
+            "session": createGuid(),
+            "type": choose(["click","order","lead","upsell","confirm"]),
+            "datetime": '2017-03-02T17:23:43.043Z',
+            "account": createGuid(),
+            "campaign": createGuid(),
+            "product_schedule": createGuid(),
+            "affiliate": createGuid(),
+            "subaffiliate_1": createGuid(),
+            "subaffiliate_2": createGuid(),
+            "subaffiliate_3": createGuid(),
+            "subaffiliate_4": createGuid(),
+            "subaffiliate_5": createGuid()
+        });
+    }
+
+    jsonArr = jsonArr.map(function(e){
+        return JSON.stringify(e);
+    });
+
+    return jsonArr.join("\n");
+}
+
+
+var putObjectPromise  = function loadBucket() {
+    s3.createBucket({Bucket: myBucket}, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            let params = {Bucket: myBucket, Key: myKey, Body: generateJson()};
+
+            return s3.putObject(params, function(err, data){
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Successfully uploaded data to myBucket/myKey");
+                }
+            }).promise()
+        }
+    })
+};
+
+putObjectPromise();
---
+++ extra/aws/redshift/script_json_loader_events.js
@@ -0,0 +1,21 @@
+var redshiftClient = require('./redshift.js');
+
+redshiftClient.connect(function(err){
+
+    var myBucket = 'sixcrm-redshift-staging';
+    var myKey = 'test_json_gen_events.json';
+
+    if(err) {
+        return console.error('error fetching client from pool', err);
+    }
+    redshiftClient.query("copy f_events from 's3://"+myBucket+"/"+myKey+"' credentials 'aws_access_key_id=AKIAIP6FAI6MVLVAPRWQ;aws_secret_access_key=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' json 'auto' timeformat 'YYYY-MM-DDTHH:MI:SS'"
+      , function(err, result){
+          if(err) {
+              return console.error('Error running load', err);
+          }
+          console.log('Success running load');
+          redshiftClient.end(function (err) {
+              if (err) throw err;
+          });
+      });
+});
