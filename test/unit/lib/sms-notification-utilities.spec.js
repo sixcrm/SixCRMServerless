cd7681e48cfac425cd938e17d3b12b64835bb7aa
Ljubomir Simin <ljubomir@coingcs.com>
2017-05-22 19:46:37 +0200

    Test for SMS notification utils.
    Former-commit-id: 73564ec1a88bbdd820af9e3e25119cb58cea2c93

---
+++ test/unit/lib/sms-notification-utilities.spec.js
@@ -0,0 +1,83 @@
+let SmsNotificationUtilities = global.routes.include('lib', 'sms-notification-utilities.js');
+let chai = require('chai');
+let expect = chai.expect;
+const mockery = require('mockery');
+
+describe('lib/notification-utilities', () => {
+
+    before(() => {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnReplace: false,
+            warnOnUnregistered: false
+        });
+    });
+
+    afterEach(() => {
+        mockery.resetCache();
+    });
+
+    after(() => {
+        mockery.deregisterAll();
+    });
+
+    let valid_notification_object = {
+        id: 'aa26af35-5542-4c2b-9a75-45100e78fc97',
+        account: '*',
+        user: 'any',
+        type: 'any',
+        action: 'any',
+        title: 'any',
+        body: 'any',
+        created_at: '2017-04-06T18:40:41.405Z',
+        updated_at: '2017-04-06T18:40:41.405Z'
+    };
+
+    describe('sendNotificationViaSms', () => {
+
+        it('should not send a message when the object is not valid', () => {
+            // given
+            let notification_object = Object.assign({}, valid_notification_object);
+            let sms_number = '+381630000000';
+
+            mockery.registerMock(global.routes.path('lib', 'sns-utilities'), {
+                sendSMS: (message) => {
+                    expect(message).not.to.equal(message, 'SNS utilities should not have been called.');
+                }
+            });
+
+            // when
+            delete notification_object.id;
+            delete notification_object.user;
+
+            return SmsNotificationUtilities.sendNotificationViaSms(notification_object, sms_number).catch((error) => {
+                // then
+                expect(error).not.to.be.null;
+                return expect(error.message).to.equal('One or more validation errors occurred.');
+            });
+        });
+
+        it('should attempt to send a message when the object is valid', (done) => {
+            // given
+            let sms_number = '+381630000000';
+
+            mockery.registerMock(global.routes.path('lib', 'sns-utilities'), {
+                sendSMS: (message) => {
+                    expect(message).to.be.defined;
+                    done();
+                }
+            });
+            let SmsNotificationUtilities = global.routes.include('lib', 'sms-notification-utilities.js');
+
+            SmsNotificationUtilities.sendNotificationViaSms(valid_notification_object, sms_number)
+                .catch((error) => {
+                    console.log(error.message);
+                    done(error.message);
+                });
+        });
+
+    });
+
+
+
+});
