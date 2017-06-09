ddb0cac9d468e6c6211cec178945f213ef9bef18
Ljubomir Simin <ljubomir@coingcs.com>
2017-06-09 20:10:39 +0200

    Add pagination to `customernotelistbycustomer` enpoint.
    Former-commit-id: d9a12d63e5cadf95b2c0c177606ddc6d39cf93cb

--- controllers/entities/CustomerNote.js
+++ controllers/entities/CustomerNote.js
@@ -27,12 +27,7 @@ class customerNoteController extends entityController {
 
     listByCustomer(customer, pagination){
 
-        return this.queryBySecondaryIndex('customer', customer, 'customer-index', pagination, true)
-          .then((result) => this.getResult(result))
-          //Technical Debt:  Redundancy?
-          .then((result) => {
-              return { customernotes: result }
-          });
+        return this.queryBySecondaryIndex('customer', customer, 'customer-index', pagination, true);
     }
 
 }
