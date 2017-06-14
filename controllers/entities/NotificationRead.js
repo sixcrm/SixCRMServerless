b7177e5b62fb7ab21f10053d62881b523c366042
Ljubomir Simin <ljubomir@coingcs.com>
2017-06-14 14:32:18 +0200

    Refactor NotificationRead to have UUID id property, as well as secondary index.
    Former-commit-id: f1ccbc501edb57c99239599fac389fecdb3761f1

--- controllers/entities/NotificationRead.js
+++ controllers/entities/NotificationRead.js
@@ -15,15 +15,9 @@ class notificationReadController extends entityController {
      * Save the time the current user has seen the notifications for the current account.
      */
     markNotificationsAsSeen() {
-
-        du.debug('Mark notifications as seen.');
-
-        let notificationread = {
-            id: `${global.user.id}/${global.account}`
-        };
-
-        return this.touch(notificationread);
-
+        return this.getOrCreateNotificationRead().then(notification_read => {
+            this.touch(notification_read);
+        });
     }
 
     /**
@@ -31,13 +25,44 @@ class notificationReadController extends entityController {
      * database - create one.
      */
     getLastSeenTime() {
-        let id = `${global.user.id}/${global.account}`;
+        return this.getOrCreateNotificationRead().then(notification_read => {
+            return notification_read.updated_at;
+        });
+    }
 
-        return this.get(id).then((data) => {
-            if (!data) {
-                return this.markNotificationsAsSeen().then(() => timestamp.getISO8601());
+    getNotificationRead() {
+        let user_id = global.user.id;
+        let account_id = global.account;
+
+        return this.queryBySecondaryIndex('user', user_id, 'user-index').then((response) => {
+            let notification_reads = response.notificationreads;
+
+            if (!notification_reads || notification_reads.length === 0) {
+                return null;
+            }
+
+            let data_for_account = notification_reads.filter((notification_read) => notification_read.account === account_id);
+
+            if (!data_for_account || data_for_account.length === 0) {
+                return null;
             } else {
-                return data.updated_at;
+                return data_for_account[0];
+            }
+        });
+    }
+
+    getOrCreateNotificationRead() {
+        let user_id = global.user.id;
+        let account_id = global.account;
+
+        return this.getNotificationRead().then(response => {
+            if (!response) {
+                return this.create({
+                    user: user_id,
+                    account: account_id
+                });
+            } else {
+                return response;
             }
         });
     }
