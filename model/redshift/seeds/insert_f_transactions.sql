c8264f7ccc7ce12e8dc2d0eea3de0cb60422367c
aldo.zelen@toptal.com <azelen@gmail.com>
2017-06-20 20:55:45 +0200

    Bug fix
    Former-commit-id: 984e0199db945b49d026ba155ce1e5bfdad945dd

--- model/redshift/seeds/insert_f_transactions.sql
+++ model/redshift/seeds/insert_f_transactions.sql
@@ -1,13 +1,21 @@
-INSERT INTO f_transactions(session, type, datetime, account, campaign, product_schedule, affiliate, subaffiliate_1, subaffiliate_2, subaffiliate_3, subaffiliate_4, subaffiliate_5)
+INSERT INTO public.f_transactions (id, datetime, customer, creditcard, merchant_provider, campaign, affiliate, amount, processor_result, account, transaction_type, transaction_subtype, product_schedule, subaffiliate_1, subaffiliate_2, subaffiliate_3, subaffiliate_4, subaffiliate_5)
+VALUES (
+  '99999999-999e-44aa-999e-aaa9a99a9999',
+  '2017-01-01 10:06:54.000000',
+  '99999999-999e-44aa-999e-aaa9a99a9999',
+  '99999999-999e-44aa-999e-aaa9a99a9999',
+  '99999999-999e-44aa-999e-aaa9a99a9999',
+  '99999999-999e-44aa-999e-aaa9a99a9999',
+  '99999999-999e-44aa-999e-aaa9a99a9999',
+  0,
+  'success',
+  '99999999-999e-44aa-999e-aaa9a99a9999',
+  'new',
+  'upsell',
+  '99999999-999e-44aa-999e-aaa9a99a9999',
+  '',
+  '',
+  '',
+  '',
+  '');
 
-VALUES
-  ('99999999-999e-44aa-999e-aaa9a99a9999', 'lead', '2017-01-01 00:00:00.000000', '99999999-999e-44aa-999e-aaa9a99a9999',
-                                           '99999999-999e-44aa-999e-aaa9a99a9999',
-                                           '99999999-999e-44aa-999e-aaa9a99a9999',
-                                           '99999999-999e-44aa-999e-aaa9a99a9999',
-                                           '99999999-999e-44aa-999e-aaa9a99a9999',
-                                           '99999999-999e-44aa-999e-aaa9a99a9999',
-                                           '99999999-999e-44aa-999e-aaa9a99a9999',
-                                           '99999999-999e-44aa-999e-aaa9a99a9999',
-   '99999999-999e-44aa-999e-aaa9a99a9999'
-  );
