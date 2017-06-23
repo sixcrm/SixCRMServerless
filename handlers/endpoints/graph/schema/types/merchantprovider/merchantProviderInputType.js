dee58ede6f2deb82e990879836003eefe4bf818f
Ljubomir Simin <ljubomir@coingcs.com>
2017-06-23 15:19:51 +0200

    Oops.
    Former-commit-id: cd0459ba1f80124a6b51a28810c54ef8434d2d0a

--- handlers/endpoints/graph/schema/types/merchantprovider/merchantProviderInputType.js
+++ handlers/endpoints/graph/schema/types/merchantprovider/merchantProviderInputType.js
@@ -6,7 +6,7 @@ const GraphQLInputObjectType = require('graphql').GraphQLInputObjectType;
 module.exports.graphObj = new GraphQLInputObjectType({
     name: 'MerchantProviderInput',
     fields: () => ({
-        id:					{ type: new GraphQLString },
+        id:					{ type: GraphQLString },
         name:				{ type: new GraphQLNonNull(GraphQLString) },
         username:			{ type: new GraphQLNonNull(GraphQLString) },
         password:			{ type: new GraphQLNonNull(GraphQLString) },
