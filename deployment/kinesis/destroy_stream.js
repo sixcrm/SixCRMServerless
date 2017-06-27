440f919c967a636f3c4e39641249eb2f9f05e452
aldo.zelen@toptal.com <azelen@gmail.com>
2017-06-27 22:19:45 +0200

    Getting list of kinesis streams to destroy
    Former-commit-id: 8cab17f5de70ed41433f7c803bd695176742a032

---
+++ deployment/kinesis/destroy_stream.js
@@ -0,0 +1,42 @@
+'use strict';
+require('../../routes.js');
+
+const fs = require('fs');
+
+const du = global.routes.include('lib', 'debug-utilities.js');
+const stringUtilities = global.routes.include('deployment', 'utilities/string-utilities.js');
+const KinesisDeployment = global.routes.include('deployment', 'utilities/kinesis-deployment.js');
+
+const AWS = require("aws-sdk");
+var firehose = new AWS.Firehose({apiVersion: '2015-08-04',  region: 'us-east-1'});
+
+let environment = process.argv[2] || 'development';
+let region = process.argv[3] || process.env.AWS_REGION || 'us-east-1';
+
+du.highlight('Destroying Kinesis Stream');
+
+let kinesisDeployment = new KinesisDeployment(environment);
+
+/* Set the list of relevant streams*/
+
+let stream_list = Object.keys(kinesisDeployment.getConfig().streams).filter(name => name.match(/\_stream$/));
+
+
+stream_list.map( stream =>  {
+
+  let stream_parameters = {
+    StreamName: kinesisDeployment.getConfig().streams[stream].DeliveryStreamName
+  };
+  du.output('Stream parameters are:', stream_parameters);
+
+  kinesisDeployment.streamExists(stream_parameters.StreamName).then(exists => {
+      if (exists) {
+          du.warning('Stream exists, destroying.');
+          return Promise.resolve();
+      } else {
+          du.output('Stream does not exist, aborting.');
+          return Promise.resolve();
+      }
+  }).then(() => { du.highlight('Complete')});
+}
+);
