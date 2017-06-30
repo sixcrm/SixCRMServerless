15303af60fdf8cd5f9878dff1c568ef0e326d080
aldo.zelen@toptal.com <azelen@gmail.com>
2017-06-30 21:34:23 +0200

    Work on S3 bucket
    Former-commit-id: 253aebf19fa1e50001be488a960c4ba19b53a857

--- deployment/s3/create_buckets.js
+++ deployment/s3/create_buckets.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const du = global.routes.include('lib', 'debug-utilities.js');
 const stringUtilities = global.routes.include('deployment', 'utilities/string-utilities.js');
-const KinesisDeployment = global.routes.include('deployment', 'utilities/s3-deployment.js');
+const S3sDeployment = global.routes.include('deployment', 'utilities/s3-deployment.js');
 
 const AWS = require("aws-sdk");
 var s3 = new AWS.S3({apiVersion: '2006-03-01',  region: 'us-east-1'});
@@ -16,3 +16,15 @@ let region = process.argv[3] || process.env.AWS_REGION || 'us-east-1';
 du.highlight('Creating S3 Bucket');
 
 let s3Deployment = new S3Deployment(environment);
+
+s3Deployment.createBucketAndWait(stream_parameters.DeliveryStreamName).then(exists => {
+    if (exists) {
+        du.warning('Bucket exists, aborting.');
+        return Promise.resolve();
+    } else {
+        du.output('Bucket does not exist, creating.');
+        return s3Deployment.createBucket(stream_parameters).then(response => {
+        du.output(response);
+        });
+    }
+}).then(() => { du.highlight('Complete')});
