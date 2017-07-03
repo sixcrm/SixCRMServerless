e328e7f792563e9e1179e9c8e593f3b1b9f6e260
aldo.zelen@toptal.com <azelen@gmail.com>
2017-07-03 22:01:14 +0200

    Making seed dml scripts idempotent
    Former-commit-id: 66b5d7071482859731c793ecf2c78b76ac636b9a

--- model/redshift/seeds/insert_f_activity.sql
+++ model/redshift/seeds/insert_f_activity.sql
@@ -1,11 +1,26 @@
-INSERT INTO f_activity (id, datetime, account, actor, actor_type, action, acted_upon, acted_upon_type, associated_with, associated_with_type) VALUES
-  ('99999999-999e-44aa-999e-aaa9a99a9999',
-   '2017-01-01 00:00:00.000000',
-   '99999999-999e-44aa-999e-aaa9a99a9999',
-   'system',
-   'system',
-   'deleted',
-   '99999999-999e-44aa-999e-aaa9a99a9999',
-   'rebill',
-   '99999999-999e-44aa-999e-aaa9a99a9999',
-   'merchantprovider');
+INSERT INTO f_activity (id, datetime, account, actor, actor_type, action, acted_upon, acted_upon_type, associated_with, associated_with_type)
+  ((
+     SELECT
+       '99999999-999e-44aa-999e-aaa9a99a9999',
+       '2017-01-01 00:00:00.000000',
+       '99999999-999e-44aa-999e-aaa9a99a9999',
+       'system',
+       'system',
+       'deleted',
+       '99999999-999e-44aa-999e-aaa9a99a9999',
+       'rebill',
+       '99999999-999e-44aa-999e-aaa9a99a9999',
+       'merchantprovider')
+   EXCEPT
+   SELECT
+     fa.id,
+     fa.datetime,
+     fa.account,
+     fa.actor,
+     fa.actor_type,
+     fa.action,
+     fa.acted_upon,
+     fa.acted_upon_type,
+     fa.associated_with,
+     fa.associated_with_type
+   FROM f_activity fa);
--- model/redshift/seeds/insert_f_events.sql
+++ model/redshift/seeds/insert_f_events.sql
@@ -1,15 +1,29 @@
 INSERT INTO f_events (session, type, datetime, account, campaign, product_schedule, affiliate, subaffiliate_1, subaffiliate_2, subaffiliate_3, subaffiliate_4, subaffiliate_5)
-VALUES
-  ('99999999-999e-44aa-999e-aaa9a99a9999',
-    'lead',
-    '2017-01-01 00:00:00.000000',
-    '99999999-999e-44aa-999e-aaa9a99a9999',
-    '99999999-999e-44aa-999e-aaa9a99a9999',
-    '99999999-999e-44aa-999e-aaa9a99a9999',
-    '99999999-999e-44aa-999e-aaa9a99a9999',
-    '99999999-999e-44aa-999e-aaa9a99a9999',
-    '99999999-999e-44aa-999e-aaa9a99a9999',
-    '99999999-999e-44aa-999e-aaa9a99a9999',
-    '99999999-999e-44aa-999e-aaa9a99a9999',
-   '99999999-999e-44aa-999e-aaa9a99a9999'
-  );
+  ((SELECT
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      'lead',
+      '2017-01-01 00:00:00.000000',
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      '99999999-999e-44aa-999e-aaa9a99a9999',
+      '99999999-999e-44aa-999e-aaa9a99a9999')
+   EXCEPT
+   SELECT
+     fe.session,
+     fe.type,
+     fe.datetime,
+     fe.account,
+     fe.campaign,
+     fe.product_schedule,
+     fe.affiliate,
+     fe.subaffiliate_1,
+     fe.subaffiliate_2,
+     fe.subaffiliate_3,
+     fe.subaffiliate_4,
+     fe.subaffiliate_5
+   FROM f_events fe);
