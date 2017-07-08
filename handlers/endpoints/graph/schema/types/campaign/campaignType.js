3369faf93d19a8b6d36ee558b311735a97fbe7da
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-07-07 18:44:58 -0700

    Affiliate Allow/Deny
    Former-commit-id: 83132259b1149363371da763f12fe02bfb63c8eb

--- handlers/endpoints/graph/schema/types/campaign/campaignType.js
+++ handlers/endpoints/graph/schema/types/campaign/campaignType.js
@@ -5,6 +5,7 @@ const GraphQLObjectType = require('graphql').GraphQLObjectType;
 const GraphQLString = require('graphql').GraphQLString;
 
 let emailTemplateType = require('../emailtemplate/emailTemplateType');
+let affiliateAllowDenyType = require('../affiliate/affiliateAllowDenyType');
 let productScheduleType = require('../productschedule/productScheduleType');
 
 module.exports.graphObj = new GraphQLObjectType({
@@ -14,37 +15,55 @@ module.exports.graphObj = new GraphQLObjectType({
     	id: {
         type: new GraphQLNonNull(GraphQLString),
         description: 'The id of the campaign.',
-    },
-        name: {
-            type: new GraphQLNonNull(GraphQLString),
-            description: 'The name of the campaign.',
-        },
-        productschedules: {
-            type: new GraphQLList(productScheduleType.graphObj),
-            description: 'The configured product schedules associated with the campaign',
-            resolve: (campaign) => {
-                var campaignController = global.routes.include('controllers', 'entities/Campaign.js');
+      },
+      name: {
+          type: new GraphQLNonNull(GraphQLString),
+          description: 'The name of the campaign.',
+      },
+      productschedules: {
+          type: new GraphQLList(productScheduleType.graphObj),
+          description: 'The configured product schedules associated with the campaign',
+          resolve: (campaign) => {
+              var campaignController = global.routes.include('controllers', 'entities/Campaign.js');
 
-                return campaignController.getProductSchedules(campaign);
-            }
-        },
-        emailtemplates: {
-            type: new GraphQLList(emailTemplateType.graphObj),
-            descsription: 'Email templates configured and associated with the campaign',
-            resolve: (campaign) => {
-                var campaignController = global.routes.include('controllers', 'entities/Campaign.js');
+              return campaignController.getProductSchedules(campaign);
+          }
+      },
+      emailtemplates: {
+          type: new GraphQLList(emailTemplateType.graphObj),
+          descsription: 'Email templates configured and associated with the campaign',
+          resolve: (campaign) => {
+              var campaignController = global.routes.include('controllers', 'entities/Campaign.js');
 
-                return campaignController.getEmailTemplates(campaign);
-            }
-        },
-        created_at: {
-  	    type: new GraphQLNonNull(GraphQLString),
-            description: 'ISO8601 datetime when the entity was created.',
-        },
-        updated_at: {
-            type: new GraphQLNonNull(GraphQLString),
-            description: 'ISO8601 datetime when the entity was updated.',
+              return campaignController.getEmailTemplates(campaign);
+          }
+      },
+      affiliate_allow: {
+        type: new GraphQLList(affiliateAllowDenyType.graphObj),
+        description: 'The affiliate allow list on this campaign.',
+        resolve: (campaign) => {
+          var campaignController = global.routes.include('controllers', 'entities/Campaign.js');
+
+          return campaignController.getAffiliateAllowDenyList(campaign.affiliate_allow);
         }
+      },
+      affiliate_deny: {
+        type: new GraphQLList(affiliateAllowDenyType.graphObj),
+        description: 'The affiliate deny list on this campaign.',
+        resolve: (campaign) => {
+          var campaignController = global.routes.include('controllers', 'entities/Campaign.js');
+
+          return campaignController.getAffiliateAllowDenyList(campaign.affiliate_deny);
+        }
+      },
+      created_at: {
+	       type: new GraphQLNonNull(GraphQLString),
+         description: 'ISO8601 datetime when the entity was created.',
+      },
+      updated_at: {
+          type: new GraphQLNonNull(GraphQLString),
+          description: 'ISO8601 datetime when the entity was updated.',
+      }
     }),
     interfaces: []
 });
