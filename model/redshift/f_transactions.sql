38c62b7113ab1a40d0b7fd994e117c82250613d7
aldo.zelen@toptal.com <azelen@gmail.com>
2017-07-11 20:13:52 +0200

    Added a new version of f_transactions table with the prepaid boolean type
    Former-commit-id: 974c6365474b393a7ac1a31fe4f763b4fe83c9bd

--- model/redshift/f_transactions.sql
+++ model/redshift/f_transactions.sql
@@ -2,7 +2,7 @@
 21.04.2017 A.Zelen Initial table definition
 03.07.2017 A.Zelen Logic from idempotent versioning
 
-TABLE_VERSION 1
+TABLE_VERSION 2
 */
 
 DROP TABLE f_transactions;
@@ -10,7 +10,7 @@ DROP TABLE f_transactions;
 DELETE FROM sys_sixcrm.sys_table_version WHERE table_name ='f_transactions';
 
 INSERT INTO sys_sixcrm.sys_table_version
-     SELECT 'f_transactions',1,getdate();
+     SELECT 'f_transactions',2,getdate();
 
 
 CREATE TABLE IF NOT EXISTS f_transactions
@@ -32,7 +32,8 @@ CREATE TABLE IF NOT EXISTS f_transactions
   subaffiliate_2      VARCHAR(128) encode ZSTD,
   subaffiliate_3      VARCHAR(128) encode ZSTD,
   subaffiliate_4      VARCHAR(128) encode ZSTD,
-  subaffiliate_5      VARCHAR(128) encode ZSTD
+  subaffiliate_5      VARCHAR(128) encode ZSTD,
+  prepaid             BOOLEAN
 ) INTERLEAVED SORTKEY (account, datetime);
 
 COMMENT ON TABLE f_transactions IS 'Main Fact table with information about transactions';
