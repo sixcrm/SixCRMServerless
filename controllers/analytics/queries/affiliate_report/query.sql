679022b463b191247d2c4f8a97cead38b7420039
aldo.zelen@toptal.com <azelen@gmail.com>
2017-07-14 22:06:50 +0200

    Skeleton of affiliates query
    Former-commit-id: 3542b6adf524968966310787e407fc8bd38a5270

---
+++ controllers/analytics/queries/affiliate_report/query.sql
@@ -0,0 +1,57 @@
+SELECT fe.affiliate,
+       fe.count_click,
+       fe.count_partials,
+       decode(fe.count_click,0,0, 1.0*fe.count_partials / fe.count_click) partials_percent,
+       coalesce(decline_count,0),
+       coalesce(decode(decline_count,0,0, 1.0*decline_count / fe.count_click),0) declines_percent,
+       fe.count_sales,
+       decode(fe.count_sales,0,0, 1.0*fe.count_sales / fe.count_click) sales_percent,
+       fe.count_upsell,
+       decode(fe.count_upsell,0,0, 1.0*fe.count_upsell / fe.count_click) upsell_percent,
+       sum_upsell,
+       sum_amount,
+       sum(sum_amount) over () all_sum_amount,
+       fe.{{period}}
+FROM
+  (SELECT affiliate,
+          count(CASE
+                    WHEN TYPE='click' THEN 1
+                    ELSE NULL
+                END) count_click,
+          count(CASE
+                    WHEN TYPE='lead' THEN 1
+                    ELSE NULL
+                END) count_partials,
+          count(CASE
+                    WHEN TYPE LIKE 'upsell%' THEN 1
+                    ELSE NULL
+                END) count_upsell,
+          count(CASE
+                    WHEN TYPE ='order' THEN 1
+                    ELSE NULL
+                END) count_sales,
+          DATE_TRUNC('{{period}}',datetime) AS {{period}}
+   FROM f_events fe
+   WHERE 1 {{filter}}
+     AND datetime BETWEEN TIMESTAMP '{{start}}' AND TIMESTAMP '{{end}}'
+   GROUP BY affiliate,
+            DATE_TRUNC('{{period}}',datetime)) fe
+RIGHT OUTER JOIN
+  (SELECT sum(amount) sum_amount,
+          sum(CASE
+                  WHEN transaction_subtype LIKE 'upsell%' THEN amount
+                  ELSE 0
+              END) sum_upsell,
+          count(CASE
+                    WHEN transaction_subtype IN ('order','main')
+                         AND processor_result ='decline' THEN 1
+                    ELSE NULL
+                END) decline_count,
+          affiliate,
+          DATE_TRUNC('{{period}}',datetime) AS {{period}}
+   FROM f_transactions
+   WHERE 1 {{filter}}
+     AND datetime BETWEEN TIMESTAMP '{{start}}' AND TIMESTAMP '{{end}}'
+   GROUP BY affiliate,
+            DATE_TRUNC('{{period}}',datetime)) ft
+              ON (fe.affiliate = ft.affiliate AND fe.{{period}} = ft.{{period}});
