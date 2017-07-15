bda61991e2a40e7c3eee0c51d10aad227efbd1b3
aldo.zelen@toptal.com <azelen@gmail.com>
2017-07-15 20:01:31 +0200

    Affiliates report query
    Former-commit-id: 76d3f24e99188e295aa1b1d6c18909621285969a

--- controllers/analytics/queries/affiliate_report/query.sql
+++ controllers/analytics/queries/affiliate_report/query.sql
@@ -1,33 +1,33 @@
 SELECT fe.affiliate,
        fe.count_click,
        fe.count_partials,
-       decode(fe.count_click,0,0, 1.0*fe.count_partials / fe.count_click) partials_percent,
-       coalesce(decline_count,0),
-       coalesce(decode(decline_count,0,0, 1.0*decline_count / fe.count_click),0) declines_percent,
+       decode(fe.count_click,0,0, 1.0*fe.count_partials / fe.count_click) AS partials_percent,
+       coalesce(decline_count,0) AS decline_count,
+       coalesce(decode(decline_count,0,0, 1.0*decline_count / fe.count_click),0) AS declines_percent,
        fe.count_sales,
-       decode(fe.count_sales,0,0, 1.0*fe.count_sales / fe.count_click) sales_percent,
+       decode(fe.count_sales,0,0, 1.0*fe.count_sales / fe.count_click) AS sales_percent,
        fe.count_upsell,
-       decode(fe.count_upsell,0,0, 1.0*fe.count_upsell / fe.count_click) upsell_percent,
+       decode(fe.count_upsell,0,0, 1.0*fe.count_upsell / fe.count_click) AS upsell_percent,
        sum_upsell,
        sum_amount,
-       sum(sum_amount) over () all_sum_amount,
+       sum(sum_amount) over () AS all_sum_amount,
        fe.{{period}}
 FROM
   (SELECT affiliate,
           count(CASE
-                    WHEN TYPE='click' THEN 1
+                    WHEN type='click' THEN 1
                     ELSE NULL
                 END) count_click,
           count(CASE
-                    WHEN TYPE='lead' THEN 1
+                    WHEN type='lead' THEN 1
                     ELSE NULL
                 END) count_partials,
           count(CASE
-                    WHEN TYPE LIKE 'upsell%' THEN 1
+                    WHEN type LIKE 'upsell%' THEN 1
                     ELSE NULL
                 END) count_upsell,
           count(CASE
-                    WHEN TYPE ='order' THEN 1
+                    WHEN type ='order' THEN 1
                     ELSE NULL
                 END) count_sales,
           DATE_TRUNC('{{period}}',datetime) AS {{period}}
@@ -53,5 +53,8 @@ RIGHT OUTER JOIN
    WHERE 1 {{filter}}
      AND datetime BETWEEN TIMESTAMP '{{start}}' AND TIMESTAMP '{{end}}'
    GROUP BY affiliate,
-            DATE_TRUNC('{{period}}',datetime)) ft
-              ON (fe.affiliate = ft.affiliate AND fe.{{period}} = ft.{{period}});
+            DATE_TRUNC('{{period}}',datetime)) ft ON (fe.affiliate = ft.affiliate
+                                                      AND fe.{{period}} = ft.{{period}})
+ORDER BY DATETIME {{order}}
+LIMIT {{limit}}
+OFFSET {{offset}};
--- controllers/analytics/queries/events/transform.js
+++ controllers/analytics/queries/events/transform.js
@@ -3,43 +3,48 @@ let _ = require('underscore');
 let du = global.routes.include('lib', 'debug-utilities.js');
 let paginationutilities = global.routes.include('lib', 'pagination-utilities.js');
 
-module.exports = function(results, parameters){
+module.exports = function(results, parameters) {
 
-    du.debug('Transformation Function');
+  du.debug('Transformation Function');
 
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
 
-        let result_array = [];
+    let result_array = [];
 
-        du.info(results);
+    du.info(results);
 
-        results.forEach((result) => {
+    results.forEach((result) => {
 
-            result_array.push({
-                session:result.session,
-      			  type: result.type,
-      			  datetime: result.datetime,
-      			  account: result.account,
-      			  campaign: result.campaign,
-      			  product_schedule: result.product_schedule,
-      			  affiliate: result.affiliate,
-      			  subaffiliate_1: result.subaffiliate_1,
-      			  subaffiliate_2: result.subaffiliate_2,
-      			  subaffiliate_3: result.subaffiliate_3,
-      			  subaffiliate_4: result.subaffiliate_4,
-      			  subaffiliate_5: result.subaffiliate_5
-            });
-
-        });
-
-        parameters['count'] = results.length;
-
-        let pagination_object = paginationutilities.createSQLPaginationObject(parameters);
-
-        let return_object = {events:result_array, pagination: pagination_object};
-
-        return resolve(return_object);
+      result_array.push({
+        affiliate: result.affiliate,
+        count_click: result.count_click,
+        count_partials: result.count_partials,
+        partials_percent: result.partials_percent,
+        decline_count: result.decline_count,
+        declines_percent: result.declines_percent,
+        count_sales: result.count_sales,
+        sales_percent: result.sales_percent,
+        count_upsell: result.count_upsell,
+        upsell_percent: result.upsell_percent,
+        sum_upsell: result.sum_upsell,
+        sum_amount: result.sum_amount,
+        all_sum_amount: result.all_sum_amount,
+        period: result.period
+      });
 
     });
+
+    parameters['count'] = results.length;
+
+    let pagination_object = paginationutilities.createSQLPaginationObject(parameters);
+
+    let return_object = {
+      affiliates: result_array,
+      pagination: pagination_object
+    };
+
+    return resolve(return_object);
+
+  });
 
 }
