cfe6d8fcde99e8d800bd612af7d84c270c4f5364
Ljubomir Simin <ljubomir@coingcs.com>
2017-08-23 13:55:01 +0200

    Add more fields to fulfillment provider.
    Former-commit-id: caca46f4d4de99653cd01624252bb8540007fd57

--- handlers/endpoints/graph/schema/types/fulfillmentprovider/fulfillmentProviderInputType.js
+++ handlers/endpoints/graph/schema/types/fulfillmentprovider/fulfillmentProviderInputType.js
@@ -11,6 +11,12 @@ module.exports.graphObj = new GraphQLInputObjectType({
         username:			{ type: new GraphQLNonNull(GraphQLString) },
         password:			{ type: new GraphQLNonNull(GraphQLString) },
         endpoint:			{ type: new GraphQLNonNull(GraphQLString) },
-        provider:			{ type: new GraphQLNonNull(GraphQLString) }
+        provider:			{ type: new GraphQLNonNull(GraphQLString) },
+        company:            { type: GraphQLString },
+        threepl_key:        { type: GraphQLString },
+        facility_id:        { type: GraphQLString },
+        threepl_id:         { type: GraphQLString },
+        customer_id:        { type: GraphQLString },
+        return_address:     { type: GraphQLString }
     })
 });
