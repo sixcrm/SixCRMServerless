0bbaa4830cc8be311f0b6a70e6abcc82f2fe946a
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-08-24 19:06:13 -0700

    For novelty?
    Former-commit-id: c2d0af016004e6aaabcffe8a6148d16e696eb60a

--- deployment/redshift/depreciated/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Deploying Redshift';
-
-#
-# dc1.large
-# 1
-
-aws redshift create-cluster --node-type dc1.large --number-of-nodes 2 --master-username admin --master-user-password Sladoled9 --cluster-identifier sixcrm-test-deploy
--- deployment/redshift/depreciated/destroy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'Destroying Redshift cluster';
-
-#
-
-aws redshift delete-cluster --cluster-identifier sixcrm-test-deploy --skip-final-cluster-snapshot
