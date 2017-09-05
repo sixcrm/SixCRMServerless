4655f04ed2e09198bc6bf5f7749e3cd3ba16f75e
Ljubomir Simin <ljubomir@coingcs.com>
2017-09-05 18:04:43 +0200

    Update notification helpers, remove obsolete configuration.
    Former-commit-id: 0221d95c9743523ac3248299bcb53492600c2c41

--- helper/notifications/generatesmsnotification.js
+++ helper/notifications/generatesmsnotification.js
@@ -5,24 +5,6 @@ const yaml = require('js-yaml');
 
 require('../../SixCRM.js');
 
-const site_config = yaml.safeLoad(fs.readFileSync(__dirname+`/../../config/${process.env.stage}/site.yml`, 'utf8'));
-
-process.env.users_table = site_config.dynamodb.users_table;
-process.env.user_acls_table = site_config.dynamodb.user_acls_table;
-process.env.notifications_table = site_config.dynamodb.notifications_table;
-process.env.notification_settings_table = site_config.dynamodb.notification_settings_table;
-process.env.notifications_read_table = site_config.dynamodb.notifications_read_table;
-process.env.dynamo_endpoint = site_config.dynamodb.endpoint;
-process.env.search_indexing_queue = 'search_indexing'
-
-if (process.env.stage === 'local') {
-    process.env.users_table = 'local' + process.env.users_table;
-    process.env.user_acls_table = 'local' + process.env.user_acls_table;
-    process.env.notifications_table = 'local' + process.env.notifications_table;
-    process.env.notifications_read_table = 'local' + process.env.notifications_read_table;
-    process.env.notification_settings_table = 'local' + process.env.notification_settings_table;
-}
-
 const SmsNotificationProvider = global.SixCRM.routes.include('controllers','providers/notification/sms-notification-provider');
 const PermissionUtilities = global.SixCRM.routes.include('lib','permission-utilities');
 const du = global.SixCRM.routes.include('lib','debug-utilities.js');
