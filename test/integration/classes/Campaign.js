2af30459e134f7ea00fa2acf3eb4dfd67974d1db
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-09-18 12:59:34 -0700

    Campaign Block Test
    Former-commit-id: 1965d71ac96b77a85d06726c9f756107fe84c807

---
+++ test/integration/classes/Campaign.js
@@ -0,0 +1,83 @@
+'use strict'
+const uuidV4 = require('uuid/v4');
+
+const tu = global.SixCRM.routes.include('lib','test-utilities.js');
+const du = global.SixCRM.routes.include('lib','debug-utilities.js');
+
+const IntegrationTest = global.SixCRM.routes.include('test', 'integration/classes/IntegrationTest');
+
+module.exports = class CampaignTest extends IntegrationTest {
+
+  constructor(){
+
+    super();
+
+    this.endpoint = global.integration_test_config.endpoint;
+    this.account = global.test_accounts[1];
+    this.user = global.test_users[0];
+    this.test_jwt = tu.createTestAuth0JWT(this.user.email, global.SixCRM.configuration.site_config.jwt.site.secret_key);
+
+  }
+
+  executeTrackerBlockTest(){
+
+    du.output('Execute Tracker Block Test');
+
+    let campaign_id = uuidV4();
+    let tracker_id = uuidV4();
+
+    du.info('Campaign ID: '+campaign_id);
+    du.info('Tracker ID: '+tracker_id);
+
+    return this.createCampaign(campaign_id)
+    .then(() => this.createTracker(tracker_id, campaign_id))
+    .then(() => this.deleteCampaign(campaign_id, 403))
+    .then(response => {
+      return response;
+    })
+    .then(() => this.deleteTracker(tracker_id))
+    .then(() => this.deleteCampaign(campaign_id));
+
+  }
+
+  createCampaign(campaign_id){
+
+    du.output('Create Campaign');
+
+    let campaign_create_query = `mutation { createcampaign ( campaign: { id: "`+campaign_id+`", name: "Testing Campaign", allow_prepaid: false, show_prepaid: false, productschedules:[], emailtemplates:[], affiliate_allow:[], affiliate_deny:[] } ) { id  } }`;
+
+    return this.executeQuery(campaign_create_query);
+
+  }
+
+  createTracker(tracker_id, campaign_id){
+
+    du.output('Create Tracker');
+
+    let tracker_create_query = `mutation { createtracker ( tracker: { id: "`+tracker_id+`", event_type:["main"], affiliates: [], campaigns: ["`+campaign_id+`"] type: "postback", name:"Testing Tracker 3", body:"http://sofun.com"}) { id } }`;
+
+    return this.executeQuery(tracker_create_query);
+
+  }
+
+  deleteTracker(id, code){
+
+    du.output('Delete Tracker');
+
+    let tracker_delete_query = 'mutation { deletetracker (id: "'+id+'") { id } }';
+
+    return this.executeQuery(tracker_delete_query, code);
+
+  }
+
+  deleteCampaign(id, code){
+
+    du.output('Delete Campaign');
+
+    let campaign_delete_query = 'mutation { deletecampaign (id: "'+id+'") { id } }';
+
+    return this.executeQuery(campaign_delete_query, code);
+
+  }
+
+}
