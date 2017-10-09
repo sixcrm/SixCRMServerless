b4466025c23e5c5d4e180a6327dc0ee9f1f7e1b6
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-10-09 12:04:48 -0700

    Retry...
    Former-commit-id: 45265b8731752257e04d73797fa1eeb5c25cd7dc

---
+++ tools/inCommit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd "$(git rev-parse --show-toplevel)"
+
+FILES_TO_CHECK=($(git diff --cached --name-only --diff-filter=ACM)
+
+if [[ "$FILES_TO_CHECK" = "" ]]; then
+  echo "No files to check."
+  exit 0
+fi
+
+echo "Checking ESLint for ${#FILES_TO_CHECK[@]} files..."
+
+$LINT_CMD "${FILES_TO_CHECK[@]}" --fix
+
+LINT_RESULT="$?"
+
+git add "${FILES_TO_CHECK[@]}"
+
+if [[ "${LINT_RESULT}" == 0 ]]; then
+  printf "Lint check \033[42mPASSED\033[0m\n"
+else
+  printf "Lint check \033[41mFAILED\033[0m Fix above errors\n"
+  exit 1
+fi
+
+exit $?
