52bb77c806858d835ea54ada9d4fe781ff13b096
Ljubomir Simin <ljubomir@coingcs.com>
2017-10-09 14:01:49 +0200

    Unit tests for confirmShipped.
    Former-commit-id: 6f97075777989763b8d071c45f5aa9c9a9307a72

--- test/unit/workers/confirmShipped/confirmShipped.spec.js
+++ test/unit/workers/confirmShipped/confirmShipped.spec.js
@@ -1,24 +1,96 @@
-var fs = require('fs');
-var util = require('util');
-var path = require('path');
-var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-var expect = chai.expect;
-var assert = chai.assert;
-chai.should();
-require('../../../bootstrap.test')
+const fs = require('fs');
+const chai = require("chai");
+const expect = chai.expect;
+const mockery = require('mockery');
+const modelgenerator = global.SixCRM.routes.include('test', 'model-generator.js');
 
-// Disabled for now. One of two errors, either:
-// "Error: Unexpected response from USPS"
-// or "TypeError: this.dynamodb.query is not a function" in lib/dynamodb-utilities.js
-// describe('workers/confirmShipped', function () {
-// 	describe('confirmShipped', function (done) {
-// 		it('will be SHIPPED', function() {
-//             var confirmShipped = require('../../../../controllers/workers/confirmShipped');
-//             var rebill = require('./fixtures/validRebill');
-// 			var actual = confirmShipped.confirmShipped(rebill);
-// 			return Promise.resolve(actual).should.eventually.equal("SHIPPED")
-// 		})
-// 	});
-// });
+require('../../../bootstrap.test');
+
+describe('controllers/workers/confirmShipped', function () {
+
+    before(() => {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnReplace: false,
+            warnOnUnregistered: false
+        });
+    });
+
+    let random_rebill;
+    let random_transactions;
+    let random_products;
+    let random_transaction_product;
+    let random_shipping_receipt;
+
+    beforeEach((done) => { Promise.all([
+        modelgenerator.randomEntityWithId('rebill').then(rebill => { random_rebill = rebill}),
+        modelgenerator.randomEntityWithId('shippingreceipt').then(shipping_receipt => { random_shipping_receipt = shipping_receipt}),
+        modelgenerator.randomEntityWithId('transaction').then(transaction => { random_transactions = [transaction]}),
+        modelgenerator.randomEntityWithId('product').then(product => { random_products = [{product: product},{product: product}]}),
+        modelgenerator.randomEntityWithId('transactionproduct').then(transaction_product => { random_transaction_product = transaction_product})
+    ]).then(() =>{
+            mockery.registerMock(global.SixCRM.routes.path('controllers', 'entities/Rebill.js'), {
+                listTransactions: (rebill) => {
+                    return Promise.resolve(random_transactions);
+                }
+            });
+
+            mockery.registerMock(global.SixCRM.routes.path('controllers', 'entities/Transaction.js'), {
+                getTransactionProduct: (transaction) => {
+                    return Promise.resolve(random_transaction_product);
+                },
+                get: (id) => {
+                    return Promise.resolve(random_products[0].product);
+                }
+            });
+
+            done();
+        }
+    )});
+
+    afterEach(() => {
+        mockery.resetCache();
+    });
+
+    after(() => {
+        mockery.deregisterAll();
+    });
+
+    describe('confirmShipped', () => {
+
+        it('returns success message when shipped', () => {
+
+            delete random_products[0].product.ship;
+
+            const confirmShipped = global.SixCRM.routes.include('controllers', 'workers/confirmShipped.js');
+
+            return confirmShipped.confirmShipped(random_rebill)
+                .then(result => expect(result).to.equal(confirmShipped.messages.shipped));
+        });
+
+        it('returns not ship message when missing shipping receipt', () => {
+
+            random_products[0].product.ship = 'true';
+
+            const confirmShipped = global.SixCRM.routes.include('controllers', 'workers/confirmShipped.js');
+
+            return confirmShipped.confirmShipped(random_rebill)
+                .then(result => expect(result).to.equal(confirmShipped.messages.notshipped));
+        });
+
+        it('returns ship message when shipping receipt is present', () => {
+
+            random_products[0].product.ship = 'true';
+            random_transaction_product.shippingreceipt = random_shipping_receipt;
+
+            const confirmShipped = global.SixCRM.routes.include('controllers', 'workers/confirmShipped.js');
+
+            return confirmShipped.confirmShipped(random_rebill)
+                .then(result => expect(result).to.equal(confirmShipped.messages.shipped));
+        });
+
+
+    });
+
+
+});
