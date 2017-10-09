b6d1574aab249f7a3e25fdc70b084b5b933857f5
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-10-09 13:22:10 -0700

    Adding deployment script updates
    Former-commit-id: db7f971d468d485aedbe012a2db2395f287dce37

--- tools/inCommit.sh
+++ tools/inCommit.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 cd "$(git rev-parse --show-toplevel)"
 
-FILES_TO_CHECK=($(git diff --cached --name-only --diff-filter=ACM)
+FILE_MATCH_REGULAR_EXPRESSION=$1
+FILES_TO_CHECK=$(git diff --cached --name-only --diff-filter=ACM | grep -e "$FILE_MATCH_REGULAR_EXPRESSION")
 
 if [[ "$FILES_TO_CHECK" = "" ]]; then
-  echo "No files to check."
-  exit 0
+  echo "No files to check.";
+  exit 0;
 fi
 
-echo "Checking ESLint for ${#FILES_TO_CHECK[@]} files..."
+echo "Updated files: ";
+echo  "$FILES_TO_CHECK";
 
-$LINT_CMD "${FILES_TO_CHECK[@]}" --fix
-
-LINT_RESULT="$?"
-
-git add "${FILES_TO_CHECK[@]}"
-
-if [[ "${LINT_RESULT}" == 0 ]]; then
-  printf "Lint check \033[42mPASSED\033[0m\n"
-else
-  printf "Lint check \033[41mFAILED\033[0m Fix above errors\n"
-  exit 1
-fi
-
-exit $?
+exit 1;
