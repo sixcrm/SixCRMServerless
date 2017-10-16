a68ba3f9767094590c707ed56907f1ddfe121eca
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-10-16 14:00:34 -0700

    Updating currency json, currency utilities.
    Former-commit-id: 5611af025fbaa3dae6ef5dc52641924bd0d95638

--- controllers/analytics/queries/merchant_provider_summary/transform.js
+++ controllers/analytics/queries/merchant_provider_summary/transform.js
@@ -1,4 +1,6 @@
 'use strict';
+const _ = require('underscore');
+
 let du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
 let paginationutilities = global.SixCRM.routes.include('lib', 'pagination-utilities.js');
 let currencyutilities = global.SixCRM.routes.include('lib', 'currency-utilities.js');
@@ -19,15 +21,15 @@ module.exports = function(results, parameters){
               summary: {
                 today: {
                   count: parseInt(result.num_transactions_today),
-                  amount: currencyutilities.toCurrency(result.amount_transactions_today)
+                  amount: currencyutilities.toCurrency(parseFloat(result.amount_transactions_today))
                 },
                 thisweek: {
                   count: parseInt(result.num_transactions_week),
-                  amount: currencyutilities.toCurrency(result.amount_transactions_week)
+                  amount: currencyutilities.toCurrency(parseFloat(result.amount_transactions_week))
                 },
                 thismonth: {
                   count: parseInt(result.num_transactions_month),
-                  amount: currencyutilities.toCurrency(result.amount_transactions_month)
+                  amount: currencyutilities.toCurrency(parseFloat(result.amount_transactions_month))
                 }
               }
           });
@@ -39,8 +41,6 @@ module.exports = function(results, parameters){
         let pagination_object = paginationutilities.createSQLPaginationObject(parameters);
 
         let return_object = {merchantproviders:return_array, pagination: pagination_object};
-
-        du.output(return_object);
 
         return resolve(return_object);
 
