715d9ca49a89f6ceceef0bcdf87b993342e7f58e
=Aldo Zelen <aldo.zelen@toptal.com>
2017-10-17 20:45:34 +0200

    Added Merchant Summary query
    Former-commit-id: a70667d7223501d36c0bf37400439fa8d87e92d6

---
+++ controllers/analytics/queries/merchant_report_summary/query.sql
@@ -0,0 +1,55 @@
+SELECT
+  coalesce(SUM(
+      CASE
+        WHEN processor_result = 'success' AND transaction_type = 'new' THEN 1
+        ELSE 0
+      END
+  ),0) AS sale_count,
+  coalesce(SUM(
+      CASE
+        WHEN processor_result = 'success' AND transaction_type = 'new' THEN amount
+        ELSE 0
+      END
+  ),0) AS sale_gross_revenue,
+  coalesce(SUM(
+      CASE
+        WHEN processor_result = 'success' AND transaction_type = 'refund' THEN amount
+        ELSE 0
+      END
+  ),0) AS refund_expenses,
+  coalesce(SUM(
+      CASE
+        WHEN processor_result = 'success' AND transaction_type = 'refund' THEN 1
+        ELSE 0
+      END
+  ),0) AS refund_count,
+  coalesce(SUM(
+      CASE
+        WHEN processor_result = 'success' AND transaction_type = 'new' THEN amount
+        ELSE 0
+      END
+  ),0) -
+  coalesce(SUM(
+      CASE
+        WHEN processor_result = 'success' AND transaction_type = 'refund' THEN amount
+        ELSE 0
+      END
+  ),0) AS net_Revenue,
+  coalesce(SUM(
+      CASE
+        WHEN processor_result = 'success' AND transaction_type = 'new' AND datetime >= DATE_TRUNC('Month',current_date) THEN 1
+        ELSE 0
+      END
+  ),0) AS mtd_sales_count,
+  coalesce(SUM(
+      CASE
+        WHEN processor_result = 'success' AND transaction_type = 'new' AND datetime >= DATE_TRUNC('Month',current_date) THEN amount
+        ELSE 0
+      END
+  ),0) AS mtd_gross_count
+FROM f_transactions
+WHERE 1
+  {{filter}}
+  AND datetime BETWEEN TIMESTAMP '{{start}}' AND TIMESTAMP '{{end}}'
+LIMIT {{limit}}
+OFFSET {{offset}};
