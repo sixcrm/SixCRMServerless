939c1780079dc8ff2a7098c7331704260dd51152
=Aldo Zelen <aldo.zelen@toptal.com>
2017-10-20 20:01:30 +0200

    Adding a subaffiliates overivew query
    Former-commit-id: 73dd94618799dedd9486bdbdb7976869cf8a55a9

---
+++ controllers/analytics/queries/reports/affiliate/affiliate_report_subaffiliates_overview/transform.js
@@ -0,0 +1,35 @@
+'use strict';
+let _ = require('underscore');
+let du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+let paginationutilities = global.SixCRM.routes.include('lib', 'pagination-utilities.js');
+
+module.exports = function(results, parameters){
+
+    du.debug('Transformation Function');
+
+    return new Promise((resolve, reject) => {
+
+        let result_array = [];
+
+        du.info(results);
+
+        results.forEach((result) => {
+
+            result_array.push({
+      			  subaffiliate: result.subaffiliate,
+              amount: result.affiliate
+            });
+
+        });
+
+        let return_object = {
+            count: result_array.length,
+            affiliates: result_array
+        };
+
+        return resolve(return_object);
+
+
+    });
+
+}
