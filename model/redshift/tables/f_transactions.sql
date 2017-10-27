c55c592293fa2c49280ba327743a11adfbb0865e
=Aldo Zelen <aldo.zelen@toptal.com>
2017-10-27 20:09:46 +0200

    Transactions work
    Former-commit-id: 1cee2b8572ec6ec42bf7ff3b5db76d54066c2925

--- model/redshift/tables/f_transactions.sql
+++ model/redshift/tables/f_transactions.sql
@@ -15,25 +15,28 @@ INSERT INTO sys_sixcrm.sys_table_version
 
 CREATE TABLE IF NOT EXISTS f_transactions
 (
-  id                  VARCHAR(128)   NOT NULL PRIMARY KEY encode ZSTD,
-  datetime            TIMESTAMP     NOT NULL encode ZSTD,
-  customer            VARCHAR(128)   NOT NULL encode ZSTD,
-  creditcard          VARCHAR(128)   NOT NULL encode ZSTD,
-  merchant_provider   VARCHAR(128)   NOT NULL encode ZSTD,
-  campaign            VARCHAR(128)   NOT NULL encode ZSTD,
-  affiliate           VARCHAR(128) encode ZSTD,
-  amount              DECIMAL(8, 2) NOT NULL encode ZSTD,
-  processor_result    VARCHAR(16)   NOT NULL encode ZSTD,
-  account             VARCHAR(128)   NOT NULL encode ZSTD,
-  transaction_type    VARCHAR(10)    NOT NULL encode ZSTD,
-  transaction_subtype VARCHAR(10)    NOT NULL encode ZSTD,
-  product_schedule    VARCHAR(36) encode ZSTD,
-  subaffiliate_1      VARCHAR(128) encode ZSTD,
-  subaffiliate_2      VARCHAR(128) encode ZSTD,
-  subaffiliate_3      VARCHAR(128) encode ZSTD,
-  subaffiliate_4      VARCHAR(128) encode ZSTD,
-  subaffiliate_5      VARCHAR(128) encode ZSTD,
-  prepaid             BOOLEAN encode ZSTD
-) INTERLEAVED SORTKEY (account, datetime);
+    id                  VARCHAR(128)  NOT NULL ENCODE ZSTD
+      CONSTRAINT f_transactions_pkey
+      PRIMARY KEY,
+    datetime            TIMESTAMP     NOT NULL ENCODE DELTA,
+    customer            VARCHAR(128)  NOT NULL ENCODE ZSTD,
+    creditcard          VARCHAR(128)  NOT NULL ENCODE ZSTD,
+    merchant_provider   VARCHAR(128)  NOT NULL ENCODE ZSTD,
+    campaign            VARCHAR(128)  NOT NULL ENCODE ZSTD,
+    affiliate           VARCHAR(128) ENCODE ZSTD,
+    amount              NUMERIC(8, 2) NOT NULL,
+    processor_result    VARCHAR(16)   NOT NULL ENCODE ZSTD,
+    account             VARCHAR(128)  NOT NULL ENCODE RUNLENGTH,
+    transaction_type    VARCHAR(10)   NOT NULL ENCODE TEXT255,
+    transaction_subtype VARCHAR(10)   NOT NULL ENCODE TEXT255,
+    product_schedule    VARCHAR(36)  ENCODE ZSTD,
+    subaffiliate_1      VARCHAR(128) ENCODE ZSTD,
+    subaffiliate_2      VARCHAR(128) ENCODE ZSTD,
+    subaffiliate_3      VARCHAR(128) ENCODE ZSTD,
+    subaffiliate_4      VARCHAR(128) ENCODE ZSTD,
+    subaffiliate_5      VARCHAR(128) ENCODE ZSTD,
+    prepaid             BOOLEAN      ENCODE ZSTD
+  )
+    INTERLEAVED SORTKEY (account, datetime);
 
 COMMENT ON TABLE f_transactions IS 'Main Fact table with information about transactions';
