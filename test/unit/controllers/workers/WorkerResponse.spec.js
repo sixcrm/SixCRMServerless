eea6c8ece48288a73b4e006011f6c7ffbc77276c
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-11-07 20:46:30 -0800

    Worker Response class test
    Former-commit-id: bef9d797ea52d4570adf74daf2e1eb353d906f72

---
+++ test/unit/controllers/workers/WorkerResponse.spec.js
@@ -0,0 +1,40 @@
+'use strict'
+const _ = require('underscore');
+const chai = require("chai");
+const uuidV4 = require('uuid/v4');
+const expect = chai.expect;
+const mockery = require('mockery');
+
+let WorkerResponseController = global.SixCRM.routes.include('controllers', 'workers/WorkerResponse.js');
+
+describe('controllers/workers/WorkerResponse.js', () => {
+
+  describe('constructor', () => {
+    it('successfully executes the constructor', () => {
+      let workerResponseController = new WorkerResponseController();
+
+      expect(workerResponseController).to.have.property('merged_response_types');
+      expect(workerResponseController.merged_response_types).to.have.property('noaction');
+      expect(workerResponseController.merged_response_types['noaction']).to.deep.equal({code: 'noaction'});
+    });
+  });
+
+  describe('setAdditionalInformation', () => {
+    it('successfully sets additional information', () => {
+      let workerResponseController = new WorkerResponseController();
+
+      workerResponseController.setAdditionalInformation('hi there');
+      expect(workerResponseController.additional_information).to.equal('hi there');
+    });
+  });
+
+  describe('getAdditionalInformation', () => {
+    it('successfully gets additional information', () => {
+      let workerResponseController = new WorkerResponseController();
+
+      workerResponseController.setAdditionalInformation('hi there');
+      expect(workerResponseController.getAdditionalInformation()).to.equal('hi there');
+    });
+  });
+
+});
