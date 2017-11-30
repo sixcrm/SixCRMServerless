faf0b2f7eb5151e8609356ccc58c9af2235a2164
Nikola Bosic <nikola.bosic@coingcs.com>
2017-11-30 15:28:53 +0100

    Fix pickRebill and unit tests
    Former-commit-id: f1be97bcf413f7c5d5cdceb65cad234e0d4e33d6

--- controllers/workers/pickRebill.js
+++ controllers/workers/pickRebill.js
@@ -1,8 +1,10 @@
 'use strict';
 const _ = require('underscore');
 
+const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
 const timestamp = global.SixCRM.routes.include('lib', 'timestamp.js');
 const workerController = global.SixCRM.routes.include('controllers', 'workers/components/worker.js');
+const arrayutilities = global.SixCRM.routes.include('lib', 'array-utilities.js');
 
 class pickRebillController extends workerController {
 
@@ -15,6 +17,7 @@ class pickRebillController extends workerController {
   execute(){
 
     du.debug('Execute');
+
     return this.acquireRebills()
     .then(() => this.markRebillsAsProcessing())
     .then(() => this.respond());
@@ -28,14 +31,14 @@ class pickRebillController extends workerController {
     let now = timestamp.createTimestampSeconds();
 
     if(!_.has(this, 'rebillController')){
-      let this.rebillController = global.SixCRM.routes.include('controllers', 'entities/Rebill.js');
+      this.rebillController = global.SixCRM.routes.include('controllers', 'entities/Rebill.js');
     }
 
     return this.rebillController.getRebillsAfterTimestamp(now).then(rebills => {
 
       this.parameters.set('rebills', rebills);
 
-      return true;
+      return Promise.resolve();
 
     });
 
@@ -48,34 +51,20 @@ class pickRebillController extends workerController {
     let rebills = this.parameters.get('rebills');
 
     if(!_.has(this, 'rebillController')){
-      let this.rebillController = global.SixCRM.routes.include('controllers', 'entities/Rebill.js');
+      this.rebillController = global.SixCRM.routes.include('controllers', 'entities/Rebill.js');
     }
 
-    return this.rebillController.markRebillAsProcessing({rebill: rebill, new_state: this.destination_queue}).then(rebill => {
+    const worker_promises = arrayutilities.map(rebills, (rebill) => {
+      return this.rebillController.markRebillAsProcessing(rebill)
+    });
 
-      return true;
+    return Promise.all(worker_promises).then(() => {
+
+      return Promise.resolve();
 
     });
 
   }
-
-
-  pickRebill(){
-
-
-
-    .then(() => this.markRebillsAsProcessing())
-    .then(() => this.respond());
-    return this.rebillController.getRebillsAfterTimestamp(now)
-    .then(() => {
-
-    })
-    .then((rebills) => {}Promise.all(rebills.map(rebill => rebillController.sendMessageAndMarkRebill(rebill))))
-    .then(() => true);
-
-  }
-
-
 
 }
 
--- test/unit/controllers/workers/pickRebill/pickRebill.spec.js
+++ test/unit/controllers/workers/pickRebill/pickRebill.spec.js
@@ -2,8 +2,24 @@ const fs = require('fs');
 const chai = require("chai");
 const expect = chai.expect;
 const mockery = require('mockery');
+const arrayutilities = global.SixCRM.routes.include('lib', 'array-utilities.js');
 
 require('../../../../bootstrap.test');
+
+function getValidRebill(){
+
+    return {
+        "id": "70de203e-f2fd-45d3-918b-460570338c9b",
+        "bill_at": "2017-04-06T18:40:41.405Z",
+        "account":"d3fa3bf3-7824-49f4-8261-87674482bf1c",
+        "parentsession": "1fc8a2ef-0db7-4c12-8ee9-fcb7bc6b075d",
+        "product_schedules": ["2200669e-5e49-4335-9995-9c02f041d91b"],
+        "amount": 79.99,
+        "created_at":"2017-04-06T18:40:41.405Z",
+        "updated_at":"2017-04-06T18:41:12.521Z"
+    };
+
+}
 
 describe('controllers/workers/pickRebill', function () {
 
@@ -15,7 +31,7 @@ describe('controllers/workers/pickRebill', function () {
         });
     });
 
-	describe('pickRebill', function () {
+	describe('execute', function () {
 
         afterEach(() => {
             mockery.resetCache();
@@ -25,45 +41,40 @@ describe('controllers/workers/pickRebill', function () {
             mockery.deregisterAll();
         });
 
-	    it('returns true', () => {
+	      it('does not process anything when no rebills are found', () => {
 
             mockery.registerMock(global.SixCRM.routes.path('controllers', 'entities/Rebill.js'), {
                 getRebillsAfterTimestamp: (time) => {
                     return Promise.resolve([]);
                 },
-                sendMessageAndMarkRebill: (rebill) => {
+                markRebillAsProcessing: (rebill) => {
+                    expect.fail();
                     return Promise.resolve();
                 }
             });
 
-			let pickRebill = global.SixCRM.routes.include('controllers', 'workers/pickRebill.js');
+			      let pickRebill = global.SixCRM.routes.include('controllers', 'workers/pickRebill.js');
 
-			return pickRebill.pickRebill().then(response => expect(response).to.be.true);
-		});
+			      return pickRebill.execute();
 
-        it('passes correct date to rebill controller', (done) => {
+		    });
 
-            // given
-            const now = 1487768599196;
-
-            mockery.registerMock(global.SixCRM.routes.path('lib', 'timestamp.js'), {
-                createTimestampSeconds: () => {
-                    return now
-                }
-            });
+        it('processes rebill', () => {
 
             mockery.registerMock(global.SixCRM.routes.path('controllers', 'entities/Rebill.js'), {
                 getRebillsAfterTimestamp: (time) => {
-                    // then
-                    expect(time).to.equal(now);
-                    done();
+                    return Promise.resolve([getValidRebill()]);
+                },
+                markRebillAsProcessing: (rebill) => {
+                    expect(rebill).to.deep.equal(getValidRebill());
+                    return Promise.resolve();
                 }
             });
 
-            let pickRebill = global.SixCRM.routes.include('controllers', 'workers/pickRebill.js');
+			      let pickRebill = global.SixCRM.routes.include('controllers', 'workers/pickRebill.js');
 
-            // when
-            pickRebill.pickRebill();
-        });
+			      return pickRebill.execute();
+
+		    });
 	});
 });
