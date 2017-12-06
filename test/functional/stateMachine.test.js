bb24ab1ff0debcb3f9011da4959e487d8210b8b9
Ljubomir Simin <ljubomir@coingcs.com>
2017-12-06 18:41:34 +0100

    Updated state machine test that actually reads and sends messages.
    Former-commit-id: d6d00d84907dc055a2eb788c2e905ef02e770970

--- test/functional/stateMachine.test.js
+++ test/functional/stateMachine.test.js
@@ -1,9 +1,11 @@
 const expect = require('chai').expect;
+const mockery = require('mockery');
 const SqSTestUtils = require('./sqs-test-utils');
 const TestUtils = require('./test-utils');
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+const arrayutilities = global.SixCRM.routes.include('lib', 'array-utilities.js');
 
-describe.only('stateMachine', function () {
+describe.only('stateMachine', () => {
     let lambdas = [];
     let lambda_names = [
         'billtohold',
@@ -16,12 +18,19 @@ describe.only('stateMachine', function () {
         'holdtoarchive',
         'rebilltoarchive',
         'recovertoarchive',
-        'pickrebillstobill'
+        // 'pickrebillstobill'
     ];
 
     before((done) => {
         process.env.require_local = true;
         process.env.stage = 'local';
+
+        mockery.enable({
+            useCleanCache: true,
+            warnOnReplace: false,
+            warnOnUnregistered: false
+        });
+
         TestUtils.setGlobalUser();
         TestUtils.setEnvironmentVariables();
         configureLambdas();
@@ -30,9 +39,9 @@ describe.only('stateMachine', function () {
         });
     });
 
-    describe.only('Flush', function () {
+    describe('Flush', () => {
 
-        it.only('should execute all lambdas', function () {
+        it('should execute all lambdas', () => {
 
             flushStateMachine()
                 .then(results => {
@@ -42,14 +51,67 @@ describe.only('stateMachine', function () {
 
     });
 
+    describe('Moving messages', () => {
+
+        it('should move a message from bill to hold', () => {
+
+            mockery.registerMock(global.SixCRM.routes.path('controllers', 'workers/processBilling.js'), {
+                execute: () => {
+                    const WorkerResponse = class {
+                        getCode() {
+                            return 'success';
+                        }
+                    };
+
+                    return Promise.resolve(new WorkerResponse());
+                }
+            });
+
+            let RebillHelperController = global.SixCRM.routes.include('helpers', 'entities/rebill/Rebill.js');
+            RebillHelperController.prototype.updateRebillState = () => {
+                return Promise.resolve();
+            };
+
+            return Promise.all([
+                SqSTestUtils.messageCountInQueue('bill'),
+                SqSTestUtils.messageCountInQueue('hold')])
+            .then((counts) => {
+                return expect(counts).to.deep.equal([0,0])})
+            .then(() => {
+                return SqSTestUtils.sendMessageToQueue('bill', JSON.stringify(getValidRebill()))})
+            .then(() => {
+                return SqSTestUtils.messageCountInQueue('bill')})
+            .then((count) => {
+                return expect(count).to.equal(1)})
+            .then(() => {
+                return flushStateMachine()})
+            .then(() => {
+                return Promise.all([
+                    SqSTestUtils.messageCountInQueue('bill'),
+                    SqSTestUtils.messageCountInQueue('hold')])})
+            .then((counts) => {
+                return expect(counts).to.deep.equal([0, 1])})
+
+        });
+
+    });
+
+
     function flushStateMachine() {
-        return Promise.all(lambdas.map(lambda => lambda(null, null, () => {})))
+        let all_function_executions = arrayutilities.map(lambdas, (lambda) => {
+            let function_name = Object.keys(lambda); // function is the first property of the handler
+
+            return lambda[function_name](null, null, () => {})
+        });
+
+        return Promise.all(all_function_executions);
     }
 
     function configureLambdas() {
         lambda_names.forEach((lambda_name) => {
             let lambda = global.SixCRM.configuration.serverless_config.functions[lambda_name];
-            let handler = lambda.handler;
+            let handler = lambda.handler.replace(/\.[^/.]+$/, '') ; // strip function name from path, i.e. 'handler.billtohold'
+
             let path = global.SixCRM.routes.root + '/' + handler;
 
             if (lambda.environment) {
@@ -60,6 +122,21 @@ describe.only('stateMachine', function () {
 
             lambdas.push(require(path));
         });
+    }
+
+    function getValidRebill(){
+
+        return {
+            "bill_at": "2017-04-06T18:40:41.405Z",
+            "id": "70de203e-f2fd-45d3-918b-460570338c9b",
+            "account":"d3fa3bf3-7824-49f4-8261-87674482bf1c",
+            "parentsession": "1fc8a2ef-0db7-4c12-8ee9-fcb7bc6b075d",
+            "product_schedules": ["2200669e-5e49-4335-9995-9c02f041d91b"],
+            "amount": 79.99,
+            "created_at":"2017-04-06T18:40:41.405Z",
+            "updated_at":"2017-04-06T18:41:12.521Z"
+        };
+
     }
 
 });
