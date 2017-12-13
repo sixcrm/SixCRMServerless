68728d76fa8671b1735e054bdfc8e81661029bdd
aldo.zelen@toptal.com <azelen@gmail.com>
2017-12-13 22:09:57 +0100

    Bug fix
    Former-commit-id: 93c1ff6a2a47a6567cd2ca38a57274ebd8335f28

--- deployment/redshift/seed_test_referential.js
+++ deployment/redshift/seed_test_referential.js
@@ -4,7 +4,7 @@ require('../../SixCRM.js');
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
 const redshiftSchemaDeployment = global.SixCRM.routes.include('deployment', 'utilities/redshift-schema-deployment.js');
 
-return redshiftSchemaDeployment.seed_referential().then((result) => {
+return redshiftSchemaDeployment.seed_test_referential().then((result) => {
   du.highlight(result);
 }).catch(error => {
   du.error(error);
