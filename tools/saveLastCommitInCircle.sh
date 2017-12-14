f0cc60ae070d1e9357807a2defbee3486f9261c5
Ljubomir Simin <ljubomir@coingcs.com>
2017-12-14 15:40:17 +0100

    Add debug info to script for saving last commit hash.
    Former-commit-id: aa2b786cbf65810dbdd17dfb80e990a71c91d8f7

--- tools/saveLastCommitInCircle.sh
+++ tools/saveLastCommitInCircle.sh
@@ -9,5 +9,11 @@ var_name="LAST_COMMIT_$CIRCLE_BRANCH"
 
 test -z $file && file="$HOME/.circlerc"
 
+echo "Curent commit is ${current_commit}"
+echo "Variable name is ${var_name}"
+echo "Saving to ${file}"
+
 touch $file
 cat $file | grep $var_name > /dev/null && sed -i "s/export $var_name=.*/export $var_name=$current_commit/" $file || echo "export $var_name=$current_commit" >> $file
+
+cat $file
