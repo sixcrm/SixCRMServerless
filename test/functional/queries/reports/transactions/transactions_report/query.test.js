e74e2786c8bba063b55e9119a31a42f00ef76413
aldo.zelen@toptal.com <azelen@gmail.com>
2017-12-14 21:31:34 +0100

    First successfull test on transaction query from docker image
    Former-commit-id: 820219a058d8bde1b13cc2033cfcdc57aed25af4

---
+++ test/functional/queries/reports/transactions/transactions_report/query.test.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+const analyticsController = global.SixCRM.routes.include('controllers', 'analytics/Analytics.js');
+const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+
+describe('Reports/Transactions/Transactions_report query', function () {
+
+    let query_input = { analyticsfilter:
+                        { start: '2017-01-01T00:00:00.000Z',
+                          end: '2017-01-13T00:00:00.000Z' },
+                          pagination: { limit: 10, order: 'asc', offset: 0 } };
+
+    it('Expect to return at least one row based on test seed data', () => {
+      global.account = '99999999-999e-44aa-999e-aaa9a99a9999';
+      global.user = 'admin.user@test.com';
+      expect(analyticsController.executeAnalyticsFunction(query_input, 'getTransactionSummaryReport')).to.exist;
+    });
+
+
+});
