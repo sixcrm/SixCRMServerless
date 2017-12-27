b78ec5f3e0b9af1cd08f4a9342921d2913bb5615
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2017-12-26 16:49:04 -0800

    Updating Shipping Receipt schema
    Former-commit-id: 1b35d06c3193f91bb2185c4312cedee03bb0a96a

--- handlers/endpoints/graph/schema/types/shippingreceipt/shippingReceiptInputType.js
+++ handlers/endpoints/graph/schema/types/shippingreceipt/shippingReceiptInputType.js
@@ -6,8 +6,9 @@ const GraphQLInputObjectType = require('graphql').GraphQLInputObjectType;
 module.exports.graphObj = new GraphQLInputObjectType({
     name: 'ShippingReceiptInputType',
     fields: () => ({
-        id:					{ type: new GraphQLNonNull(GraphQLString) },
-        fulfillment_provider: { type: new GraphQLNonNull(GraphQLString) },
-        trackingnumber:		{ type: GraphQLString }
+      id:					{ type: new GraphQLNonNull(GraphQLString) },
+      fulfillment_provider: { type: new GraphQLNonNull(GraphQLString) },
+      trackingnumber:		{ type: GraphQLString },
+      status: { type: new GraphQLNonNull(GraphQLString) }
     })
 });
