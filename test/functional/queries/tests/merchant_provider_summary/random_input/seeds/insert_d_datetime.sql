83f6d51f9580dec2c0233d638b119b5667684ffb
aldo.zelen@toptal.com <azelen@gmail.com>
2018-01-10 21:46:59 +0100

    Adding more tests for old queries
    Former-commit-id: 53302bb97a8235043f4db61023f7dff9630ca952

---
+++ test/functional/queries/tests/merchant_provider_summary/random_input/seeds/insert_d_datetime.sql
@@ -0,0 +1,6 @@
+INSERT INTO d_datetime(datetime)
+SELECT dd
+FROM generate_series( '2017-01-01'::timestamp, '2017-01-05'::timestamp, '1 second'::interval) dd
+EXCEPT
+SELECT datetime
+FROM d_datetime;
