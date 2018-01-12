ad5a1c638fcc70d6960979aa20dec88fc41f56a4
aldo.zelen@toptal.com <azelen@gmail.com>
2018-01-12 20:27:46 +0100

    Adding new f_product_schedules definition
    Former-commit-id: 24848ba4e5014344a699d78c25b696fb6843972f

--- model/redshift/tables/f_product_schedules.sql
+++ model/redshift/tables/f_product_schedules.sql
@@ -1,7 +1,7 @@
 /*
 05.07.2017 A.Zelen Logic from idempotent versioning
 
-TABLE_VERSION 1
+TABLE_VERSION 2
 */
 
 DROP TABLE IF EXISTS f_product_schedules;
@@ -9,16 +9,15 @@ DROP TABLE IF EXISTS f_product_schedules;
 DELETE FROM sys_sixcrm.sys_table_version WHERE table_name ='f_product_schedules';
 
 INSERT INTO sys_sixcrm.sys_table_version
-     SELECT 'f_product_schedules',1,getdate();
-
+     SELECT 'f_product_schedules',2,getdate();
 
 CREATE TABLE IF NOT EXISTS f_product_schedules
 (
-  session_id       VARCHAR(36) NOT NULL,
-  product_schedule VARCHAR(36) NOT NULL,
-  created_at       TIMESTAMP,
-  updated_at       TIMESTAMP
+  transactions_id  VARCHAR(36) ENCODE ZSTD,
+  product_schedule VARCHAR(36) NOT NULL ENCODE ZSTD,
+  datetime         TIMESTAMP   NOT NULL ENCODE DELTA
+)
+DISTSTYLE KEY DISTKEY (transactions_id);
 
-) DISTKEY (session_id);
-
-COMMENT ON TABLE f_product_schedules IS 'Child fact table build on different product schedules in data';
+COMMENT ON TABLE f_product_schedules IS 'Child fact table build on different product schedules in data tied to transactions'
+;
