864006e9154f4fc16f433cf389ee38c4b5ed990f
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-02-14 12:56:22 -0800

    Removing old test
    Former-commit-id: 579f68c2ea0e6ef39e3ce6352eab8f7fe894d68a

--- test/integration/transaction/checkout.test.js
+++ test/integration/transaction/checkout.test.js
@@ -1,162 +1,351 @@
-var request = require('supertest');
-var chai = require('chai');
-
-chai.use(require('chai-json-schema'));
-var assert = require('chai').assert
-var fs = require('fs');
-var yaml = require('js-yaml');
+'use strict'
+const _ = require('underscore');
+const chai = require('chai');
+const expect = chai.expect;
 
 const du = global.SixCRM.routes.include('lib','debug-utilities.js');
+const mvu = global.SixCRM.routes.include('lib','model-validator-utilities.js');
+const arrayutilities = global.SixCRM.routes.include('lib', 'array-utilities.js');
+const objectutilities = global.SixCRM.routes.include('lib', 'object-utilities.js');
+const httputilities = global.SixCRM.routes.include('lib', 'http-utilities.js');
 const random = global.SixCRM.routes.include('lib','random.js');
 const signatureutilities = global.SixCRM.routes.include('lib','signature.js');
 const tu = global.SixCRM.routes.include('lib','test-utilities.js');
 
-try {
-    var config = yaml.safeLoad(fs.readFileSync('./test/integration/config/'+process.env.stage+'.yml', 'utf8'));
-} catch (e) {
-    du.warning(e);
+const MockEntities = global.SixCRM.routes.include('test', 'mock-entities.js');
+
+function createSignature(){
+
+  let request_time = new Date().getTime();
+  let secret_key = config.access_keys.super_user.secret_key;
+  let access_key = config.access_keys.super_user.access_key;
+  let signature = signatureutilities.createSignature(secret_key, request_time);
+
+  return access_key+':'+request_time+':'+signature;
+
 }
 
-var endpoint = config.endpoint;
-var appropriate_spacing = '        ';
+function getValidAcquireTokenPostBody(campaign){
 
-describe('Checkout Test', function() {
-  describe('Confirms a sale in a single transaction', function() {
-    it('Returns a confirmed sale', function (done) {
+  let affiliates =  null;
 
-    	let request_time = new Date().getTime();
-    	let secret_key = config.access_keys.super_user.secret_key;
-    	let access_key = config.access_keys.super_user.access_key;
-    	let account = config.account;
-      var campaign_id = '70a6689a-5814-438b-b9fd-dd484d0812f9';
+  arrayutilities.map(['affiliate', 'subaffiliate1', 'subaffiliate2', 'subaffiliate3', 'subaffiliate4', 'subaffiliate5', 'cid'], (field) => {
+    if(random.randomBoolean()){
+      if(_.isNull(affiliates)){
+        affiliates = {};
+      }
+      affiliates[field] = random.createRandomString(20);
+    }
+  });
 
-      let signature = signatureutilities.createSignature(secret_key, request_time);
-      let authorization_string = access_key+':'+request_time+':'+signature;
-      let this_request = request(endpoint);
+  let return_object = {
+    campaign:(_.has(campaign, 'id'))?campaign.id:campaign
+  };
 
-      du.highlight('Request Time: ', request_time);
-      du.highlight('Signature: ', signature);
-      du.highlight('Authorization String: ', authorization_string);
-      du.highlight('Endpoint: ', endpoint);
-      du.output(appropriate_spacing+'Acquiring Token');
+  if(!_.isNull(affiliates)){
+    return_object.affiliates = affiliates;
+  }
 
-      let affiliate_id = random.createRandomString(10);
-      let subaffiliate_1_id = random.createRandomString(10);
-      let subaffiliate_2_id = random.createRandomString(10);
-      let subaffiliate_3_id = random.createRandomString(10);
-      let subaffiliate_4_id = random.createRandomString(10);
-      let subaffiliate_5_id = random.createRandomString(10);
-      let cid = random.createRandomString(10);
+  return return_object;
 
-      var post_body = {
-          "campaign":campaign_id,
-          "affiliates":{
-              "affiliate":affiliate_id,
-              "subaffiliate_1":subaffiliate_1_id,
-              "subaffiliate_2":subaffiliate_2_id,
-              "subaffiliate_3":subaffiliate_3_id,
-              "subaffiliate_4":subaffiliate_4_id,
-              "subaffiliate_5":subaffiliate_5_id,
-              "cid":cid
-          }
+}
+
+function checkout(token, post_body){
+
+  du.output('Checkout');
+  let account = config.account;
+
+  let argument_object = {
+    url: config.endpoint+'checkout/'+account,
+    body: post_body,
+    headers:{
+      Authorization: token
+    }
+  };
+
+  return httputilities.postJSON(argument_object)
+  .then((result) => {
+    du.debug(result.body);
+    expect(result.response.statusCode).to.equal(200);
+    expect(result.response.statusMessage).to.equal('OK');
+    expect(result.body).to.have.property('success');
+    expect(result.body).to.have.property('code');
+    expect(result.body).to.have.property('response');
+    expect(result.body.success).to.equal(true);
+    expect(result.body.code).to.equal(200);
+    return result.body;
+  });
+
+}
+
+function acquireToken(campaign){
+
+  du.output('Acquire Token');
+
+  let account = config.account;
+  let authorization_string = createSignature();
+  var post_body = getValidAcquireTokenPostBody(campaign);
+
+  let argument_object = {
+    url: config.endpoint+'token/acquire/'+account,
+    body: post_body,
+    headers:{
+      Authorization: authorization_string
+    }
+  };
+
+  return httputilities.postJSON(argument_object)
+  .then((result) => {
+    du.debug(result.body);
+    expect(result.response.statusCode).to.equal(200);
+    expect(result.response.statusMessage).to.equal('OK');
+    expect(result.body).to.have.property('success');
+    expect(result.body).to.have.property('code');
+    expect(result.body).to.have.property('response');
+    expect(result.body.success).to.equal(true);
+    expect(result.body.code).to.equal(200);
+    expect(_.isString(result.body.response)).to.equal(true);
+    let authorization_token = result.body.response;
+
+    return authorization_token;
+  });
+
+}
+
+function createCheckoutCustomer(){
+
+  let customer = MockEntities.getValidCustomer();
+
+  delete customer.id;
+  delete customer.account;
+  delete customer.created_at;
+  delete customer.updated_at;
+  delete customer.creditcards;
+  customer.billing = customer.address;
+
+  return customer;
+
+}
+
+function createCheckoutCreditCard(){
+
+  let creditcard = MockEntities.getValidTransactionCreditCard();
+
+  creditcard.number = "4111111111111111";
+
+  return creditcard;
+
+}
+
+function createCheckoutBody(campaign, sale_object){
+
+  let customer = createCheckoutCustomer();
+  let creditcard = createCheckoutCreditCard();
+
+  return objectutilities.merge(sale_object, {
+    campaign:(_.has(campaign, 'id'))?campaign.id:campaign,
+    customer:customer,
+    creditcard:creditcard
+  });
+
+}
+
+let config = global.SixCRM.routes.include('test', 'integration/config/'+process.env.stage+'.yml');
+let campaign = '70a6689a-5814-438b-b9fd-dd484d0812f9';
+
+describe('Checkout', () => {
+  describe('Product Schedules', () => {
+    it('returns a confirmed sale', () => {
+
+      let sale_object = {
+        product_schedules:[{
+          product_schedule: "12529a17-ac32-4e46-b05b-83862843055d",
+          quantity:2
+        }]
       };
 
-      du.debug('Post data', post_body);
+      return acquireToken(campaign)
+      .then((token) => {
+        expect(token).to.be.defined;
+        let checkout_body = createCheckoutBody(campaign, sale_object);
 
-    	this_request.post('token/acquire/'+account)
-      .send(post_body)
-			.set('Content-Type', 'application/json')
-			.set('Authorization', authorization_string)
-			.expect(200)
-			.expect('Content-Type', 'application/json')
-			.expect('Access-Control-Allow-Origin','*')
-			.expect('Access-Control-Allow-Methods', 'OPTIONS, POST')
-			.expect('Access-Control-Allow-Headers','Content-Type, X-Amz-Date,Authorization, X-Api-Key, X-Amz-Security-Token')
-			.end(function(err, response){
+        return checkout(token, checkout_body);
+      })
+      .then(result => {
+        let validated = mvu.validateModel(result, global.SixCRM.routes.path('model', 'endpoints/checkout/response.json'));
 
-        du.debug(response.body)
+        expect(validated).to.equal(true);
+      })
 
-        tu.assertSuccessfulResponse(response.body, 'graph');
-
-        var jwt = response.body.response;
-
-        var post_body = {
-					"product_schedules": [{
-						"product_schedule": "12529a17-ac32-4e46-b05b-83862843055d",
-						"quantity":2
-					}],
-          "campaign":campaign_id,
-          "affiliates":{
-            "affiliate":affiliate_id,
-            "subaffiliate_1":subaffiliate_1_id,
-            "subaffiliate_2":subaffiliate_2_id,
-            "subaffiliate_3":subaffiliate_3_id,
-            "subaffiliate_4":subaffiliate_4_id,
-            "subaffiliate_5":subaffiliate_5_id
-          },
-          "customer":{
-            "firstname":"Rama",
-            "lastname":"Damunaste",
-            "email":"rama@damunaste2.com",
-            "phone":"1234567890",
-            "billing":{
-              "line1":"10 Downing St.",
-              "city":"Detroit",
-              "state":"MI",
-              "zip":"12345",
-              "country":"US"
-            },
-            "address":{
-              "line1":"334 Lombard St.",
-              "line2":"Apartment 2",
-              "city":"Portland",
-              "state":"OR",
-              "zip":"97203",
-              "country":"US"
-            }
-          },
-          "creditcard":{
-      			"number": "4111111111111111",
-      			"expiration": "1024",
-      			"ccv": "888",
-      			"name": "Rama Damunaste",
-      			"address": {
-      				"line1": "10 Different Skid Rw.",
-      				"line2": "Suite 101",
-      				"city": "Portland",
-      				"state": "OR",
-      				"zip": "97203",
-      			 	"country": "US"
-      			}
-          }
-        };
-
-        du.info(endpoint+'checkout/'+account, post_body, jwt);
-
-        this_request.post('checkout/'+account)
-				.send(post_body)
-				.set('Content-Type', 'application/json')
-				.set('Authorization', jwt)
-				.expect(200)
-				.expect('Content-Type', 'application/json')
-				.expect('Access-Control-Allow-Origin','*')
-				.expect('Access-Control-Allow-Methods', 'OPTIONS,POST')
-				.expect('Access-Control-Allow-Headers','Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token')
-				.end(function(err, response){
-          du.debug(response.body)
-          du.debug('Confirming results', response.body);
-          assert.property(response.body, "success");
-          assert.equal(response.body.success, true);
-          assert.property(response.body, "response");
-          assert.property(response.body.response, "session");
-          assert.property(response.body.response, "customer");
-          assert.property(response.body.response, "transactions");
-          assert.property(response.body.response, "transaction_products");
-          //assert.equal(response.body.response.transactions.length, 2);
-          //assert.equal(response.body.response.transaction_products.length, 2);
-          done();
-        }, done);
-      }, done);
     });
   });
+
+  describe('Straight Sale', () => {
+    it('returns a confirmed sale', () => {
+
+      let sale_object = {
+        products:[{
+          product: "668ad918-0d09-4116-a6fe-0e7a9eda36f8",
+          quantity:2
+        }]
+      };
+
+      return acquireToken(campaign)
+      .then((token) => {
+        expect(token).to.be.defined;
+        let checkout_body = createCheckoutBody(campaign, sale_object);
+
+        return checkout(token, checkout_body);
+      })
+      .then(result => {
+        let validated = mvu.validateModel(result, global.SixCRM.routes.path('model', 'endpoints/checkout/response.json'));
+
+        expect(validated).to.equal(true);
+      });
+
+    });
+  });
+
+  describe('Straight Sale With Dynamic Price', () => {
+    it('returns a confirmed sale', () => {
+
+      let sale_object = {
+        products:[{
+          product: "668ad918-0d09-4116-a6fe-0e7a9eda36f8",
+          quantity:2,
+          price: 12.99
+        }]
+      };
+
+      return acquireToken(campaign)
+      .then((token) => {
+        expect(token).to.be.defined;
+        let checkout_body = createCheckoutBody(campaign, sale_object);
+
+        return checkout(token, checkout_body);
+      })
+      .then(result => {
+        let validated = mvu.validateModel(result, global.SixCRM.routes.path('model', 'endpoints/checkout/response.json'));
+
+        expect(validated).to.equal(true);
+      });
+
+    });
+  });
+
+  describe('Mixed Sale', () => {
+    it('returns a confirmed sale', () => {
+
+      let sale_object = {
+        product_schedules:[{
+          product_schedule: "12529a17-ac32-4e46-b05b-83862843055d",
+          quantity:2
+        }],
+        products:[{
+          product: "668ad918-0d09-4116-a6fe-0e7a9eda36f8",
+          quantity:2,
+          price: 12.99
+        }]
+      };
+
+      return acquireToken(campaign)
+      .then((token) => {
+        expect(token).to.be.defined;
+        let checkout_body = createCheckoutBody(campaign, sale_object);
+
+        return checkout(token, checkout_body);
+      })
+      .then(result => {
+        let validated = mvu.validateModel(result, global.SixCRM.routes.path('model', 'endpoints/checkout/response.json'));
+
+        expect(validated).to.equal(true);
+      });
+
+    });
+
+  });
+
+  describe('Dynamic Product Schedule', () => {
+    it('returns a confirmed sale', () => {
+
+      let sale_object = {
+        product_schedules:[{
+          product_schedule: {
+            schedule:[
+              {
+                product: {
+                  id: "668ad918-0d09-4116-a6fe-0e7a9eda36f8",
+                  name: "Dynamic Watermark Product"
+                },
+                start: 0,
+                period: 30,
+                price: 12.49
+              }
+            ]
+          },
+          quantity:1
+        }]
+      };
+
+      return acquireToken(campaign)
+      .then((token) => {
+        expect(token).to.be.defined;
+        let checkout_body = createCheckoutBody(campaign, sale_object);
+
+        return checkout(token, checkout_body);
+      })
+      .then(result => {
+        let validated = mvu.validateModel(result, global.SixCRM.routes.path('model', 'endpoints/checkout/response.json'));
+
+        expect(validated).to.equal(true);
+      });
+
+    });
+
+  });
+
+  describe('Mixed Sale with Dynamic Product Schedule', () => {
+    it('returns a confirmed sale', () => {
+
+      let sale_object = {
+        product_schedules:[{
+          product_schedule: {
+            schedule:[
+              {
+                product: {
+                  id: "668ad918-0d09-4116-a6fe-0e7a9eda36f8",
+                  name: "Dynamic Watermark Product"
+                },
+                start: 0,
+                period: 30,
+                price: 12.49
+              }
+            ]
+          },
+          quantity:1
+        }],
+        products:[{
+          product: "668ad918-0d09-4116-a6fe-0e7a9eda36f8",
+          quantity:2,
+          price: 12.99
+        }]
+      };
+
+      return acquireToken(campaign)
+      .then((token) => {
+        expect(token).to.be.defined;
+        let checkout_body = createCheckoutBody(campaign, sale_object);
+
+        return checkout(token, checkout_body);
+      })
+      .then(result => {
+        let validated = mvu.validateModel(result, global.SixCRM.routes.path('model', 'endpoints/checkout/response.json'));
+
+        expect(validated).to.equal(true);
+      });
+
+    });
+
+  });
+
 });
