d16f505448da5b0d69fd04a6cb4346c2c84b94b9
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-02-26 17:41:26 -0800

    Functional RDS Deployment
    Former-commit-id: 83007b0946b994673b89d1a3d32bbbb2e87477b3

--- deployment/utilities/rds-cluster-deployment.js
+++ deployment/utilities/rds-cluster-deployment.js
@@ -3,10 +3,8 @@
 const _ = require('underscore');
 
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
-const eu = global.SixCRM.routes.include('lib', 'error-utilities.js');
 const arrayutilities = global.SixCRM.routes.include('lib', 'array-utilities.js');
 const objectutilities = global.SixCRM.routes.include('lib', 'object-utilities.js');
-const stringutilities = global.SixCRM.routes.include('lib', 'string-utilities.js');
 const parserutilities = global.SixCRM.routes.include('lib', 'parser-utilities.js');
 const fileutilities = global.SixCRM.routes.include('lib','file-utilities.js');
 
@@ -72,8 +70,66 @@ class RDSClusterDeployment {
     du.debug('Deploy Cluster');
 
     return this.getParameterConfigurationFromFile()
+    .then((parameters) => this.parseParameters(parameters))
     .then((parameters) => {
-      return this.rdsutilities.putCluster(parameters);
+      return this.rdsutilities.putCluster(parameters).then(result => {
+        du.info(result);
+        return parameters;
+      });
+    })
+    .then((parameters) => this.deployClusterInstances(parameters))
+    .then(() => {
+      return 'Complete';
+    });
+
+  }
+
+  parseParameters(parameters){
+
+    du.debug('Parse Parameters');
+
+    let parser_data = {
+      region: global.SixCRM.configuration.site_config.aws.region,
+      stage:global.SixCRM.configuration.stage,
+      account: global.SixCRM.configuration.site_config.aws.account
+    };
+
+    objectutilities.map(parameters, key => {
+      if(_.isString(parameters[key])){
+        parameters[key] = parserutilities.parse(parameters[key], parser_data);
+      }
+      if(_.isArray(parameters[key]) && arrayutilities.nonEmpty(parameters[key])){
+        parameters[key] = arrayutilities.map(parameters[key], a_parameter_value => {
+          if(_.isString(a_parameter_value)){
+            return parserutilities.parse(a_parameter_value, parser_data);
+          }
+          return a_parameter_value;
+        });
+      }
+    });
+
+    return parameters;
+
+  }
+
+  deployClusterInstances(parameters){
+
+    du.debug('Deploy Cluster Instances');
+
+    let instances = parameters.Instances;
+
+    let instance_deployment_promises = arrayutilities.map(instances, instance => {
+
+      let instance_parameters = this.parseParameters(instance);
+
+      return this.rdsutilities.putDBInstance(instance_parameters);
+
+    });
+
+    return Promise.all(instance_deployment_promises).then((instance_deployment_promises) => {
+
+      return instance_deployment_promises;
+
     });
 
   }
