cdf19f60704ea099225293c53c44a96e11d03389
Nick Acker <acker2@me.com>
2018-02-28 16:09:16 -0800

    Role account-bound integration tests
    Former-commit-id: d27ff56ee2e925a74816bfda737282c2bc99a11c

--- test/integration/graph/entities/roles.test.js
+++ test/integration/graph/entities/roles.test.js
@@ -1,11 +1,8 @@
 const request = require('supertest');
 const chai = require('chai');
 const assert = require('chai').assert
-const fs = require('fs');
-const yaml = require('js-yaml');
 
 const tu = global.SixCRM.routes.include('lib','test-utilities.js');
-const du = global.SixCRM.routes.include('lib','debug-utilities.js');
 
 chai.use(require('chai-json-schema'));
 
@@ -37,40 +34,74 @@ let this_request = request(endpoint);
 
 describe('Graph '+entity+' Test', function() {
 
-  	global.test_accounts.forEach((test_account) => {
+    global.test_accounts.forEach((test_account) => {
 
-  		global.test_users.forEach((test_user) => {
+        global.test_users.forEach((test_user) => {
 
-      describe('Test the graph '+entity+' endpoint using "'+test_user.name+'" credentials on the account "'+test_account.name+'"', function() {
+            describe('Test the graph '+entity+' endpoint using "'+test_user.name+'" credentials on the account "'+test_account.name+'"', function() {
 
-          let test_jwt = tu.createTestAuth0JWT(test_user.email, global.SixCRM.configuration.site_config.jwt.site.secret_key);
+                let test_jwt = tu.createTestAuth0JWT(test_user.email, global.SixCRM.configuration.site_config.jwt.site.secret_key);
 
-          tests.forEach((test) => {
+                tests.forEach((test) => {
 
-					//let account = tu.getAccount(test.query);
-              let account = test_account.id;
+                    //let account = tu.getAccount(test.query);
+                    let account = test_account.id;
 
-              it('Should return only '+test_user.name+' fields for '+entity+' '+test.name+'.', function (done) {
-                  var query = tu.getQuery(test.query);
+                    it('Should return only '+test_user.name+' fields for '+entity+' '+test.name+'.', function (done) {
+                        var query = tu.getQuery(test.query);
 
-                  this_request.post('graph/'+account)
-							.set('Authorization', test_jwt)
-							.send(query)
-							.expect(200)
-							.expect('Content-Type', 'application/json')
-							.expect('Access-Control-Allow-Origin','*')
-							.expect('Access-Control-Allow-Methods', 'OPTIONS,POST')
-							.expect('Access-Control-Allow-Headers','Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token')
-							.end(function(err, response){
-    tu.assertResultSetAsync(response, test_user.role,test.name, done);
-});
-              });
-          });
+                        this_request.post('graph/'+account)
+                        .set('Authorization', test_jwt)
+                        .send(query)
+                        .expect(200)
+                        .expect('Content-Type', 'application/json')
+                        .expect('Access-Control-Allow-Origin','*')
+                        .expect('Access-Control-Allow-Methods', 'OPTIONS,POST')
+                        .expect('Access-Control-Allow-Headers','Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token')
+                        .end(function(err, response){
+                            tu.assertResultSetAsync(response, test_user.role,test.name, done);
+                        });
+                    });
+                });
 
-      });
+            });
 
-  });
+        });
 
-  });
+    });
+
+    it('Test role index endpoint should return account-bound roles', done => {
+        let account = global.test_accounts[0];
+        let test_user = global.test_users[0];
+        let test_jwt = tu.createTestAuth0JWT(test_user.email, global.SixCRM.configuration.site_config.jwt.site.secret_key);
+
+        let query = 'query { rolelist(pagination:{limit: \"10\"}) { roles { id } }';
+
+        this_request.post('graph/'+account)
+        .set('Authorization', test_jwt)
+        .send(query)
+        .expect(200)
+        .end((error, response) => {
+            assert.notInclude(response.body.data.rolelist.roles, { id: "e09ac44b-6cde-4572-8162-609f6f0aeca8" });
+            done();
+        });
+    });
+
+    it('Test role view endpoint should return only account-bound roles', done => {
+        let account = global.test_accounts[0];
+        let test_user = global.test_users[0];
+        let test_jwt = tu.createTestAuth0JWT(test_user.email, global.SixCRM.configuration.site_config.jwt.site.secret_key);
+
+        let query = 'query { role(id: "e09ac44b-6cde-4572-8162-609f6f0aeca8") { id } }';
+
+        this_request.post('graph/'+account)
+        .set('Authorization', test_jwt)
+        .send(query)
+        .expect(200)
+        .end((error, response) => {
+            assert.isNull(response.body.data.role);
+            done();
+        });
+    });
 
 });
