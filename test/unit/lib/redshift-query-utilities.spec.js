167bcf628bd6ca58a5a900697b089b62216ffa86
Alexander Makeev <alexander.v.makeev@gmail.com>
2018-03-01 13:43:09 +0500

    Redshift-query-utilities tests repaired.
    Former-commit-id: 1eb89d2aaa37244fef0e471f1e63d0e6865c48df

--- test/unit/lib/redshift-query-utilities.spec.js
+++ test/unit/lib/redshift-query-utilities.spec.js
@@ -1,246 +1,221 @@
 const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe('lib/redshift-query-utilities', () => {
 
-    describe('transformResult', () => {
+  describe('transformResult', () => {
 
-        it('successfully transforms result', () => {
+    it('successfully transforms result', () => {
 
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
 
-            expect(redshiftQueryUtilities.transformResult([{a_result: 'any_data'}]))
-                .to.deep.equal([{a_result: 'any_data'}]);
-        });
-
-        it('returns data from rows when result has rows property', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            //result has rows property
-            expect(redshiftQueryUtilities.transformResult({rows: 'any_data'}))
-                .to.equal('any_data');
-        });
-
-        it('throws error when rows property is not set', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            //result is missing rows property
-            try {
-                redshiftQueryUtilities.transformResult({not_rows: 'any_data'})
-            }catch(error){
-                expect(error.message).to.equal('[500] Result does not have rows property');
-            }
-        });
-
-        it('throws error when result is unrecognized return type', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            //result is missing rows property
-            try {
-                redshiftQueryUtilities.transformResult('any_data')
-            }catch(error){
-                expect(error.message).to.equal('[500] Unrecognized return type');
-            }
-        });
+      expect(redshiftQueryUtilities.transformResult([{a_result: 'any_data'}]))
+        .to.deep.equal([{a_result: 'any_data'}]);
     });
 
-    describe('queryRaw', () => {
+    it('returns data from rows when result has rows property', () => {
 
-        it('throws error when redshift connection is not set', () => {
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
 
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            delete redshiftQueryUtilities.redshift_connection;
-
-            return redshiftQueryUtilities.queryRaw().catch((error) => {
-                expect(error.message).to.equal('[500] Unset redshift_connection.');
-            });
-        });
-
-        it('throws error when redshift query wasn\'t successfull', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            return redshiftQueryUtilities.queryRaw().catch((error) => {
-                expect(error.message).to.equal('[500] Unable to query redshift.');
-            });
-        });
-
-        it('throws error from redshift connection query', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            redshiftQueryUtilities.redshift_connection = {
-                query: (query, params, callback) => {
-                    callback('fail', null);
-                }
-            };
-
-            return redshiftQueryUtilities.queryRaw('a_query', 'any_parameters').catch((error) => {
-                expect(error.message).to.equal('[500] fail');
-            });
-        });
-
-        it('returns data from redshift connection query', () => {
-
-            let data = [{a_result: 'any_data'}];
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            redshiftQueryUtilities.redshift_connection = {
-                query: (query, params, callback) => {
-                    callback(null, data);
-                }
-            };
-
-            return redshiftQueryUtilities.queryRaw('a_query', 'any_parameters').then((result) => {
-                expect(result).to.deep.equal(data);
-            });
-        });
+      //result has rows property
+      expect(redshiftQueryUtilities.transformResult({rows: 'any_data'}))
+        .to.equal('any_data');
     });
 
-    describe('openConnection', () => {
+    it('throws error when rows property is not set', () => {
 
-        it('throws error when redshift connection is not set', () => {
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
 
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            delete redshiftQueryUtilities.redshift_connection;
-
-            return redshiftQueryUtilities.openConnection().catch((error) => {
-                expect(error.message).to.equal('[500] Unset redshift_connection.');
-            });
-        });
-
-        it('throws error when redshift connection wasn\'t successfull', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            return redshiftQueryUtilities.openConnection().catch((error) => {
-                expect(error.message).to.equal('[500] redshift_connection.connect is not a function.');
-            });
-        });
-
-        it('throws error from redshift connection connect', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            redshiftQueryUtilities.redshift_connection = {
-                connect: (callback) => {
-                    callback('fail');
-                }
-            };
-
-            return redshiftQueryUtilities.openConnection().catch((error) => {
-                expect(error.message).to.equal('[500] fail');
-            });
-        });
-
-        it('returns true when connected successfully', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            redshiftQueryUtilities.redshift_connection = {
-                connect: (callback) => {
-                    callback(null);
-                }
-            };
-
-            return redshiftQueryUtilities.openConnection().then((result) => {
-                expect(result).to.be.true
-            });
-        });
+      //result is missing rows property
+      expect(redshiftQueryUtilities.transformResult({not_rows: 'any_data'})).to.be.rejectedWith('[500] Result does not have rows property');
     });
 
-    describe('closeConnection', () => {
+    it('throws error when result is unrecognized return type', () => {
 
-        it('throws error when redshift connection is not set', () => {
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
 
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+      //result is missing rows property
+      expect(redshiftQueryUtilities.transformResult('any_data')).to.be.rejectedWith('[500] Unrecognized return type');
+    });
+  });
 
-            delete redshiftQueryUtilities.redshift_connection;
+  describe('queryRaw', () => {
 
-            return redshiftQueryUtilities.closeConnection().catch((error) => {
-                expect(error.message).to.equal('[500] Unset redshift_connection.');
-            });
-        });
+    it('throws error when redshift connection is not set', () => {
 
-        it('throws error when closing redshift connection wasn\'t successfull', () => {
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
 
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+      delete redshiftQueryUtilities.redshift_connection;
 
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            return redshiftQueryUtilities.closeConnection().catch((error) => {
-                expect(error.message).to.equal('[500] redshift_connection.end is not a function.');
-            });
-        });
-
-        it('throws error from redshift connection end', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            redshiftQueryUtilities.redshift_connection = {
-                end: (callback) => {
-                    callback('fail');
-                }
-            };
-
-            return redshiftQueryUtilities.closeConnection().catch((error) => {
-                expect(error).to.equal('fail');
-            });
-        });
-
-        it('returns true when connection was closed successfully', () => {
-
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
-
-            redshiftQueryUtilities.redshift_connection = 'a_connection';
-
-            redshiftQueryUtilities.redshift_connection = {
-                end: (callback) => {
-                    callback(null);
-                }
-            };
-
-            return redshiftQueryUtilities.closeConnection().then((result) => {
-                expect(result).to.be.true
-            });
-        });
+      return redshiftQueryUtilities.queryRaw().catch((error) => {
+        expect(error.message).to.equal('[500] Unset db_client.');
+      });
     });
 
-    describe('instantiateRedshift', () => {
+    it('throws error when redshift query wasn\'t successfull', () => {
 
-        it('throws error if redshift configuration parameter is missing', () => {
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
 
-            const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+      redshiftQueryUtilities.db_client = 'a_connection';
 
-            redshiftQueryUtilities.required_configuration = ['a_param'];
-
-            delete redshiftQueryUtilities.redshift_configuration;
-
-            try {
-                redshiftQueryUtilities.instantiateRedshift()
-            }catch(error){
-                expect(error.message).to.equal('[500] Redshift connection errors: Missing Redshift configuration parameter: a_param');
-            }
-        });
+      return redshiftQueryUtilities.queryRaw().catch((error) => {
+        expect(error.message).to.equal('[500] Unable to query redshift.');
+      });
     });
+
+    it('throws error from redshift connection query', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = {
+        query: (query, params) => {
+          return Promise.reject(new Error('fail'));
+        }
+      };
+
+      return redshiftQueryUtilities.queryRaw('a_query', 'any_parameters').catch((error) => {
+        expect(error.message).to.equal('fail');
+      });
+    });
+
+    it('returns data from redshift connection query', () => {
+
+      let data = [{a_result: 'any_data'}];
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = {
+        query: (query, params) => {
+          return Promise.resolve(data);
+        }
+      };
+
+      return redshiftQueryUtilities.queryRaw('a_query', 'any_parameters').then((result) => {
+        expect(result).to.deep.equal(data);
+      });
+    });
+  });
+
+  describe('getConnection', () => {
+
+    it('throws error when redshift connection is not set', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = null;
+      delete redshiftQueryUtilities.pg;
+
+      return expect(redshiftQueryUtilities.getConnection()).to.be.rejectedWith('[500] Unset pg.');
+    });
+
+    it('throws error when redshift connection wasn\'t successfull', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = null;
+      redshiftQueryUtilities.pg = {
+        Client: function () {
+        }
+      };
+
+      return expect(redshiftQueryUtilities.getConnection()).to.be.rejectedWith('[500] db.connect is not a function.');
+    });
+
+    it('throws error from redshift connection connect', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = null;
+      redshiftQueryUtilities.pg = {
+        Client: function () {
+          this.connect = () => Promise.reject(new Error('Fail'));
+        }
+      };
+
+      return expect(redshiftQueryUtilities.getConnection()).to.be.rejectedWith('Fail');
+    });
+
+    it('returns true when connected successfully', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = null;
+      redshiftQueryUtilities.pg = {
+        Client: function () {
+          this.ok = 1;
+          this.connect = () => Promise.resolve();
+        }
+      };
+
+      return expect(redshiftQueryUtilities.getConnection()).eventually.to.have.property('ok');
+    });
+  });
+
+  describe('closeConnection', () => {
+
+    it('throws error when redshift connection is not set', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      delete redshiftQueryUtilities.db_client;
+
+      return redshiftQueryUtilities.closeConnection().catch((error) => {
+        expect(error.message).to.equal('[500] Unset db_client.');
+      });
+    });
+
+    it('throws error when closing redshift connection wasn\'t successfull', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = 'a_connection';
+
+      return redshiftQueryUtilities.closeConnection().catch((error) => {
+        expect(error.message).to.equal('[500] db_client.end is not a function.');
+      });
+    });
+
+    it('throws error from redshift connection end', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = {
+        end: () => {
+          return Promise.reject('fail');
+        }
+      };
+
+      return expect(redshiftQueryUtilities.closeConnection()).to.be.rejectedWith('fail');
+    });
+
+    it('returns true when connection was closed successfully', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      redshiftQueryUtilities.db_client = {
+        end: () => {
+          return Promise.resolve(true);
+        }
+      };
+
+      return expect(redshiftQueryUtilities.closeConnection()).eventually.to.be.equal(true);
+    });
+  });
+
+  describe('getDBConfig', () => {
+
+    it('throws error if redshift configuration parameter is missing', () => {
+
+      const redshiftQueryUtilities = global.SixCRM.routes.include('lib', 'redshift-query-utilities.js');
+
+      delete redshiftQueryUtilities.db_config;
+
+      redshiftQueryUtilities.required_configuration = ['a_param'];
+
+      expect(redshiftQueryUtilities.getDBConfig()).to.be.rejectedWith('[500] Redshift connection errors: Missing Redshift configuration parameter: a_param');
+    });
+  });
 });
