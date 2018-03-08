7e102fec34efd193508d8fb0ab25bfa513b88095
jadurham909 <justin@adexchangegrp.com>
2018-03-08 17:08:30 -0500

    Add aurora connectivity tests
    Former-commit-id: 31855aee78ba4c459e148696e375c38419d44314

--- deployment/utilities/rds-cluster-deployment.js
+++ deployment/utilities/rds-cluster-deployment.js
@@ -1,67 +1,18 @@
 'use strict';
 
 const _ = require('underscore');
-
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+const eu = global.SixCRM.routes.include('lib', 'error-utilities.js');
 const arrayutilities = global.SixCRM.routes.include('lib', 'array-utilities.js');
 const objectutilities = global.SixCRM.routes.include('lib', 'object-utilities.js');
 const parserutilities = global.SixCRM.routes.include('lib', 'parser-utilities.js');
-const fileutilities = global.SixCRM.routes.include('lib','file-utilities.js');
+const fileutilities = global.SixCRM.routes.include('lib', 'file-utilities.js');
 
 class RDSClusterDeployment {
 
   constructor() {
 
-    //super();
-
-    this.rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
-
-  }
-
-
-  createClusterAndWait() {
-
-    du.debug('Create Cluster and Wait');
-
-    let parameters = this.createParametersObject('create');
-
-    return this.appendSecurityGroupIDs(parameters).then((parameters) => {
-
-      return this.redshiftutilities.createCluster(parameters).then(() => {
-
-        parameters = this.createParametersObject('wait');
-
-        return this.redshiftutilities.waitForCluster('clusterAvailable', parameters).then((data) => {
-
-          return this.writeHostConfiguration(data).then(() => {
-
-            return data;
-
-          });
-
-        })
-
-      });
-
-    });
-
-  }
-
-  getParameterConfigurationFromFile(){
-
-    du.debug('Get Parameter Configuration From File.');
-
-    return fileutilities.getDirectoryFiles(global.SixCRM.routes.path('deployment','rds/clusters/')).then(directory_files => {
-
-      let dpcf = arrayutilities.find(directory_files, (directory_file) => {
-        return (directory_file == global.SixCRM.configuration.stage+'.json');
-      });
-
-      dpcf = (_.isUndefined(dpcf) || _.isNull(dpcf))?'default.json':dpcf;
-
-      return global.SixCRM.routes.include('deployment', 'rds/clusters/'+dpcf);
-
-    });
+    this._rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
 
   }
 
@@ -69,38 +20,69 @@ class RDSClusterDeployment {
 
     du.debug('Deploy Cluster');
 
-    return this.getParameterConfigurationFromFile()
-    .then((parameters) => this.parseParameters(parameters))
-    .then((parameters) => {
-      return this.rdsutilities.putCluster(parameters).then(result => {
-        du.info(result);
-        return parameters;
+    return this._getParameterConfigurationFromFile()
+      .then((parameters) => this._parseParameters(parameters))
+      .then((parameters) => {
+        return this._rdsutilities.putCluster(parameters).then(data => {
+
+          du.info(data);
+
+          if (!objectutilities.hasRecursive(data, 'DBClusters.0.Endpoint')) {
+
+            eu.throwError('server', 'Data object does not contain appropriate key: Clusters.0.Endpoint.Address');
+
+          }
+
+          global.SixCRM.configuration.setEnvironmentConfig('aurora.host', data.DBClusters[0].Endpoint);
+
+          console.log(global.SixCRM.configuration.site_config.aurora);
+
+          return parameters;
+
+        });
+      })
+      .then((parameters) => this._deployClusterInstances(parameters))
+      .then(() => {
+        return 'Complete';
       });
-    })
-    .then((parameters) => this.deployClusterInstances(parameters))
-    .then(() => {
-      return 'Complete';
+
+  }
+
+  _getParameterConfigurationFromFile() {
+
+    du.debug('Get Parameter Configuration From File.');
+
+    return fileutilities.getDirectoryFiles(global.SixCRM.routes.path('deployment', 'rds/clusters/')).then(directory_files => {
+
+      let dpcf = arrayutilities.find(directory_files, (directory_file) => {
+        return (directory_file == global.SixCRM.configuration.stage + '.json');
+      });
+
+      dpcf = (_.isUndefined(dpcf) || _.isNull(dpcf)) ? 'default.json' : dpcf;
+
+      return global.SixCRM.routes.include('deployment', 'rds/clusters/' + dpcf);
+
     });
 
   }
 
-  parseParameters(parameters){
+  _parseParameters(parameters) {
 
     du.debug('Parse Parameters');
 
     let parser_data = {
-      region: this.rdsutilities.getRegion(),
-      stage:global.SixCRM.configuration.stage,
+      region: this._rdsutilities.getRegion(),
+      stage: global.SixCRM.configuration.stage,
       account: global.SixCRM.configuration.site_config.aws.account
     };
 
     objectutilities.map(parameters, key => {
-      if(_.isString(parameters[key])){
+      if (_.isString(parameters[key])) {
         parameters[key] = parserutilities.parse(parameters[key], parser_data);
       }
-      if(_.isArray(parameters[key]) && arrayutilities.nonEmpty(parameters[key])){
+      if (_.isArray(parameters[key]) && arrayutilities.nonEmpty(parameters[key])) {
         parameters[key] = arrayutilities.map(parameters[key], a_parameter_value => {
-          if(_.isString(a_parameter_value)){
+          if (_.isString(a_parameter_value)) {
             return parserutilities.parse(a_parameter_value, parser_data);
           }
           return a_parameter_value;
@@ -112,7 +94,7 @@ class RDSClusterDeployment {
 
   }
 
-  deployClusterInstances(parameters){
+  _deployClusterInstances(parameters) {
 
     du.debug('Deploy Cluster Instances');
 
@@ -120,9 +102,9 @@ class RDSClusterDeployment {
 
     let instance_deployment_promises = arrayutilities.map(instances, instance => {
 
-      let instance_parameters = this.parseParameters(instance);
+      let instance_parameters = this._parseParameters(instance);
 
-      return this.rdsutilities.putDBInstance(instance_parameters);
+      return this._rdsutilities.putDBInstance(instance_parameters);
 
     });
 
