1fe2c4b2a548eca5fc39a5708f55b8ddf3b398a6
Justin Durham <justin.durham@icloud.com>
2018-03-09 16:15:51 +0000

    Revert "Cleanup (pull request #32)"
    This reverts pull request #32.
    
    > Cleanup
    
    Former-commit-id: d826b4a3cc40deea983c27595722a4ce30ff682c

---
+++ controllers/analytics/queries/deprecated/event_facet_query.sql
@@ -0,0 +1,66 @@
+SELECT
+  {{facet}},
+  events_count,
+  SUM(CASE WHEN R_ID IN ({{limit_partition}} + {{offset}},0) THEN all_events_count ELSE 0 END)
+  OVER () AS all_events_count
+FROM
+  (SELECT
+     R_ID,
+     {{facet}},
+     events_count,
+     all_events_count
+   FROM
+     (SELECT R_ID,
+        CASE WHEN R_ID = {{limit_partition}} + {{offset}}
+          THEN 'all-other'
+        ELSE {{facet}} END           AS {{facet}},
+        CASE WHEN R_ID = {{limit_partition}} + {{offset}}
+          THEN all_events_count - r_sum_count
+        ELSE events_count END  AS events_count,
+        all_events_count
+      FROM
+        (
+          SELECT
+            {{facet}},
+            events_count,
+            all_events_count,
+            sum(events_count)
+            OVER (
+              ORDER BY events_count {{order}}
+              ROWS BETWEEN {{limit}} PRECEDING AND 0 FOLLOWING ) r_sum_count,
+            ROW_NUMBER()
+            OVER (
+              ORDER BY events_count {{order}} ) AS       R_ID
+          FROM
+            (
+              SELECT
+                {{facet}},
+                count(*)    AS events_count,
+                sum(count(*))
+                OVER ( )    AS all_events_count
+              FROM f_events
+              WHERE {{offset}}
+                    AND datetime BETWEEN TIMESTAMP '{{start}}' AND TIMESTAMP '{{end}}'
+                    AND {{facet}} IS NOT NULL
+              GROUP BY {{facet}}
+            )
+          ORDER BY events_count {{order}}
+          LIMIT {{limit_partition}}
+          OFFSET {{offset}}
+        ))
+   UNION ALL
+   (
+     SELECT 0 AS R_ID,
+       'none'      AS {{facet}},
+       count(*)    AS events_count,
+       {{offset}}           AS all_events_count
+     FROM f_events
+     WHERE {{offset}}
+           AND datetime BETWEEN TIMESTAMP '{{start}}' AND TIMESTAMP '{{end}}'
+           AND {{facet}} IS NULL
+     GROUP BY {{facet}}
+   )
+  )
+ORDER BY events_count {{order}}
+
+
