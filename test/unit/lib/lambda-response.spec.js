881b48752ccfacb38a776d576367f7fbcd7171d9
Ljubomir Simin <ljubomir@coingcs.com>
2018-03-13 15:05:46 +0100

    Lambda response utilities tests completed.
    Former-commit-id: f308bc7be0ff1906aa110962d1ce8b0925122a74

--- test/unit/lib/lambda-response.spec.js
+++ test/unit/lib/lambda-response.spec.js
@@ -8,6 +8,17 @@ const anyEvent = {};
 const anyError = { code: 500, name:'Server Error', message: 'Internal Service Error.', issues: [] };
 
 describe('lib/lambda-response', () => {
+
+    let lambda_response_headers_copy;
+
+    before(() => {
+        lambda_response_headers_copy = global.six_lambda_response_headers
+    });
+
+    after(() => {
+        global.six_lambda_response_headers = lambda_response_headers_copy
+    });
+
     describe('response', () => {
 
         it('should issue a response', (done) => {
@@ -122,6 +133,142 @@ describe('lib/lambda-response', () => {
             });
         });
 
+    });
+
+    describe('createResponseBody', () => {
+
+        it('successfully creates response body', () => {
+
+            let a_success = true;
+            let any_code = 200;
+            let any_response = 'Success';
+
+            const lambdaResponse = new LambdaResponse();
+
+            let result = lambdaResponse.createResponseBody(a_success, any_code, any_response);
+
+            expect(result).to.have.property('success');
+            expect(result).to.have.property('code');
+            expect(result).to.have.property('response');
+        });
+    });
+
+    describe('issueSuccess', () => {
+
+        it('should issue a success', () => {
+
+            let any_response = anyResponse();
+
+            const lambdaResponse = new LambdaResponse();
+
+            lambdaResponse.issueSuccess(any_response, ()=>{});
+
+            let response = JSON.parse(lambdaResponse.lambda_response.body)
+
+            expect(response).to.have.property('success');
+            expect(response.code).to.equal(200);
+            expect(response.response.statusCode).to.equal(200);
+            expect(response.response.headers).to.deep.equal(any_response.headers);
+            expect(response).to.have.property('response');
+        });
+    });
+
+    describe('setLambdaResponseHeader', () => {
+
+        it('sets lambda response header', () => {
+            let params = {
+                key: 'a_key',
+                value: 'a_value'
+            };
+
+            const lambdaResponse = new LambdaResponse();
+
+            lambdaResponse.setLambdaResponseHeader(params.key, params.value);
+
+            expect(lambdaResponse.lambda_response.headers).to.have.property(params.key);
+            expect(lambdaResponse.lambda_response.headers[params.key]).to.equal(params.value);
+        });
+    });
+
+    describe('setResponseHeader', () => {
+
+        it('sets response header', () => {
+            let params = {
+                key: 'a_key',
+                value: 'a_value'
+            };
+
+            const lambdaResponse = new LambdaResponse();
+
+            lambdaResponse.setResponseHeader(params.key, params.value);
+
+            expect(lambdaResponse.lambda_response.headers).to.have.property(params.key);
+            expect(lambdaResponse.lambda_response.headers[params.key]).to.equal(params.value);
+        });
+    });
+
+    describe('assureGlobal', () => {
+
+        it('assures that lambda response headers are defined', () => {
+
+            const lambdaResponse = new LambdaResponse();
+
+            lambdaResponse.assureGlobal();
+
+            expect(global.six_lambda_response_headers).to.be.defined;
+        });
+    });
+
+    describe('setGlobalHeader', () => {
+
+        it('sets global header', () => {
+            let params = {
+                key: 'a_key',
+                value: 'a_value'
+            };
+
+            const lambdaResponse = new LambdaResponse();
+
+            lambdaResponse.setGlobalHeader(params.key, params.value);
+
+            expect(global.six_lambda_response_headers).to.have.property(params.key);
+            expect(global.six_lambda_response_headers[params.key]).to.equal(params.value);
+        });
+    });
+
+    describe('setGlobalHeaders', () => {
+
+        it('sets global headers', () => {
+            let params = {
+                key: 'a_key',
+                value: 'a_value'
+            };
+
+            const lambdaResponse = new LambdaResponse();
+
+            lambdaResponse.setGlobalHeaders(params.key, params.value);
+
+            expect(global.six_lambda_response_headers).to.be.defined;
+            expect(global.six_lambda_response_headers).to.have.property(params.key);
+            expect(global.six_lambda_response_headers[params.key]).to.equal(params.value);
+        });
+    });
+
+    describe('setLambdaResponseHeaders', () => {
+
+        it('sets lambda response headers', () => {
+            let params = {
+                key: 'a_key',
+                value: 'a_value'
+            };
+
+            const lambdaResponse = new LambdaResponse();
+
+            lambdaResponse.setLambdaResponseHeaders(params.key, params.value);
+
+            expect(lambdaResponse.lambda_response.headers).to.have.property(params.key);
+            expect(lambdaResponse.lambda_response.headers[params.key]).to.equal(params.value);
+        });
     });
 
     function anyResponse() {
