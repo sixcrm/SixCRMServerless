3bdf9e98f9dc6acb6ead69db48887945cf4f7788
Ljubomir Simin <ljubomir@coingcs.com>
2018-03-13 13:08:47 +0100

    Added more tests for rds-utilities.
    Former-commit-id: a3b6a3c1488c938598ece269e984e73b83feab71

--- test/unit/lib/rds-utilities.spec.js
+++ test/unit/lib/rds-utilities.spec.js
@@ -66,4 +66,252 @@ describe('lib/rds-utilities', () => {
 
   });
 
+  describe('describeDBInstances', () => {
+
+    it('throws error when db instance identifier is not found', () => {
+
+      let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+      rdsutilities.rds = {
+        describeDBInstances:(parameters, callback) => {
+          let error = new Error('DBInstanceNotFoundFault: DBInstance '+parameters.DBInstanceIdentifier+' not found');
+
+          error.statusCode = 404;
+          callback(error, null);
+        }
+      };
+
+      let parameters = {
+          DBInstanceIdentifier:'sixcrm'
+      };
+
+      return rdsutilities.describeDBInstances(parameters).then(result => {
+        expect(result).to.have.property('ResponseMetadata');
+        expect(result).to.have.property('DBInstances');
+      });
+
+    });
+
+    it('successfully describes db instances', () => {
+
+      let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+      rdsutilities.rds = {
+        describeDBInstances:(parameters, callback) => {
+          callback(null, {ResponseMetadata:{}, DBInstances:[]});
+        }
+      };
+
+      return rdsutilities.describeDBInstances({}).then(result => {
+        expect(result).to.have.property('ResponseMetadata');
+        expect(result).to.have.property('DBInstances');
+
+      });
+
+    });
+
+  });
+
+  describe('createDBInstance', () => {
+
+    it('successfully creates db instance', () => {
+
+      let parameters = {
+          DBInstanceClass:'db.r4.large',
+          DBInstanceIdentifier: 'sixcrm',
+          Engine:'aurora-postgresql'
+      };
+
+      let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+      rdsutilities.rds = {
+        createDBInstance:(parameters, callback) => {
+          callback(null, {ResponseMetadata:{}, DBInstances:[]});
+        }
+      };
+
+      return rdsutilities.createDBInstance(parameters).then(result => {
+        expect(result).to.have.property('ResponseMetadata');
+        expect(result).to.have.property('DBInstances');
+
+      });
+
+    });
+
+  });
+
+  describe('putDBInstance', () => {
+
+    it('successfully creates and describes db instance', () => {
+
+      let parameters = {
+          DBInstanceClass:'db.r4.large',
+          DBInstanceIdentifier: 'sixcrm',
+          Engine:'aurora-postgresql'
+      };
+
+      let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+      rdsutilities.rds = {
+        createDBInstance:(parameters, callback) => {
+            callback(null, {ResponseMetadata:{}, DBInstances:[]});
+        },
+        describeDBInstances:(parameters, callback) => {
+            callback(null, {ResponseMetadata:{}, DBInstances:[]});
+        }
+      };
+
+      return rdsutilities.putDBInstance(parameters).then(result => {
+        expect(result).to.have.property('ResponseMetadata');
+        expect(result).to.have.property('DBInstances');
+
+      });
+
+    });
+
+    it('successfully puts db instance', () => {
+
+      let parameters = {
+          DBInstanceIdentifier: 'sixcrm'
+      };
+
+      let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+      rdsutilities.rds = {
+        describeDBInstances:(parameters, callback) => {
+            callback(null, {ResponseMetadata:{}, DBInstances:[{DBInstanceArn: {}}]});
+        }
+      };
+
+      return rdsutilities.putDBInstance(parameters).then(result => {
+        expect(result).to.have.property('DBInstanceArn');
+      });
+
+    });
+
+  });
+
+  describe('createCluster', () => {
+
+      it('successfully creates cluster', () => {
+
+          let parameters = {
+              DBClusterIdentifier: 'sixcrm',
+              Engine:'aurora-postgresql'
+          };
+
+          let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+          rdsutilities.rds = {
+              createDBCluster:(parameters, callback) => {
+                  callback(null, {ResponseMetadata:{}, DBCluster:[]});
+              }
+          };
+
+          return rdsutilities.createCluster(parameters).then(result => {
+              expect(result).to.have.property('ResponseMetadata');
+              expect(result).to.have.property('DBCluster');
+
+          });
+
+      });
+
+  });
+
+  describe('describeClusters', () => {
+
+      it('throws error when db cluster identifier is not found', () => {
+
+          let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+          rdsutilities.rds = {
+              describeDBClusters:(parameters, callback) => {
+                  let error = new Error('DBClusterNotFoundFault: DBCluster '+parameters.DBClusterIdentifier+' not found');
+
+                  error.statusCode = 404;
+                  callback(error, null);
+              }
+          };
+
+          let parameters = {
+              DBClusterIdentifier:'sixcrm'
+          };
+
+          return rdsutilities.describeClusters(parameters).then(result => {
+              expect(result).to.have.property('ResponseMetadata');
+              expect(result).to.have.property('DBClusters');
+          });
+
+      });
+
+      it('successfully describes db clusters', () => {
+
+          let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+          rdsutilities.rds = {
+              describeDBClusters:(parameters, callback) => {
+                  callback(null, {ResponseMetadata:{}, DBClusters:[]});
+              }
+          };
+
+          return rdsutilities.describeClusters({}).then(result => {
+              expect(result).to.have.property('ResponseMetadata');
+              expect(result).to.have.property('DBClusters');
+
+          });
+
+      });
+
+  });
+
+    describe('putCluster', () => {
+
+        it('successfully creates and describes db cluster', () => {
+
+            let parameters = {
+                DBClusterIdentifier: 'sixcrm',
+                Engine:'aurora-postgresql'
+            };
+
+            let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+            rdsutilities.rds = {
+                createDBCluster:(parameters, callback) => {
+                    callback(null, {ResponseMetadata:{}, DBCluster:[]});
+                },
+                describeDBClusters:(parameters, callback) => {
+                    callback(null, {ResponseMetadata:{}, DBClusters:[]});
+                }
+            };
+
+            return rdsutilities.putCluster(parameters).then(result => {
+                expect(result).to.have.property('ResponseMetadata');
+                expect(result).to.have.property('DBCluster');
+
+            });
+
+        });
+
+        it('successfully puts db cluster', () => {
+
+            let parameters = {
+                DBClusterIdentifier: 'sixcrm'
+            };
+
+            let rdsutilities = global.SixCRM.routes.include('lib', 'rds-utilities.js');
+
+            rdsutilities.rds = {
+                describeDBClusters:(parameters, callback) => {
+                    callback(null, {ResponseMetadata:{}, DBClusters:[{}]});
+                }
+            };
+
+            return rdsutilities.putCluster(parameters).then(result => {
+                expect(result).to.have.property('DBClusters');
+            });
+
+        });
+
+    });
+
 });
