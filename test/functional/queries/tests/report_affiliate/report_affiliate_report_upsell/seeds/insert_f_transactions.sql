0b1e9e8a1b4fb4705375f9bdf6895e8054615330
jadurham909 <justin@adexchangegrp.com>
2018-03-15 11:49:05 -0400

    Fix tests
    Former-commit-id: bfc2954fcb1b65457b0e33d62af233071601423f

--- test/functional/queries/tests/report_affiliate/report_affiliate_report_upsell/seeds/insert_f_transactions.sql
+++ test/functional/queries/tests/report_affiliate/report_affiliate_report_upsell/seeds/insert_f_transactions.sql
@@ -1,17 +1,17 @@
 INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, merchant_provider, campaign, affiliate, amount, processor_result, account, type, subtype, product_schedule, subaffiliate_1, subaffiliate_2, subaffiliate_3, subaffiliate_4, subaffiliate_5)
-  ((
+(
      SELECT
       'e624af6a-21dc-4c64-b310-3b0523f8ca42',
-      timestamp'2017-01-01T18:40:41.405Z',
+      '2017-01-01T18:40:41.405Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
       34.99,
-      'success',
+      'success'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'new',
+      'new'::analytics.d_event_type,
       'upsell',
       '12529a17-ac32-4e46-b05b-83862843055d',
       '',
@@ -22,16 +22,16 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
      UNION ALL
      SELECT
       'cf3cd926-b321-453a-99be-591611f07e3d',
-      timestamp'2017-01-01T18:40:41.405Z',
+      '2017-01-01T18:40:41.406Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
       4.99,
-      'success',
+      'success'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'new',
+      'new'::analytics.d_event_type,
       'upsell2',
       '12529a17-ac32-4e46-b05b-83862843055d',
       '',
@@ -42,16 +42,16 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
      UNION ALL
      SELECT
       '07110153-41c5-4a7f-ae49-8204d7ba7ff0',
-      timestamp'2017-01-01T18:40:41.405Z',
+      '2017-01-01T18:40:41.407Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
       124.99,
-      'success',
+      'success'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'new',
+      'new'::analytics.d_event_type,
       'upsell',
       '300848c1-8b83-4b8d-aff9-529ba6459d0f',
       '',
@@ -62,16 +62,16 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
      UNION ALL
      SELECT
       'b5a1753d-717a-4454-ab4e-2abeef1c306f',
-      timestamp'2017-01-01T18:40:41.405Z',
+      '2017-01-01T18:40:41.408Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
       1399,
-      'success',
+      'success'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'new',
+      'new'::analytics.d_event_type,
       'upsell',
       '2200669e-5e49-4335-9995-9c02f041d91b',
       '',
@@ -82,16 +82,16 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
      UNION ALL
      SELECT
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      timestamp'2017-01-02T18:40:41.405Z',
+      '2017-01-02T18:40:41.409Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
       139.99,
-      'success',
+      'success'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'upsell',
+      'upsell'::analytics.d_event_type,
       'order',
       '2200669e-5e49-4335-9995-9c02f041d91b',
       '',
@@ -99,7 +99,6 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
       '',
       '',
       ''
-   )
    EXCEPT
    SELECT
      ft.id,
