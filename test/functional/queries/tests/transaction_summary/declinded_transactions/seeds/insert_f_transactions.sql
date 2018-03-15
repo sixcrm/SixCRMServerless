b75e9bb265be8a8ac167fa2f80d5be9abdd7605b
jadurham909 <justin@adexchangegrp.com>
2018-03-15 18:01:29 -0400

    Fix more tests
    Former-commit-id: d0a9bc1c3b1f47dd2b3720382457119a04f3cef6

--- test/functional/queries/tests/transaction_summary/declinded_transactions/seeds/insert_f_transactions.sql
+++ test/functional/queries/tests/transaction_summary/declinded_transactions/seeds/insert_f_transactions.sql
@@ -1,17 +1,17 @@
 INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, merchant_provider, campaign, affiliate, amount, processor_result, account, type, subtype, product_schedule, subaffiliate_1, subaffiliate_2, subaffiliate_3, subaffiliate_4, subaffiliate_5)
-  ((
+(
      SELECT
       '99999999-999e-44aa-999e-aaa9a99a9999',
-      timestamp'2017-01-02T18:50:41.405Z',
+      '2017-01-02T18:50:41.405Z'::timestamp,
       '99999999-999e-44aa-999e-aaa9a99a9999',
       '99999999-999e-44aa-999e-aaa9a99a9999',
       '99999999-999e-44aa-999e-aaa9a99a9999',
       '99999999-999e-44aa-999e-aaa9a99a9999',
       '99999999-999e-44aa-999e-aaa9a99a9999',
       10,
-      'decline',
+      'decline'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'new',
+      'new'::analytics.d_event_type,
       'upsell',
       '99999999-999e-44aa-999e-aaa9a99a9999',
       '',
@@ -22,16 +22,16 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
      UNION ALL
      SELECT
       'e624af6a-21dc-4c64-b310-3b0523f8ca42',
-      timestamp'2017-01-02T08:10:41.405Z',
+      '2017-01-02T08:10:41.406Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       '6b6331f6-7f84-437a-9ac6-093ba301e455',
       34.99,
-      'decline',
+      'decline'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'new',
+      'new'::analytics.d_event_type,
       'order',
       '12529a17-ac32-4e46-b05b-83862843055d',
       '',
@@ -42,16 +42,16 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
      UNION ALL
      SELECT
       'cf3cd926-b321-453a-99be-591611f07e3d',
-      timestamp'2017-01-02T09:45:41.405Z',
+      '2017-01-02T09:45:41.407Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       '6b6331f6-7f84-437a-9ac6-093ba301e455',
       4.9,
-      'decline',
+      'decline'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'refund',
+      'refund'::analytics.d_event_type,
       'order',
       '12529a17-ac32-4e46-b05b-83862843055d',
       '',
@@ -62,16 +62,16 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
      UNION ALL
      SELECT
       '07110153-41c5-4a7f-ae49-8204d7ba7ff0',
-      timestamp'2017-01-02T20:20:41.405Z',
+      '2017-01-02T20:20:41.408Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       '6b6331f6-7f84-437a-9ac6-093ba301e455',
       0.99,
-      'decline',
+      'decline'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'rebill',
+      'rebill'::analytics.d_event_type,
       'order',
       '300848c1-8b83-4b8d-aff9-529ba6459d0f',
       '',
@@ -81,37 +81,17 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
       ''
      UNION ALL
      SELECT
-      'b5a1753d-717a-4454-ab4e-2abeef1c306f',
-      timestamp'2017-01-02T11:40:33.405Z',
-      '24f7c851-29d4-4af9-87c5-0298fa74c689',
-      'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
-      '6c40761d-8919-4ad6-884d-6a46a776cfb9',
-      '70a6689a-5814-438b-b9fd-dd484d0812f9',
-      '6b6331f6-7f84-437a-9ac6-093ba301e455',
-      1399,
-      'decline',
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      '',
-      '',
-      '2200669e-5e49-4335-9995-9c02f041d91b',
-      '',
-      '',
-      '',
-      '',
-      ''
-     UNION ALL
-     SELECT
-      'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      timestamp'2017-01-02T11:20:41.405Z',
+      '2017-01-02T11:20:41.409Z'::timestamp,
       '24f7c851-29d4-4af9-87c5-0298fa74c689',
       'df84f7bb-06bd-4daa-b1a3-6a2c113edd72',
       '6c40761d-8919-4ad6-884d-6a46a776cfb9',
       '70a6689a-5814-438b-b9fd-dd484d0812f9',
       '6b6331f6-7f84-437a-9ac6-093ba301e455',
       222,
-      'decline',
+      'decline'::analytics.d_processor_result,
       'd26c1887-7ad4-4a44-be0b-e80dbce22774',
-      'chargeback',
+      'chargeback'::analytics.d_event_type,
       'order',
       '2200669e-5e49-4335-9995-9c02f041d91b',
       '',
@@ -119,7 +99,6 @@ INSERT INTO analytics.f_transactions (id, datetime, customer, creditcard, mercha
       '',
       '',
       ''
-   )
    EXCEPT
    SELECT
      ft.id,
