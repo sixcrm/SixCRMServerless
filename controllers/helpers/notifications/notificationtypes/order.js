db40143be079408f121c957e71132ab1fd061c4a
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-03-16 12:02:52 -0700

    Adding additional transaction notifications
    Former-commit-id: 6475323e99fe499959c25561f501f39e5d7b5bdb

---
+++ controllers/helpers/notifications/notificationtypes/order.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const BaseNotification = global.SixCRM.routes.include('helpers','notifications/notificationtypes/components/BaseNotification.js');
+const du = global.SixCRM.routes.include('lib','debug-utilities.js');
+
+class OrderNotification extends BaseNotification {
+
+  constructor(){
+
+    super();
+
+    this.title = 'You\'ve got a new order!';
+    this.body = '{{campaign.name}} has a new order!';
+    this.category = 'transaction';
+    this.notification_type = 'order';
+
+    this.account_wide = true;
+
+  }
+
+  transformContext(context){
+
+    du.debug('Transform Context');
+
+    let return_object = {
+      user: this.getUserFromContext(context),
+      account: this.getAccountFromContext(context),
+      type: this.getNotificationType(),
+      category: this.getNotificationCategory(),
+      title: this.getTitle(context),
+      body: this.getBody(context),
+      action: this.createAction(context)
+    };
+
+
+    return return_object;
+
+  }
+
+  createAction(context){
+
+    du.debug('Create Action');
+
+    return JSON.stringify({
+      entity: 'session',
+      id: this.contextHelperController.getFromContext(context, 'session.id', 'id')
+    });
+
+  }
+
+}
+
+module.exports = new OrderNotification();
