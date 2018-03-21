b03b4c4706b33e6d37512e7607fb6a3feb32a7a6
jadurham909 <justin@adexchangegrp.com>
2018-03-20 22:19:00 -0400

    Fix serverless key
    Former-commit-id: 1798c0a29a09612ee052d543fadbaf1b8fb40146

--- handlers/workers/analyticseventhandler/handler.js
+++ handlers/workers/analyticseventhandler/handler.js
@@ -5,7 +5,7 @@ const LambdaResponse = global.SixCRM.routes.include('lib', 'lambda-response.js')
 const WriteTransactionRecords = global.SixCRM.routes.include('controllers', 'workers/analytics/WriteTransactionRecords.js');
 const auroraContext = global.SixCRM.routes.include('lib', 'analytics/aurora-context');
 
-module.exports.analyticsevents = (event, context, callback) => {
+module.exports.analyticseventhandler = (event, context, callback) => {
 
 	const writeTransactionRecords = new WriteTransactionRecords();
 
