edb795d95c1737f3097724e113730ced38fa89f8
jadurham909 <justin@adexchangegrp.com>
2018-03-23 17:11:04 -0400

    Fix broken SNS promises in functional tests
    Former-commit-id: 15a1bfd939c1b2456aa91ec7bffacfaa888a0a22

--- test/functional/state_machine/stress_tests/holdToPendingStressTest.test.js
+++ test/functional/state_machine/stress_tests/holdToPendingStressTest.test.js
@@ -1,3 +1,4 @@
+require('../state-machine-test-setup');
 const uuidV4 = require('uuid/v4');
 const SqSTestUtils = require('../../sqs-test-utils');
 const StateMachine = require('../state-machine-test-utils.js');
