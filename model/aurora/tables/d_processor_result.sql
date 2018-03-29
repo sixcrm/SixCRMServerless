34203df9ae4aef096e9268aa6827c83ab52902f5
jadurham909 <justin@adexchangegrp.com>
2018-03-29 18:10:22 -0400

    Fix the
    Former-commit-id: 28830937d288ce67d685dc2feb82b6291d8f79a8

--- model/aurora/tables/d_processor_result.sql
+++ model/aurora/tables/d_processor_result.sql
@@ -1,6 +1,8 @@
 DROP TYPE IF EXISTS analytics.d_processor_result;
 
 CREATE TYPE analytics.d_processor_result AS ENUM ( 'success',
-  'declined',
+  'declined', -- this is the register response, not sure we will ever use this?
+  'fail', -- this is mapped to decline in the register, but this is the transaction result
   'error'
 );
+
