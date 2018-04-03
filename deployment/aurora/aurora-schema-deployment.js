343562ba680ce9c9f7395ed265926c69a89346e2
jadurham909 <justin@adexchangegrp.com>
2018-04-03 13:27:50 -0400

    Simplify aurora deployments
    Former-commit-id: 081c58fcc6a6940c020e3ccdfa6cf9be42f78220

--- deployment/aurora/aurora-schema-deployment.js
+++ deployment/aurora/aurora-schema-deployment.js
@@ -7,13 +7,14 @@ const auroraContext = global.SixCRM.routes.include('lib', 'analytics/aurora-cont
 
 module.exports = class AuroraSchemaDeployment {
 
-	deploy() {
+	deploy(options = {}) {
 
 		du.debug('AuroraSchemaDeployment.deploy()');
 
 		return Promise.resolve()
-			.then(this._getVersionDirectories.bind(this))
-			.then((migrations) => BBPromise.each(migrations, this._deployMigration.bind(this)));
+			.then(() => this._getVersionDirectories(options))
+			.then((migrations) => BBPromise.each(migrations, this._deployMigration.bind(this)))
+			.then(() => 'Aurora deploy complete');
 
 	}
 
@@ -27,11 +28,15 @@ module.exports = class AuroraSchemaDeployment {
 
 	}
 
-	_getVersionDirectories() {
+	/*
+		We want all revisions from the current version from the database up until the configured version
+		in the configuration, and then run them sequentially
+	*/
+	_getVersionDirectories(options) {
 
 		du.debug('AuroraSchemaDeployment._getVersionDirectories()');
 
-		const release = Number(global.SixCRM.configuration.site_config.aurora.release);
+		const release = global.SixCRM.configuration.site_config.aurora.release;
 
 		return Promise.resolve()
 			.then(() => fileutilities.getDirectoryFiles(global.SixCRM.routes.path('deployment', 'aurora/migrations')))
@@ -41,7 +46,27 @@ module.exports = class AuroraSchemaDeployment {
 					path: path.join(global.SixCRM.routes.path('deployment', 'aurora/migrations'), r)
 				};
 			}))
-			.then((results) => _.sortBy(_.filter(results, (f) => f.version <= release), 'version'))
+			.then((migrations) => this._executeQuery('SELECT id FROM analytics.m_release ORDER BY id DESC LIMIT 1').then((currentRevision) => {
+				return {
+					currentRevision: Number(currentRevision.rows[0]),
+					migrations
+				}
+			}).catch((ex) => {
+
+				du.warning('Could not resolve the current aurora schema version', ex);
+
+				return {
+					currentRevision: 0,
+					migrations
+				}
+			}))
+			.then((result) => _.sortBy(_.filter(result.migrations, (f) => {
+
+				const currentRevision = options.fromRevision ? options.fromRevision : result.currentRevision;
+
+				return f.version <= Number(release) && f.version > currentRevision;
+
+			}), 'version'))
 
 	}
 
