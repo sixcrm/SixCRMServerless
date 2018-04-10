1d2bf5b7240b7dfb183d6b959c3ee49927e1edc9
Ljubomir Simin <ljubomir@coingcs.com>
2018-04-10 16:47:04 +0200

    Add support for Cloudsearch pagination.
    Former-commit-id: 7b229e90546ced7101bfafe198919e8e95b8b967

--- controllers/helpers/indexing/ReIndexing.js
+++ controllers/helpers/indexing/ReIndexing.js
@@ -10,6 +10,15 @@ const cloudsearchprovider = new CloudsearchProvider();
 const PreIndexingHelperController = global.SixCRM.routes.include('helpers', 'indexing/PreIndexing.js');
 let preIndexingHelperController = new PreIndexingHelperController();
 
+let entities_dynamodb = [];
+let entities_index = [];
+
+let missing_in_index = [];
+let missing_in_dynamo = [];
+
+let index_details = {};
+let db_details = {};
+
 module.exports = class ReIndexingHelperController {
 
   constructor(){
@@ -19,19 +28,56 @@ module.exports = class ReIndexingHelperController {
   //Entrypoint
   execute(fix){
 
-    du.debug('Reindexing');
+      du.debug('Reindexing');
+
+      return this.getCloudsearchItemsRecursive()
+          .then(() => this.getDynamoItems())
+          .then(() => this.determineDifferences())
+          .then(() => this.printStatistics())
+          .then(() => this.fixIndex(fix))
+          .then(() => {
+              du.output('Finished');
+          });
+  }
+
+  getCloudsearchItemsRecursive(cursor, all_items) {
+
+      const limit = 10000;
+
+      if (cursor === undefined) {
+          cursor = 'initial';
+      }
+
+      if (all_items === undefined) {
+          all_items = [];
+      }
+
+      let parameters = {
+          query: 'matchall',
+          queryParser: 'structured',
+          size: limit,
+          cursor: cursor
+      };
+
+      return cloudsearchprovider.executeStatedSearch(parameters).then((search_results) => {
+
+          all_items.push(...search_results.hits.hit);
+
+          if (search_results.hits.hit.length > 0) {
+              return this.getCloudsearchItemsRecursive(search_results.hits.cursor, all_items);
+          }
+
+          return Promise.resolve(all_items).then(() => entities_index = all_items);
+      })
+  }
+
+  getDynamoItems() {
+
+      let promises = [];
 
       let indexing_entities = preIndexingHelperController.indexing_entities;
 
       du.output('Indexing entities: ' + indexing_entities);
-
-      let entities_dynamodb = [];
-      let entities_index = [];
-
-      let missing_in_index = [];
-      let missing_in_dynamo = [];
-
-      let promises = [];
 
       indexing_entities.map(entity => {
           promises.push(() => dynamodbprovider.scanRecords(entity + 's').then(r => {
@@ -42,80 +88,71 @@ module.exports = class ReIndexingHelperController {
                       entity: c
                   });
               });
-              return true;
           }));
-
       });
 
-      return arrayutilities.serial(promises).then(() => {
-          // Technical Debt: This only works for up to 10000 entities in index.
-          return cloudsearchprovider.executeStatedSearch({ query: 'matchall', queryParser: 'structured', size: 10000})
-      }).then((search) => {
-          search.hits.hit.map((s) => {
-              entities_index.push(s);
-          })
-          return true;
-      }).then(() => {
-          let index_details = {};
-          let db_details = {};
+      return arrayutilities.serial(promises);
+  }
 
-          entities_dynamodb.map(d => {
-              if (!(entities_index.map(i => i.id).includes(d.id))) {
-                  let add = Object.assign({}, d.entity);
+  determineDifferences() {
 
-                  add.entity_type = d.entity_type;
-                  missing_in_index.push(add);
+      entities_dynamodb.map(d => {
+          if (!(entities_index.map(i => i.id).includes(d.id))) {
+              let add = Object.assign({}, d.entity);
 
-                  if (!index_details[add.entity_type]) {
-                      index_details[add.entity_type] = 0;
-                  }
+              add.entity_type = d.entity_type;
+              missing_in_index.push(add);
 
-                  index_details[add.entity_type]++;
+              if (!index_details[add.entity_type]) {
+                  index_details[add.entity_type] = 0;
               }
-          });
 
-          entities_index.map(i => {
-              if (!(entities_dynamodb.map(d => d.id).includes(i.id))) {
-                  missing_in_dynamo.push({id: i.id, entity_type: i.fields.entity_type[0]});
-
-                  if (!db_details[i.fields.entity_type]) {
-                      db_details[i.fields.entity_type] = 0;
-                  }
-
-                  db_details[i.fields.entity_type]++;
-              }
-          });
-
-          let operations = [];
-
-          du.output('Total in dynamodb: ' + entities_dynamodb.length);
-          du.output('Total in index: ' + entities_index.length);
-          du.output('Missing in index: ' + missing_in_index.length);
-          du.debug(index_details);
-          du.output('Missing in dynamodb: ' + missing_in_dynamo.length);
-          du.debug(db_details);
-
-          missing_in_index.map(m => {
-              if (fix === true) {
-                  operations.push(() => preIndexingHelperController.addToSearchIndex(m));
-              }
-          });
-
-          missing_in_dynamo.map(m => {
-              if (fix === true) {
-                  operations.push(() => preIndexingHelperController.removeFromSearchIndex(m));
-              }
-          });
-
-
-          return arrayutilities.serial(operations);
-
-      }).then(() => {
-          du.output('Finished');
-          return true;
+              index_details[add.entity_type]++;
+          }
       });
 
+      entities_index.map(i => {
+          if (!(entities_dynamodb.map(d => d.id).includes(i.id))) {
+              missing_in_dynamo.push({id: i.id, entity_type: i.fields.entity_type[0]});
+
+              if (!db_details[i.fields.entity_type]) {
+                  db_details[i.fields.entity_type] = 0;
+              }
+
+              db_details[i.fields.entity_type]++;
+          }
+      });
+  }
+
+  printStatistics() {
+
+      du.output('Total in dynamodb: ' + entities_dynamodb.length);
+      du.output('Total in index: ' + entities_index.length);
+      du.output('Missing in index: ' + missing_in_index.length);
+      du.debug(index_details);
+      du.output('Missing in dynamodb: ' + missing_in_dynamo.length);
+      du.debug(db_details);
 
   }
 
-}
+  fixIndex(fix) {
+
+      let operations = [];
+
+      missing_in_index.map(m => {
+          if (fix === true) {
+              operations.push(() => preIndexingHelperController.addToSearchIndex(m));
+          }
+      });
+
+      missing_in_dynamo.map(m => {
+          if (fix === true) {
+              operations.push(() => preIndexingHelperController.removeFromSearchIndex(m));
+          }
+      });
+
+
+      return arrayutilities.serial(operations);
+  }
+
+};
