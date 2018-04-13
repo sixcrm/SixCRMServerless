8e65693edffc021658d83177ff1076b77998e62e
Ljubomir Simin <ljubomir@coingcs.com>
2018-04-13 16:38:38 +0200

    Coverage for notification types components - Base notification and utilities.
    Former-commit-id: 9cede4974c9b98b640deff0a3095f40276c2c77e

---
+++ test/unit/controllers/helpers/notifications/notification_types/components/BaseNotification.spec.js
@@ -0,0 +1,123 @@
+'use strict'
+const chai = require('chai');
+const expect = chai.expect;
+const mockery = require('mockery');
+
+describe('/helpers/notifications/notificationtypes/components/BaseNotification.js', () => {
+
+    before(() => {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnReplace: false,
+            warnOnUnregistered: false
+        });
+    });
+
+    afterEach(() => {
+        mockery.resetCache();
+        mockery.deregisterAll();
+    });
+
+    describe('transformContext', () => {
+
+        it('successfully transforms context', () => {
+
+            let context = [{
+                user: { id: 'some@user.com' },
+                account: 'd3fa3bf3-7824-49f4-8261-87674482bf1c'
+            }];
+
+            let BaseNotificationController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/BaseNotification.js');
+            const baseNotificationController = new BaseNotificationController();
+
+            baseNotificationController.name = 'test';
+
+            expect(baseNotificationController.transformContext(context)).to.deep.equal({
+                name: 'test',
+                user: context[0].user.id,
+                account: context[0].account,
+                type: 'notification',
+                category: 'base',
+                context: {}
+            });
+
+        });
+    });
+
+    describe('createContext', () => {
+
+        it('successfully creates context', () => {
+
+            let context = [{
+                user: { id: 'some@user.com' }
+            }];
+
+            let BaseNotificationController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/BaseNotification.js');
+            const baseNotificationController = new BaseNotificationController();
+
+            baseNotificationController.context_required = ['id'];
+
+            expect(baseNotificationController.createContext(context)).to.deep.equal(context[0].user);
+
+        });
+
+        it('throws error when required context is not found', () => {
+
+            let context = [{
+                user: { id: 'some@user.com' }
+            }];
+
+            let BaseNotificationController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/BaseNotification.js');
+            const baseNotificationController = new BaseNotificationController();
+
+            baseNotificationController.context_required = ['name'];
+
+            try {
+                baseNotificationController.createContext(context)
+            } catch (error) {
+                expect(error.message).to.deep.equal("[500] Unable to identify \"name\" from context.");
+            }
+        });
+    });
+
+    describe('triggerNotifications', () => {
+
+        it('triggers notifications for account', () => {
+
+            let transformed_context = {};
+
+            let BaseNotificationController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/BaseNotification.js');
+            const baseNotificationController = new BaseNotificationController();
+
+            const NotificationProvider = global.SixCRM.routes.include('providers', 'notification/Notification.js');
+            baseNotificationController.notificationProvider = new NotificationProvider();
+
+            baseNotificationController.account_wide = true;
+            baseNotificationController.notificationProvider.createNotificationsForAccount = ({notification_prototype}) => {
+                expect(notification_prototype).to.equal(transformed_context);
+                return Promise.resolve(true);
+            };
+
+            expect(baseNotificationController.triggerNotifications(transformed_context)).to.deep.equal(transformed_context);
+
+        });
+
+        it('triggers notifications for account and user', () => {
+
+            let transformed_context = {};
+
+            let BaseNotificationController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/BaseNotification.js');
+            const baseNotificationController = new BaseNotificationController();
+
+            const NotificationProvider = global.SixCRM.routes.include('providers', 'notification/Notification.js');
+            baseNotificationController.notificationProvider = new NotificationProvider();
+            baseNotificationController.notificationProvider.createNotificationForAccountAndUser = ({notification_prototype}) => {
+                expect(notification_prototype).to.equal(transformed_context);
+                return Promise.resolve(true);
+            };
+
+            expect(baseNotificationController.triggerNotifications(transformed_context)).to.deep.equal(transformed_context);
+
+        });
+    });
+});
---
+++ test/unit/controllers/helpers/notifications/notification_types/components/NotificationUtilities.spec.js
@@ -0,0 +1,178 @@
+'use strict'
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('/helpers/notifications/notificationtypes/components/NotificationUtilities.js', () => {
+
+    describe('getName', () => {
+
+        it('successfully gets notification name', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            notificationUtilitiesController.name = 'test';
+
+            expect(notificationUtilitiesController.getName()).to.equal('test');
+
+        });
+
+        it('successfully gets notification name', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            notificationUtilitiesController.name = 'lead';
+
+            expect(notificationUtilitiesController.getName()).to.equal('lead');
+
+        });
+
+        it('throws error when notification name is not successfully retrieved', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            try{
+                notificationUtilitiesController.getName();
+            } catch (error) {
+                expect(error.message).to.equal('[500] Nameless notification, very cryptic.');
+            }
+        });
+    });
+
+    describe('getNotificationCategory', () => {
+
+        it('successfully gets notification category', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            notificationUtilitiesController.category = 'test';
+
+            expect(notificationUtilitiesController.getNotificationCategory()).to.equal('test');
+
+        });
+
+        it('successfully gets notification category', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            notificationUtilitiesController.category = 'transaction';
+
+            expect(notificationUtilitiesController.getNotificationCategory()).to.equal('transaction');
+
+        });
+
+        it('throws error when notification category is not successfully retrieved', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            try{
+                notificationUtilitiesController.getNotificationCategory();
+            } catch (error) {
+                expect(error.message).to.equal('[500] Unable to determine notification category.');
+            }
+        });
+    });
+
+    describe('getNotificationType', () => {
+
+        it('successfully gets notification type', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            notificationUtilitiesController.notification_type = 'notification';
+
+            expect(notificationUtilitiesController.getNotificationType()).to.equal('notification');
+
+        });
+
+        it('throws error when notification type is not successfully retrieved', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            try{
+                notificationUtilitiesController.getNotificationType();
+            } catch (error) {
+                expect(error.message).to.equal('[500] Unable to determine notification type.');
+            }
+        });
+
+        it('throws error when notification type is not recognized', () => {
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            notificationUtilitiesController.notification_type = 'unrecognized_type';
+
+            try{
+                notificationUtilitiesController.getNotificationType();
+            } catch (error) {
+                expect(error.message).to.equal('[500] One or more validation errors occurred: [Notification Type] instance should be equal to one of the allowed values');
+            }
+        });
+    });
+
+    describe('getUserFromContext', () => {
+
+        it('returns user id from context', () => {
+
+            let contexts = [{
+                user: { id: 'some@user.com' }
+            }];
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            expect(notificationUtilitiesController.getUserFromContext(contexts)).to.equal(contexts[0].user.id);
+
+        });
+
+        it('returns user email from context', () => {
+
+            let contexts = [{
+                user: 'Pierce.Connelly@kathryne.org'
+            }];
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            expect(notificationUtilitiesController.getUserFromContext(contexts)).to.equal(contexts[0].user);
+
+        });
+    });
+
+    describe('getAccountFromContext', () => {
+
+        it('returns account id from context', () => {
+
+            let contexts = [{
+                account: { id: 'd3fa3bf3-7824-49f4-8261-87674482bf1c' }
+            }];
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            expect(notificationUtilitiesController.getAccountFromContext(contexts)).to.equal(contexts[0].account.id);
+
+        });
+
+        it('returns account from context', () => {
+
+            let contexts = [{
+                account: 'd3fa3bf3-7824-49f4-8261-87674482bf1c'
+            }];
+
+            let NotificationUtilitiesController = global.SixCRM.routes.include('helpers', 'notifications/notificationtypes/components/NotificationUtilities.js');
+            const notificationUtilitiesController = new NotificationUtilitiesController();
+
+            expect(notificationUtilitiesController.getAccountFromContext(contexts)).to.equal(contexts[0].account);
+
+        });
+    });
+});
