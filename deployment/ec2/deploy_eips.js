f4781ddcba01c95756c717a9c717efa5cfe8f4f2
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-04-14 11:36:59 -0700

    Linting Errors
    Former-commit-id: d67b9e083c5e6c21c0884fdd216e6d8f98fd63b6

--- deployment/ec2/deploy_eips.js
+++ deployment/ec2/deploy_eips.js
@@ -5,7 +5,7 @@ const EC2Deployment = global.SixCRM.routes.include('deployment', 'utilities/ec2-
 
 let ec2Deployment = new EC2Deployment();
 
-return ec2Deployment.deployEIPs().then((result) => {
+ec2Deployment.deployEIPs().then((result) => {
   return du.highlight(result);
 }).catch(error => {
   du.error(error);
