8a2027be8700f0662d988018c47af70496f6459c
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-04-14 11:30:43 -0700

    Environment Deployment Script
    Former-commit-id: 8ac8d9a7106db9e6ba74c9421026c4ff3eed75bd

---
+++ deployment/ec2/deploy_lambda_environment.js
@@ -0,0 +1,21 @@
+require('../../SixCRM.js');
+
+const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+const EC2Deployment = global.SixCRM.routes.include('deployment', 'utilities/ec2-deployment.js');
+
+let ec2Deployment = new EC2Deployment();
+
+ec2Deployment.deployVPCs()
+.then(() => ec2Deployment.deploySecurityGroups())
+.then(() => ec2Deployment.deploySubnets())
+.then(() => ec2Deployment.deployEIPs())
+.then(() => ec2Deployment.deployNATs())
+.then(() => ec2Deployment.deployInternetGateways())
+.then(() => ec2Deployment.deployRouteTables())
+.then(() => {
+  du.highlight('Environment Deployed');
+  return true;
+}).catch(error => {
+  du.error(error);
+  du.warning(error.message);
+});
