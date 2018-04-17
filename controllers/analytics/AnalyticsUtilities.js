2bec8b4a010fd10dc081748fae67f1d3e9cb7901
jadurham909 <justin@adexchangegrp.com>
2018-04-17 09:44:40 -0400

    Remove console.log
    Former-commit-id: 4134081ad9c3b74a3afe31ab30e24b569260824a

--- controllers/analytics/AnalyticsUtilities.js
+++ controllers/analytics/AnalyticsUtilities.js
@@ -121,8 +121,6 @@ module.exports = class AnalyticsUtilities extends PermissionedController {
 
 					const auroraContext = global.SixCRM.getResource('auroraContext');
 
-					// console.log(query);
-
 					return this.cacheController.useCache(query, () => auroraContext.connection.queryWithArgs(query, []).then(result => result.rows))
 						.then((results) => transformation_function(results, parameters))
 						.then((transformed_results) => {
