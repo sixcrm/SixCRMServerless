a0628f7796c1a9a17f0eebc93410a82fbf6754bf
jadurham909 <justin@adexchangegrp.com>
2018-04-18 12:43:07 -0400

    Test aurora configuration update
    Former-commit-id: fe567281855b80f7b03b952b199c91ffbb838d16

--- deployment/aurora/aurora-deploy.js
+++ deployment/aurora/aurora-deploy.js
@@ -3,14 +3,23 @@ require('../../SixCRM.js');
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
 const AuroraSchemaDeployment = global.SixCRM.routes.include('deployment', 'aurora/aurora-schema-deployment.js');
 const auroraSchemaDeployment = new AuroraSchemaDeployment();
+const configurationAcquistion = global.SixCRM.routes.include('config', 'controllers/configuration_acquisition.js');
 
-auroraSchemaDeployment.deploy().then((result) => {
+configurationAcquistion.getAuroraClusterEndpoint()
+	.then((endpoint) => {
 
-	return du.highlight(result);
+		process.env.aurora_host = endpoint;
+		return;
 
-}).catch(error => {
+	})
+	.then(() => auroraSchemaDeployment.deploy())
+	.then((result) => {
 
-	du.error(error);
-	du.warning(error.message);
+		return du.highlight(result);
 
-});
+	}).catch(error => {
+
+		du.error(error);
+		du.warning(error.message);
+
+	});
