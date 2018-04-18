6ef68eeea02f75ea8dc7ae0a6b9df34672f0016e
jadurham909 <justin@adexchangegrp.com>
2018-04-18 18:17:25 -0400

    Seed dynamo for tests
    Former-commit-id: 71540946c0bcdf0b45793b98c30f5519e9f92636

--- controllers/analytics/queries/reports/affiliate/facets/affiliates/transform.js
+++ controllers/analytics/queries/reports/affiliate/facets/affiliates/transform.js
@@ -1,16 +1,24 @@
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+const AffiliateController = global.SixCRM.routes.include('controllers', 'entities/Affiliate.js');
 
-module.exports = (results) => {
+module.exports = async (results) => {
 
 	du.debug('Transformation Function');
 
+	const ids = results.map(r => r.affiliate);
+
+	const affiliates = await (new AffiliateController()).getByAffiliateIds(ids).affiliates || [];
+
 	return Promise.resolve({
 		facet: 'affiliate',
-		values: results.map(r => {
-			return {
-				key: r.affiliate,
-				value: r.affiliate
-			}
+		values: affiliates.map(r => {
+			return [{
+				key: 'id',
+				value: r.id
+			}, {
+				key: 'name',
+				value: r.name
+			}]
 		})
 	});
 
