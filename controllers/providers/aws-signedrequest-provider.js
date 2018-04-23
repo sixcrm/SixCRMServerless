c4a2acdb823d5f0c39dcd4cc37e432a4f69469bc
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-04-23 15:30:08 -0700

    Tested logger
    Former-commit-id: 6042ea2d63aca8d1e08f1f4c75196e60cc8e79d8

--- controllers/providers/aws-signedrequest-provider.js
+++ controllers/providers/aws-signedrequest-provider.js
@@ -1,13 +1,16 @@
+const _ = require('lodash');
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+const eu = global.SixCRM.routes.include('lib', 'error-utilities.js');
 const hashutilities = global.SixCRM.routes.include('lib', 'hash-utilities.js');
 const stringutilities = global.SixCRM.routes.include('lib', 'string-utilities.js');
-const timestamp = global.SixCRM.routes.include('lib', 'hash-utilities.js');
+const arrayutilities = global.SixCRM.routes.include('lib', 'array-utilities.js');
+const timestamp = global.SixCRM.routes.include('lib', 'timestamp.js');
+
+const HTTPProvider = global.SixCRM.routes.include('providers', 'http-provider.js');
 
 module.exports = class AWSSignedRequestProvider {
 
 	constructor() {
-
-		this.https = require('https');
 
 	}
 
@@ -16,8 +19,25 @@ module.exports = class AWSSignedRequestProvider {
 		du.debug('Signed Request');
 
 		return Promise.resolve()
+			.then(() => this.validateEnvironment())
 			.then(() => this.buildSignedRequest(endpoint, body))
 			.then((request_parameters) => this.executeRequest(request_parameters));
+
+	}
+
+	validateEnvironment() {
+
+		du.debug('Validate Environment');
+
+		if (!_.has(process.env, 'AWS_SESSION_TOKEN')) {
+			throw eu.getError('server', 'Missing "AWS_SESSION_TOKEN" in process.env');
+		}
+
+		if (!_.has(process.env, 'AWS_SECRET_ACCESS_KEY')) {
+			throw eu.getError('server', 'Missing "AWS_SECRET_ACCESS_KEY" in process.env');
+		}
+
+		return true;
 
 	}
 
@@ -25,56 +45,54 @@ module.exports = class AWSSignedRequestProvider {
 
 		du.debug('Execute Request');
 
-		return new Promise((resolve, reject) => {
+		let parameters = {
+			endpoint: request_parameters.host + request_parameters.path,
+			body: request_parameters.body,
+			headers: request_parameters.headers
+		};
 
-			let request = this.https.request(request_parameters, (response) => {
+		let httpprovider = new HTTPProvider();
 
-				var response_body = '';
+		return httpprovider.post(parameters).then(response => {
 
-				response.on('data', (chunk) => {
-					response_body += chunk;
-				});
+			let info = stringutilities.parseJSONString(response.body);
+			let failed_items = [];
+			let success = null;
+			let error = null;
 
-				response.on('end', () => {
+			if (response.statusCode >= 200 && response.statusCode < 299) {
 
-					var info = stringutilities.parseJSONString(response_body);
+				if (_.has(info, 'items') && _.isArray(info.items)) {
 
-					var failedItems;
-					var success;
-
-					if (response.statusCode >= 200 && response.statusCode < 299) {
-
-						failedItems = info.items.filter(function(x) {
-							return x.index.status >= 300;
+					if (arrayutilities.nonEmpty(info.items)) {
+						failed_items = arrayutilities.filter(info.items, (item) => {
+							return item.index.status >= 300;
 						});
-
-						success = {
-							"attemptedItems": info.items.length,
-							"successfulItems": info.items.length - failedItems.length,
-							"failedItems": failedItems.length
-						};
-
 					}
 
-					var error = response.statusCode !== 200 || info.errors === true ? {
-						"statusCode": response.statusCode,
-						"responseBody": response_body
-					} : null;
+					success = {
+						attemptedItems: info.items.length,
+						successfulItems: (info.items.length - failed_items.length),
+						failedItems: failed_items.length
+					};
 
-					return resolve({
-						error: error,
-						success: success,
-						statusCode: response.statusCode,
-						failedItems: failedItems
-					});
+				}
 
-				});
+			}
 
-			}).on('error', (e) => {
-				return reject(e);
-			});
+			if (response.statusCode !== 200 || (_.has(info, 'errors') && info.errors === true)) {
+				error = {
+					statusCode: response.statusCode,
+					responseBody: response.body
+				}
+			}
 
-			request.end(request_parameters.body);
+			return {
+				error: error,
+				success: success,
+				statusCode: response.statusCode,
+				failedItems: failed_items
+			};
 
 		});
 
@@ -103,7 +121,7 @@ module.exports = class AWSSignedRequestProvider {
 				'Content-Type': 'application/json',
 				'Host': endpoint,
 				'Content-Length': Buffer.byteLength(body),
-				'X-Amz-Security-Token': process.env.AWS_SESSION_TOKEN,
+				'X-Amz-Security-Token': global.SixCRM.configuration.getEnvironmentConfig('AWS_SESSION_TOKEN'),
 				'X-Amz-Date': datetime
 			}
 		};
@@ -142,7 +160,7 @@ module.exports = class AWSSignedRequestProvider {
 		].join('\n');
 
 		request.headers.Authorization = [
-			'AWS4-HMAC-SHA256 Credential=' + process.env.AWS_ACCESS_KEY_ID + '/' + credentialString,
+			'AWS4-HMAC-SHA256 Credential=' + global.SixCRM.configuration.getEnvironmentConfig('AWS_ACCESS_KEY_ID') + '/' + credentialString,
 			'SignedHeaders=' + signedHeaders,
 			'Signature=' + hashutilities.toHMAC(kSigning, stringToSign, 'hex')
 		].join(', ');
---
+++ test/unit/controllers/providers/aws-signedrequest-provider.spec.js
@@ -0,0 +1,204 @@
+const chai = require('chai');
+const mockery = require('mockery');
+const expect = chai.expect;
+const objectutilities = global.SixCRM.routes.include('lib','object-utilities.js');
+const du = global.SixCRM.routes.include('lib','debug-utilities.js');
+
+describe('controllers/providers/aws-signedrequest-provider', () => {
+
+	before(() => {
+		mockery.enable({
+			useCleanCache: true,
+			warnOnReplace: false,
+			warnOnUnregistered: false
+		});
+	});
+
+	afterEach(() => {
+		mockery.resetCache();
+		mockery.deregisterAll();
+	});
+
+	describe('constructor', () => {
+
+		it('successfully constructs', () => {
+
+			const AWSSignedRequestProvider = global.SixCRM.routes.include('providers', 'aws-signedrequest-provider.js');
+			let awssignedrequestprovider = new AWSSignedRequestProvider();
+
+			expect(objectutilities.getClassName(awssignedrequestprovider)).to.equal('AWSSignedRequestProvider');
+
+		});
+	});
+
+	describe('validateEnvironment', () => {
+
+		let process_env;
+		beforeEach(() => {
+			process_env = process.env;
+		});
+
+		afterEach(() => {
+			process.env = process_env;
+		})
+
+		it('succeeds', () => {
+
+			process.env.AWS_SECRET_ACCESS_KEY = 'abc123';
+			process.env.AWS_SESSION_TOKEN = 'blahblah';
+
+			const AWSSignedRequestProvider = global.SixCRM.routes.include('providers', 'aws-signedrequest-provider.js');
+			let awssignedrequestprovider = new AWSSignedRequestProvider();
+
+			let result;
+
+			try{
+				result = awssignedrequestprovider.validateEnvironment();
+			}catch(e){
+				expect(e).to.not.be.defined;
+			}
+
+			expect(result).to.equal(true);
+
+		});
+
+		it('throws error when missing AWS_SECRET_ACCESS_KEY', () => {
+
+			delete process.env.AWS_SECRET_ACCESS_KEY;
+			process.env.AWS_SESSION_TOKEN = 'blahblah';
+
+			const AWSSignedRequestProvider = global.SixCRM.routes.include('providers', 'aws-signedrequest-provider.js');
+			let awssignedrequestprovider = new AWSSignedRequestProvider();
+
+			let result;
+
+			try{
+				result = awssignedrequestprovider.validateEnvironment();
+				expect(false).to.equal(true);
+			}catch(e){
+				expect(e.message).to.equal('[500] Missing "AWS_SECRET_ACCESS_KEY" in process.env');
+			}
+
+		});
+
+		it('throws error when missing AWS_SESSION_TOKEN', () => {
+
+			delete process.env.AWS_SESSION_TOKEN;
+			process.env.AWS_SECRET_ACCESS_KEY = 'abc123';
+
+			const AWSSignedRequestProvider = global.SixCRM.routes.include('providers', 'aws-signedrequest-provider.js');
+			let awssignedrequestprovider = new AWSSignedRequestProvider();
+
+			let result;
+
+			try{
+				result = awssignedrequestprovider.validateEnvironment();
+				expect(false).to.equal(true);
+			}catch(e){
+				expect(e.message).to.equal('[500] Missing "AWS_SESSION_TOKEN" in process.env');
+			}
+
+		});
+
+	});
+
+	describe('buildSignedRequest', () => {
+
+		let process_env;
+		beforeEach(() => {
+			process_env = process.env;
+		});
+
+		afterEach(() => {
+			process.env = process_env;
+		})
+
+		it('successfully builds a signed request', () => {
+
+			process.env.AWS_SECRET_ACCESS_KEY = 'abc123';
+			process.env.AWS_SESSION_TOKEN = 'blahblah';
+
+			let body = 'some body';
+			let endpoint = 'search-sixcrm-logs-btbeleon7xa576ayj4bx2c54le.us-east-1.es.amazonaws.com';
+
+			const AWSSignedRequestProvider = global.SixCRM.routes.include('providers', 'aws-signedrequest-provider.js');
+			let awssignedrequestprovider = new AWSSignedRequestProvider();
+
+			let signed_request = awssignedrequestprovider.buildSignedRequest(endpoint, body);
+			expect(signed_request).to.have.property('host');
+			expect(signed_request).to.have.property('method');
+			expect(signed_request).to.have.property('path');
+			expect(signed_request).to.have.property('body');
+			expect(signed_request).to.have.property('headers');
+			expect(signed_request.headers).to.have.property('Content-Type');
+			expect(signed_request.headers).to.have.property('Host');
+			expect(signed_request.headers).to.have.property('Content-Length');
+			expect(signed_request.headers).to.have.property('X-Amz-Security-Token');
+			expect(signed_request.headers).to.have.property('X-Amz-Date');
+			expect(signed_request.headers).to.have.property('Authorization');
+			expect(signed_request.headers['Content-Type']).to.equal('application/json');
+			expect(signed_request.headers['Host']).to.equal(endpoint);
+			expect(signed_request.headers['Content-Length']).to.equal(9);
+			expect(signed_request.headers['Content-Length']).to.equal(9);
+			expect(signed_request.headers['X-Amz-Security-Token']).to.equal(process.env.AWS_SESSION_TOKEN);
+			expect(signed_request.headers['Authorization']).to.have.string('AWS4-HMAC-SHA256 Credential=');
+			expect(signed_request.headers['Authorization']).to.have.string('SignedHeaders=');
+			expect(signed_request.headers['Authorization']).to.have.string('Signature=');
+		});
+	});
+
+	describe('signedRequest', () => {
+
+		let process_env;
+		beforeEach(() => {
+			process_env = process.env;
+		});
+
+		afterEach(() => {
+			process.env = process_env;
+		})
+
+		it.only('successfully performs a signed request', () => {
+
+			process.env.AWS_SECRET_ACCESS_KEY = 'abc123';
+			process.env.AWS_SESSION_TOKEN = 'blahblah';
+
+			let body = 'some body';
+			let endpoint = 'search-sixcrm-logs-btbeleon7xa576ayj4bx2c54le.us-east-1.es.amazonaws.com';
+
+			mockery.registerMock(global.SixCRM.routes.path('providers', 'http-provider.js'), class {
+				constructor(){}
+				post(parameters){
+					expect(parameters).to.have.property('endpoint');
+					expect(parameters).to.have.property('body');
+					expect(parameters).to.have.property('headers');
+					return Promise.resolve({
+						body:JSON.stringify({items: []}),
+						statusCode:200
+					});
+				}
+			});
+
+			const AWSSignedRequestProvider = global.SixCRM.routes.include('providers', 'aws-signedrequest-provider.js');
+			let awssignedrequestprovider = new AWSSignedRequestProvider();
+
+			return awssignedrequestprovider.signedRequest(endpoint, body).then(result => {
+				du.info(result);
+			});
+
+		});
+
+	});
+	/*
+	signedRequest(endpoint, body) {
+
+		du.debug('Signed Request');
+
+		return Promise.resolve()
+			.then(() => this.validateEnvironment())
+			.then(() => this.buildSignedRequest(endpoint, body))
+			.then((request_parameters) => this.executeRequest(request_parameters));
+
+	}
+	*/
+});
