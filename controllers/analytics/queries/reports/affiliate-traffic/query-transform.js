b3a947bfe69588a4a5aa6b9c6bc0eb535fff928c
jadurham909 <justin@adexchangegrp.com>
2018-04-23 12:15:59 -0400

    Add subId filter
    Former-commit-id: 5063a61c31650d72980ed45b491b265820c88ce3

--- controllers/analytics/queries/reports/affiliate-traffic/query-transform.js
+++ controllers/analytics/queries/reports/affiliate-traffic/query-transform.js
@@ -13,11 +13,12 @@ module.exports = async (parameters = {}) => {
 	filter = _resolveFilterQuery(filter, 'account', 'account', parameters);
 	filter = _resolveFilterQuery(filter, 'campaign', 'campaign', parameters);
 	filter = _resolveFilterQuery(filter, 'affiliate', 'affiliate', parameters);
+	filter = _resolveFilterQuerySubId(filter, parameters);
 
 	// filter = _resolveFilterQuery(filter, 'mid', 'merchant_provider', parameters);
 	// filter = _resolveFilterQuery(filter, 'product', parameters);
 	// filter = _resolveFilterQuery(filter, 'productSchedule', parameters);
-	// filter = _resolveFilterQuery(filter, 'subId', parameters);
+
 
 	const queryParameters = [];
 
@@ -28,6 +29,7 @@ module.exports = async (parameters = {}) => {
 		_resolveFilterValue(local, 'account', parameters);
 		_resolveFilterValue(local, 'campaign', parameters);
 		_resolveFilterValue(local, 'affiliate', parameters);
+		_resolveFilterValueSubId(local, parameters);
 
 		queryParameters.push(format.withArray(filter, local))
 
@@ -55,12 +57,45 @@ function _resolveFilterQuery(filter, identifier, map, parameters) {
 
 }
 
+function _resolveFilterQuerySubId(filter, parameters) {
+
+	if (parameters['subId']) {
+
+		return filter += ` AND (%s.subaffiliate_1 = %L OR  %s.subaffiliate_2 = %L OR %s.subaffiliate_3 = %L OR %s.subaffiliate_4 = %L OR %s.subaffiliate_5 = %L) `;
+
+	} else {
+
+		return filter;
+
+	}
+
+}
+
 function _resolveFilterValue(local, identifier, parameters) {
 
 	if (parameters[identifier]) {
 
 		local.push('s');
 		local.push(parameters[identifier]);
+
+	}
+
+}
+
+function _resolveFilterValueSubId(local, parameters) {
+
+	if (parameters['subId']) {
+
+		local.push('s');
+		local.push(parameters['subId']);
+		local.push('s');
+		local.push(parameters['subId']);
+		local.push('s');
+		local.push(parameters['subId']);
+		local.push('s');
+		local.push(parameters['subId']);
+		local.push('s');
+		local.push(parameters['subId']);
 
 	}
 
