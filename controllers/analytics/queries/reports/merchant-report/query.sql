21208814593ff3c521cce1b96e8ed9d5190b29cb
jadurham909 <justin@adexchangegrp.com>
2018-04-29 16:37:35 -0400

    Updated merchant report query
    Former-commit-id: 129266ebca4ad8af0fccabd63cd703ff8f6d4df4

--- controllers/analytics/queries/reports/merchant-report/query.sql
+++ controllers/analytics/queries/reports/merchant-report/query.sql
@@ -13,18 +13,18 @@ SELECT
 	COALESCE(CAST(COALESCE(gross_orders.attempts, 0) AS DOUBLE PRECISION) / CAST(sales.sales AS DOUBLE PRECISION), 0) AS sales_percentage,
 	COALESCE(sales.renveue, 0) AS sales_revenue,
 	COALESCE(declines.declines, 0) AS declines,
-	COALESCE(CAST(COALESCE(declines.declines, 0) AS DOUBLE PRECISION) / CAST(gross_orders.attempts AS DOUBLE PRECISION), 0) AS declines_percentage,
+	COALESCE(CAST(COALESCE(declines.declines, 0) AS DOUBLE PRECISION) / CAST(gross_orders.attempts AS DOUBLE PRECISION), 0) AS decline_percentage,
 	COALESCE(chargebacks.chargebacks, 0) AS chargebacks,
 	COALESCE(chargebacks.chargeback_expense, 0) AS chargeback_expense,
 	COALESCE(CAST(COALESCE(chargebacks.chargebacks, 0) AS DOUBLE PRECISION) / CAST(sales.sales AS DOUBLE PRECISION), 0) AS chargeback_percentage,
-	0 AS refunds,
-	0 AS refund_expenses,
-	0 AS refund_percentage,
-	0 AS partial_refunds,
-	0 AS partial_refund_expenses,
-	0 AS partial_refund_percentage,
-	0 AS total_refund_expenses,
-	0 AS adjusted_sales_revenue
+	COALESCE(full_refunds.refunds, 0) AS full_refunds,
+	COALESCE(full_refunds.refunds_expense, 0) AS full_refund_expense,
+	COALESCE(CAST(COALESCE(full_refunds.refunds, 0) AS DOUBLE PRECISION) / CAST(sales.sales AS DOUBLE PRECISION), 0) AS full_refund_percentage,
+	COALESCE(partial_refunds.refunds, 0) AS partial_refunds,
+	COALESCE(partial_refunds.refunds_expense, 0) AS partial_refund_expense,
+	COALESCE(CAST(COALESCE(partial_refunds.refunds, 0) AS DOUBLE PRECISION) / CAST(sales.sales AS DOUBLE PRECISION), 0) AS partial_refund_percentage,
+	COALESCE(full_refunds.refunds_expense, 0) + COALESCE(partial_refunds.refunds_expense, 0) AS total_refund_expenses,
+	COALESCE(sales.renveue, 0) - (COALESCE(full_refunds.refunds_expense, 0) + COALESCE(partial_refunds.refunds_expense, 0)) AS adjusted_sales_revenue
 
 FROM
 
@@ -41,6 +41,7 @@ FROM
 	LEFT OUTER JOIN
 
 	(
+		-- sessions with transactions
 		SELECT
 			t.merchant_provider,
 			COUNT(DISTINCT s.id) as attempts
@@ -55,6 +56,7 @@ FROM
 	LEFT OUTER JOIN
 
 	(
+		-- sessions with a successful transactions that is not an upsell
 		SELECT
 			t.merchant_provider,
 			COUNT(DISTINCT s.id) as sales,
@@ -70,6 +72,7 @@ FROM
 	LEFT OUTER JOIN
 
 	(
+		-- sessions with no successful transactions and at least one failure
 		SELECT
 			sub_failure.merchant_provider,
 			COUNT(s.id) as declines
@@ -107,7 +110,7 @@ FROM
 	(
 		SELECT
 			t.merchant_provider,
-			COUNT(1) AS chargebacks,
+			COUNT(DISTINCT t.session) AS chargebacks,
 			SUM(t.amount) AS chargeback_expense
 		FROM analytics.f_transaction t
 		INNER JOIN analytics.f_transaction_chargeback ftc ON ftc.transaction_id = t.id
@@ -117,4 +120,32 @@ FROM
 
 	ON gateways.merchant_provider = chargebacks.merchant_provider
 
+	LEFT OUTER JOIN
 
+	(
+		SELECT
+			t.merchant_provider,
+			COUNT(DISTINCT t.session) AS refunds,
+			SUM(t.amount) AS refunds_expense
+		FROM analytics.f_transaction t
+		INNER JOIN analytics.f_transaction associated ON associated.id = t.associated_transaction
+		WHERE t.datetime BETWEEN '2017-01-01 00:00:00' AND '2019-01-01 00:00:00' AND t.processor_result = 'success' AND t.transaction_type = 'refund' AND associated.amount <= t.amount
+		GROUP BY t.merchant_provider
+	) full_refunds
+
+	ON gateways.merchant_provider = full_refunds.merchant_provider
+
+	LEFT OUTER JOIN
+
+	(
+		SELECT
+			t.merchant_provider,
+			COUNT(DISTINCT t.session) AS refunds,
+			SUM(t.amount) AS refunds_expense
+		FROM analytics.f_transaction t
+		INNER JOIN analytics.f_transaction associated ON associated.id = t.associated_transaction
+		WHERE t.datetime BETWEEN '2017-01-01 00:00:00' AND '2019-01-01 00:00:00' AND t.processor_result = 'success' AND t.transaction_type = 'refund' AND associated.amount > t.amount
+		GROUP BY t.merchant_provider
+	) partial_refunds
+
+	ON gateways.merchant_provider = partial_refunds.merchant_provider
