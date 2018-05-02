72b4fa6f09fc15a5d64e82436b4a62fb868feccd
jadurham909 <justin@adexchangegrp.com>
2018-05-02 11:49:31 -0400

    Cleanup
    Former-commit-id: 6fc77b2095c1985037199aa1513bfc56d28d0d09

--- controllers/providers/Parameters.js
+++ controllers/providers/Parameters.js
@@ -3,9 +3,7 @@ const _ = require('lodash');
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
 const eu = global.SixCRM.routes.include('lib', 'error-utilities.js');
 const mvu = global.SixCRM.routes.include('lib', 'model-validator-utilities.js');
-const arrayutilities = global.SixCRM.routes.include('lib', 'array-utilities.js');
 const objectutilities = global.SixCRM.routes.include('lib', 'object-utilities.js');
-const stringutilities = global.SixCRM.routes.include('lib', 'string-utilities.js');
 
 module.exports = class Parameters {
 
