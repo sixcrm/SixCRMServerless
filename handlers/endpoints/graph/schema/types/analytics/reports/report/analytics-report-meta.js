d935e5b8278bfbdcc4b3c74f58fae37db9f2337a
jadurham909 <justin@adexchangegrp.com>
2018-05-02 14:22:44 -0400

    Fix meta
    Former-commit-id: db246489fde1a7ae69705e51ba673344c773864a

--- handlers/endpoints/graph/schema/types/analytics/reports/report/analytics-report-meta.js
+++ handlers/endpoints/graph/schema/types/analytics/reports/report/analytics-report-meta.js
@@ -1,14 +1,13 @@
-const GraphQLList = require('graphql').GraphQLList;
+const GraphQLInt = require('graphql').GraphQLInt;
 const GraphQLObjectType = require('graphql').GraphQLObjectType;
-const AnalyticsReportDictionaryEntry = require('../common/analytics-report-dictionary-entry');
 
 module.exports = new GraphQLObjectType({
 	name: 'AnalyticsReportMeta',
 	description: 'Meta data for response',
 	fields: () => ({
 		count: {
-			type: new GraphQLList(new GraphQLList(AnalyticsReportDictionaryEntry)),
-			description: 'Records'
+			type: GraphQLInt,
+			description: 'Count'
 		}
 	}),
 	interfaces: []
