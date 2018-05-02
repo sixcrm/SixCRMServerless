6bc04de7927df6209c78d21b737e3e09411599c1
jadurham909 <justin@adexchangegrp.com>
2018-05-02 16:22:49 -0400

    Refactor facets
    Former-commit-id: f918c6d86afa6676630ecd2a67eba48b99bae193

---
+++ controllers/analytics/queries/reports/facets/products/params.js
@@ -0,0 +1,37 @@
+const eu = global.SixCRM.routes.include('lib', 'error-utilities.js');
+
+module.exports = async (parameters = {}) => {
+
+	const start = parameters.filter.filters.find(f => f.facet === 'start');
+	const end = parameters.filter.filters.find(f => f.facet === 'end');
+
+	if (start.length > 1) {
+
+		throw eu.getError('server', 'Start can only have one value');
+
+	}
+
+	if (end.length > 1) {
+
+		throw eu.getError('server', 'End can only have one value');
+
+	}
+
+	const params = {};
+
+	_resolveParamValue('start', start);
+	_resolveParamValue('end', end);
+
+	return params;
+
+	function _resolveParamValue(identifier, facet) {
+
+		if (facet) {
+
+			params[identifier] = facet.values;
+
+		}
+
+	}
+
+}
