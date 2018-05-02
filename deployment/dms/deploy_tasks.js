4422ddf9324388d911ebfacf9276b80119c70928
Nick Acker <acker2@me.com>
2018-05-01 17:26:10 -0700

    DMS Deployment
    Former-commit-id: 2c118589f503052834d0f87989da82570cdc4b2a

---
+++ deployment/dms/deploy_tasks.js
@@ -0,0 +1,12 @@
+require('../../SixCRM.js');
+
+const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+const DMSDeployment = global.SixCRM.routes.include('deployment', 'utilities/dms-deployment.js');
+const dmsDeployment = new DMSDeployment();
+
+dmsDeployment.deploy().then((result) => {
+	return du.highlight(result);
+}).catch(error => {
+	du.error(error);
+	du.warning(error.message);
+});
