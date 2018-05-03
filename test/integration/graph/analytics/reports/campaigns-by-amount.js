1215ec6631341dc57a2064c354ec431c4d43e6cf
jadurham909 <justin@adexchangegrp.com>
2018-05-03 16:57:34 -0400

    Add integration test for campaigns-by-amount
    Former-commit-id: 211ef6a99727dfe77f9f6181d2effe10ce8d1c2a

---
+++ test/integration/graph/analytics/reports/campaigns-by-amount.js
@@ -0,0 +1,61 @@
+const request = require('supertest');
+const chai = require('chai');
+const assert = require('chai').assert;
+const tu = global.SixCRM.routes.include('lib', 'test-utilities.js');
+
+chai.use(require('chai-json-schema'));
+
+let test_name = 'Get event funnel';
+
+let test_query = global.SixCRM.routes.include('handlers', 'endpoints/graph/queries/analytics/reports/campaigns-by-amount/campaigns-by-amount.json');
+
+//set the test user
+let test_user = {
+	name: "Super User",
+	email: "super.user@test.com",
+	role: "Owner"
+};
+
+//set the test account
+let account = {
+	name: 'Master Account',
+	id: '*'
+};
+
+let this_request = request(global.integration_test_config.endpoint);
+
+describe('Get ' + test_name + ' Test', function () {
+
+	let test_jwt = tu.createTestAuth0JWT(test_user.email, global.SixCRM.configuration.site_config.jwt.site.secret_key);
+
+	it('Should return return a 200 HTTP response code and a correctly formatted response', function (done) {
+
+		this_request.post('graph/' + account.id, {
+			timeout: 5000
+		})
+			.set('Authorization', test_jwt)
+			.send(test_query.body)
+			.expect(200)
+			.expect('Content-Type', 'application/json')
+			.expect('Access-Control-Allow-Origin', '*')
+			.expect('Access-Control-Allow-Methods', 'OPTIONS,POST')
+			.expect('Access-Control-Allow-Headers', 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token')
+			.end(function (err, response) {
+
+				if (err) {
+
+					// du.warning(err);
+
+				}
+
+				assert.isObject(response.body.response, JSON.stringify(response.body));
+
+				assert.isTrue(tu.validateGraphResponse(response.body.response.data.analytics, 'analytics/reports/analytics-reports'));
+
+				done();
+
+			});
+
+	});
+
+});
