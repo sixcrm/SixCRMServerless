c50b8b0630f6cdf25b01fe985e8e7f0bfc2c96cb
jadurham909 <justin@adexchangegrp.com>
2018-05-03 12:48:43 -0400

    Cleanup
    Former-commit-id: 777d9815720fe6129c11e832b4955c2d7a344077

--- controllers/analytics/Analytics.js
+++ controllers/analytics/Analytics.js
@@ -310,72 +310,20 @@ module.exports = class AnalyticsController extends AnalyticsUtilities {
 				const resolveParams = require('./queries/home/hero-chart-timeseries/average-revenue-per-order/params');
 				return this.query('home/hero-chart-timeseries/average-revenue-per-order', await resolveParams(parameters));
 			}
-			case 'affiliateTraffic':
-				return this.query('reports/affiliate-traffic', _resolveParams());
-			case 'merchantReport':
-				return this.query('reports/merchant-report', _resolveParams());
+			case 'affiliateTraffic': {
+				const resolveParams = require('./queries/reports/affiliate-traffic/params');
+				return this.query('reports/affiliate-traffic', await resolveParams(parameters));
+			}
+			case 'merchantReport': {
+				const resolveParams = require('./queries/reports/merchant-report/params');
+				return this.query('reports/merchant-report', await resolveParams(parameters));
+			}
 			case 'activities': {
 				const resolveParams = require('./queries/reports/activities/params');
 				return this.query('reports/activities', await resolveParams(parameters, parameters.pagination));
 			}
 			default:
 				throw new Error('Report not found');
-
-		}
-
-		function _resolveParams() {
-
-			const start = parameters.facets.find(f => f.facet === 'start');
-			const end = parameters.facets.find(f => f.facet === 'end');
-			const period = parameters.facets.find(f => f.facet === 'period');
-			const campaign = parameters.facets.find(f => f.facet === 'campaign');
-			const affiliate = parameters.facets.find(f => f.facet === 'affiliate');
-			const subId = parameters.facets.find(f => f.facet === 'subId');
-			const mid = parameters.facets.find(f => f.facet === 'mid');
-			const product = parameters.facets.find(f => f.facet === 'product');
-			const productSchedule = parameters.facets.find(f => f.facet === 'productSchedule');
-
-			if (start.length > 1) {
-
-				throw eu.getError('server', 'Start can only have one value');
-
-			}
-
-			if (end.length > 1) {
-
-				throw eu.getError('server', 'End can only have one value');
-
-			}
-
-			if (period && period.length > 1) {
-
-				throw eu.getError('server', 'Period can only have one value');
-
-			}
-
-			const params = {};
-
-			_resolveParamValue('start', start);
-			_resolveParamValue('end', end);
-			_resolveParamValue('period', period);
-			_resolveParamValue('campaign', campaign);
-			_resolveParamValue('affiliate', affiliate);
-			_resolveParamValue('subId', subId);
-			_resolveParamValue('mid', mid);
-			_resolveParamValue('product', product);
-			_resolveParamValue('productSchedule', productSchedule);
-
-			return params;
-
-			function _resolveParamValue(identifier, facet) {
-
-				if (facet) {
-
-					params[identifier] = facet.values;
-
-				}
-
-			}
 
 		}
 
