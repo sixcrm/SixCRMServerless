218ee316036ae7034c43f8a2418896181dd819d7
jadurham909 <justin@adexchangegrp.com>
2018-05-03 15:15:10 -0400

    Cleanup query
    Former-commit-id: 4ed44959d2a54945f60f31be4ed22aff233c933b

--- controllers/analytics/queries/reports/event-funnel/query.sql
+++ controllers/analytics/queries/reports/event-funnel/query.sql
@@ -1,39 +1,37 @@
 SELECT
-  COALESCE(sum(count_click), 0) count_click,
-  COALESCE(sum(count_lead), 0) count_lead,
-  COALESCE(sum(count_order), 0) count_order,
-  COALESCE(sum(count_confirm), 0) count_confirm,
-  COALESCE(sum(m_upsell), 0) count_upsell
+  COALESCE(SUM(count_click), 0) count_click,
+  COALESCE(SUM(count_lead), 0) count_lead,
+  COALESCE(SUM(count_order), 0) count_order,
+  COALESCE(SUM(count_confirm), 0) count_confirm,
+  COALESCE(SUM(m_upsell), 0) count_upsell
 FROM (
   SELECT
-    sum( CASE WHEN TYPE = 'click' THEN
+    SUM( CASE WHEN TYPE = 'click' THEN
         1
       ELSE
         0
-END) count_click,
-sum( CASE WHEN TYPE = 'lead' THEN
-    1
-  ELSE
-    0
-END) count_lead,
-sum( CASE WHEN TYPE = 'order' THEN
-    1
-  ELSE
-    0
-END) count_order,
-sum( CASE WHEN TYPE = 'confirm' THEN
-    1
-  ELSE
-    0
-END) count_confirm,
-max( CASE WHEN TYPE = 'upsell' THEN
-    1
-  ELSE
-    0
-END) m_upsell
+  END) count_click,
+  SUM( CASE WHEN TYPE = 'lead' THEN
+      1
+    ELSE
+      0
+  END) count_lead,
+  SUM( CASE WHEN TYPE = 'order' THEN
+      1
+    ELSE
+      0
+  END) count_order,
+  SUM( CASE WHEN TYPE = 'confirm' THEN
+      1
+    ELSE
+      0
+  END) count_confirm,
+  MAX( CASE WHEN TYPE = 'upsell' THEN
+      1
+    ELSE
+      0
+  END) m_upsell
 FROM
   analytics.f_event e
 WHERE %s
-GROUP BY
-  "session") fe;
-
+GROUP BY "session") fe;
