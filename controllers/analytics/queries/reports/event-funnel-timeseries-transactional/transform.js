3bc8a2eb35a681c18a377d3b0aef7638e349b0be
Justin Durham <justin.durham@icloud.com>
2018-05-09 14:50:55 -0400

    Fix timeseries facets for upsells and mains
    Former-commit-id: 6644e37865c1f755714a7672eeaef53a78f39b30

---
+++ controllers/analytics/queries/reports/event-funnel-timeseries-transactional/transform.js
@@ -0,0 +1,26 @@
+const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+
+module.exports = async (results) => {
+
+	du.debug('Event funnel timeseries transactional report');
+
+	return {
+		meta: {
+			count: results.length
+		},
+		records: results.map(r => {
+
+			return Object.keys(r).map(k => {
+
+				return {
+					key: k,
+					value: r[k]
+				}
+
+			})
+
+		})
+
+	};
+
+}
