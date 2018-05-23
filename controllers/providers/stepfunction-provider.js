3cecd202fbc2a99504a4a5ad2e38995b2cd1d924
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-05-22 22:50:11 -0700

    Working State Machine Deployment
    Former-commit-id: af2f73d95e1bc2f86f7097143d095e9834dcc3c2

---
+++ controllers/providers/stepfunction-provider.js
@@ -0,0 +1,108 @@
+const _ = require('lodash');
+const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
+const objectutilities = global.SixCRM.routes.include('lib', 'object-utilities.js');
+const AWSProvider = global.SixCRM.routes.include('controllers', 'providers/aws-provider.js');
+const parserutilities = global.SixCRM.routes.include('lib', 'parser-utilities.js');
+
+module.exports = class StepFunctionProvider extends AWSProvider{
+
+	constructor(){
+
+		super();
+
+		//Technical Debt:  Get this out of the constructor?
+		this.instantiateAWS();
+
+		this.stepfunction = new this.AWS.StepFunctions({apiVersion: '2016-11-23'});
+
+	}
+
+	async stepFunctionExists(name){
+
+		du.debug('Step Function Exists');
+
+		const parameters = {
+		  stateMachineArn: this.createStateMachineARN(name)
+		}
+
+		let description = null;
+
+		try{
+			description = await this.stepfunction.describeStateMachine(parameters).promise();
+		}catch(error){
+			if(error.statusCode != '400'){
+				throw error;
+			}
+			return null;
+		}
+
+		return description;
+
+	}
+
+	async createStateMachine(parameters){
+
+		du.debug('Create State Machine');
+
+		let params = objectutilities.transcribe(
+			{
+				name: 'name',
+				roleArn: 'roleArn',
+				definition: 'definition'
+			},
+			parameters,
+			{},
+			true
+		);
+
+		if(!_.isString(params.definition)){
+			params.definition = JSON.stringify(params.definition);
+		}
+
+		return this.stepfunction.createStateMachine(params).promise();
+
+	}
+
+	async updateStateMachine(parameters){
+
+		du.debug('Update State Machine');
+
+		let params = objectutilities.transcribe(
+			{
+				roleArn: 'roleArn',
+				definition: 'definition',
+				stateMachineArn: 'stateMachineArn'
+			},
+			parameters,
+			{},
+			true
+		);
+
+		if(!_.isString(params.definition)){
+			params.definition = JSON.stringify(params.definition);
+		}
+
+		let result = await this.stepfunction.updateStateMachine(params).promise();
+
+		return result;
+
+	}
+
+	createStateMachineARN(name){
+
+		du.debug('Create State Machine ARN');
+
+		const content = 'arn:aws:states:{{aws_region}}:{{aws_account_id}}:stateMachine:{{state_machine_name}}';
+
+		let data = {
+			aws_region: global.SixCRM.configuration.site_config.aws.region,
+			aws_account_id: global.SixCRM.configuration.site_config.aws.account,
+			state_machine_name: name
+		};
+
+		return parserutilities.parse(content, data);
+
+	}
+
+
+}
