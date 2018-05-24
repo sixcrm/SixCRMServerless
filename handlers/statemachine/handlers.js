9cd43982c496bbb45cba97c2e54c0b9b5160ba64
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-05-24 11:41:18 -0700

    Adding Archive Worker
    Former-commit-id: 8c91609e2745dc88ec605d4b62f993a67eab0dec

--- handlers/statemachine/handlers.js
+++ handlers/statemachine/handlers.js
@@ -1,9 +1,11 @@
 const StepFunctionHandler = require('./stepfunction-handler');
 
 const GetTrackingInformationController = global.SixCRM.routes.include('controllers', 'workers/statemachine/getTrackingInformation.js');
+const ArchiveController = global.SixCRM.routes.include('controllers', 'workers/statemachine/archive.js');
 
 module.exports = {
-	gettrackinginformation: handleStepFunction((event) => new GetTrackingInformationController().execute(event))
+	gettrackinginformation: handleStepFunction((event) => new GetTrackingInformationController().execute(event)),
+	archive: handleStepFunction((event) => new ArchiveController().execute(event))
 };
 
 function handleStepFunction(delegate) {
