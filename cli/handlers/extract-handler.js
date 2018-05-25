8276b46293d017486c05073406d856e4651fad37
Justin Durham <justin.durham@icloud.com>
2018-05-25 13:53:47 -0400

    Fetch active campaigns
    Former-commit-id: c5839c9ad1886b9785810a0590617ace840c7e20

--- cli/handlers/extract-handler.js
+++ cli/handlers/extract-handler.js
@@ -1,18 +1,21 @@
 const eu = require('../../lib/error-utilities');
+const fs = require('fs-extra');
 
 module.exports = class ExtractHandler {
 
-	constructor(crm, client, user, password) {
+	constructor(crm, client, user, password, artifactsDirectory) {
 
 		this._crm = crm;
 		this._client = client;
 		this._user = user;
 		this._password = password;
+		this._artifactsDirectory = artifactsDirectory;
 
 	}
 
 	async extract() {
 
+		await fs.ensureDir(this._artifactsDirectory);
 		return this._extract();
 
 	}
--- cli/handlers/limelight-extract-handler.js
+++ cli/handlers/limelight-extract-handler.js
@@ -1,17 +1,44 @@
 const du = require('../../lib/debug-utilities');
 const ExtractHandler = require('./extract-handler');
+const LimelightApi = require('@adexchange/aeg-limelight-api').Api;
+const fs = require('fs-extra');
+const path = require('path');
 
 module.exports = class LimelightExtractHandler extends ExtractHandler {
 
-	constructor(client, user, password) {
+	constructor(client, user, password, artifactsDirectory) {
 
-		super('limelight', client, user, password);
+		super('limelight', client, user, password, artifactsDirectory);
 
+		this._api = new LimelightApi(user, password, client)
+			.on('info', (data) => {
+
+				du.info(data);
+
+			})
+			.on('warn', (data) => {
+
+				du.warning(data);
+
+			})
+			.on('error', (data) => {
+
+				du.error('server', data);
+
+			});
 	}
 
 	async _extract() {
 
 		du.debug('LimelightExtractHandler#_extract()');
+		await this._extractCampaigns();
+
+	}
+
+	async _extractCampaigns() {
+
+		const campaigns = await this._api.findActiveCampaignsExpanded();
+		await fs.writeJson(path.join(this._artifactsDirectory, 'activeCampaigns.json'), campaigns);
 
 	}
 
