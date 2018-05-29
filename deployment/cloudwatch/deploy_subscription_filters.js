6b0a98b1f272bffd91b349a719bbace1c123305e
Entroper <entroper@gmail.com>
2018-05-29 15:30:18 -0400

    check if subscription exists, fix up async
    Former-commit-id: c53dd685e26f11140a0a77784f0332da5f2a2a57

--- deployment/cloudwatch/deploy_subscription_filters.js
+++ deployment/cloudwatch/deploy_subscription_filters.js
@@ -4,16 +4,15 @@ const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
 du.info("Environment", process.env);
 du.warning("Bypass this");
 
-// const CloudwatchDeployment = global.SixCRM.routes.include('deployment', 'utilities/cloudwatch-deployment.js');
+const CloudwatchDeployment = global.SixCRM.routes.include('deployment', 'utilities/cloudwatch-deployment.js');
 
-// let cloudwatch_deployment = new CloudwatchDeployment();
+let cloudwatch_deployment = new CloudwatchDeployment();
 
-// I don't know why the build is failing now since we're swallowing the errors again
-// cloudwatch_deployment.deployLoggerPermissions()
-// 	.then(() => cloudwatch_deployment.deploySubscriptionFilters())
-// 	.then((result) => {
-// 		return du.info(result);
-// 	}).catch(error => {
-// 		du.error(error);
-// 		//process.exit(-1);
-// 	});
+cloudwatch_deployment.deployLoggerPermissions()
+	.then(() => cloudwatch_deployment.deploySubscriptionFilters())
+	.then((result) => {
+		return du.info(result);
+	}).catch(error => {
+		du.error(error);
+		process.exit(-1);
+	});
