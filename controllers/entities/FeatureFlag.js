6071d44c5688e6758f659b2ecee960c3e6fbc606
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-05-31 12:39:03 -0700

    Resolving Feature Flag issue, adding Justin to seed.
    Former-commit-id: b8592c99c488882eef0f1f9842b05c76614bf30f

--- controllers/entities/FeatureFlag.js
+++ controllers/entities/FeatureFlag.js
@@ -1,3 +1,4 @@
+const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
 const entityController = global.SixCRM.routes.include('controllers', 'entities/Entity.js');
 
 module.exports = class FeatureFlagController extends entityController {
@@ -8,6 +9,19 @@ module.exports = class FeatureFlagController extends entityController {
 
 		this.primary_key = 'environment';
 		this.range_key = 'account';
+
+	}
+
+	async get(){
+
+		du.debug('FeatureFlag.get()');
+
+		//Technical Debt: Hack!
+		this.disableACLs();
+		let result = await super.get(arguments[0]);
+		this.enableACLs();
+
+		return result;
 
 	}
 
