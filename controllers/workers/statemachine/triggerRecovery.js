8a867206b954e9ad8b65a5bd455a0f77d7d9277f
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-06-06 10:05:49 -0700

    Updating Step Function Trigger reference
    Former-commit-id: 471efa27d31db74e735d79399ad9dea5c03f6571

--- controllers/workers/statemachine/triggerRecovery.js
+++ controllers/workers/statemachine/triggerRecovery.js
@@ -1,4 +1,4 @@
-const StepFunctionTriggerController = global.SixCRM.routes.include('controllers', 'workers/statemachine/components/StepFunctionTrigger.js');
+const StepFunctionTriggerController = global.SixCRM.routes.include('controllers', 'workers/statemachine/components/stepFunctionTrigger.js');
 
 module.exports = class TriggerRecoveryController extends StepFunctionTriggerController {
 
