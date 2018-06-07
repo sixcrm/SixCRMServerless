2dce48d50c47d7207c01737ce1517d9661f8793f
Timothy Dalbey <timothy.dalbey@crmblackbox.com>
2018-06-07 10:25:01 -0700

    Adding State Table
    Former-commit-id: f002456695e20ea670e19542c6de509b843be664

--- deployment/dynamodb/deploy_autoscaling.js
+++ deployment/dynamodb/deploy_autoscaling.js
@@ -1,11 +1,13 @@
 
 require('../../SixCRM.js');
-
+const _ = require('lodash');
 const du = global.SixCRM.routes.include('lib', 'debug-utilities.js');
 const DynamoDBAutoscalingDeployment = global.SixCRM.routes.include('deployment', 'utilities/dynamodb-autoscaling-deployment.js');
 const dynamoDBAutoscalingDeployment = new DynamoDBAutoscalingDeployment();
 
-dynamoDBAutoscalingDeployment.autoscaleTables().then((result) => {
+let table = (!_.isUndefined(process.argv[2]))?process.argv[2]:null;
+
+dynamoDBAutoscalingDeployment.autoscaleTables(table).then((result) => {
 	return du.info(result);
 }).catch(error => {
 	du.error(error);
