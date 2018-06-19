a7eaf474f71d5f7ee2de5a4ab5ca984fe49defe6
Justin Durham <justin.durham@icloud.com>
2018-06-19 10:33:07 -0400

    Write campaigns
    Former-commit-id: f3e0ebc80f8c400fa02628365610b198663e116b

--- cli/handlers/limelight/limelight-ingest-handler.js
+++ cli/handlers/limelight/limelight-ingest-handler.js
@@ -1,7 +1,22 @@
+require('@sixcrm/sixcrmcore');
 const du = require('@sixcrm/sixcrmcore/util/debug-utilities').default;
 const IngestHandler = require('../ingest-handler');
+const fs = require('fs-extra');
+const path = require('path');
+const BBPromise = require('bluebird');
+const uuid = require('uuid');
+const CampaignController = require('../../../controllers/entities/Campaign');
 
 module.exports = class LimelightIngestHandler extends IngestHandler {
+
+	constructor(crm, client, account, artifactsDirectory) {
+
+		super(crm, client, account, artifactsDirectory);
+
+		this._campaignController = new CampaignController();
+		this._campaignController.disableACLs();
+
+	}
 
 	async _ingest() {
 
@@ -21,6 +36,26 @@ module.exports = class LimelightIngestHandler extends IngestHandler {
 	async _ingestCampaigns() {
 
 		du.info('LimelightIngestHandler#_ingestCampaigns');
+
+		const campaigns = await fs.readJSON(path.join(this._artifactsDirectory, 'scraped-campaigns.json'));
+		await BBPromise.each(campaigns, async (llCampaign) => {
+
+			const campaign = await this._campaignController.create({
+				entity: {
+					id: uuid.v4(),
+					account: this._account,
+					name: llCampaign.name,
+					description: llCampaign.desc,
+					allow_on_order_form: true, // todo-tim: is this right?
+					show_prepaid: true, // todo-tim: is this right?
+					allow_prepaid: true, // todo-tim: is this right?
+					// emailtemplates
+				}
+			});
+
+			console.log(campaign);
+
+		});
 
 	}
 
