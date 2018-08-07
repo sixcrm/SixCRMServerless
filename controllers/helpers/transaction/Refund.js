79ed1a34de6786a79a6c549d706ae26e7f330d78
Ljubomir Simin <ljubomir@coingcs.com>
2018-08-07 17:16:50 +0200

    Remove unused method.

--- controllers/helpers/transaction/Refund.js
+++ controllers/helpers/transaction/Refund.js
@@ -1,9 +1,7 @@
 
 const _ = require('lodash');
-const querystring = require('querystring');
 
 const du = require('@6crm/sixcrmcore/util/debug-utilities').default;
-const objectutilities = require('@6crm/sixcrmcore/util/object-utilities').default;
 const TransactionUtilities = global.SixCRM.routes.include('helpers', 'transaction/TransactionUtilities.js');
 const TransactionController = global.SixCRM.routes.include('entities','Transaction.js');
 
@@ -59,8 +57,6 @@ module.exports = class Refund extends TransactionUtilities {
 				let instantiated_gateway = this.parameters.get('instantiated_gateway');
 				let processing_parameters = this.parameters.get('refund');
 
-				processing_parameters = this.ensureTransactionId(processing_parameters);
-
 				return instantiated_gateway.refund(processing_parameters);
 
 			})
@@ -73,34 +69,6 @@ module.exports = class Refund extends TransactionUtilities {
 				return refund_response;
 			});
 
-	}
-
-	/**
-     * It can happen that the transactionid is not stored in the processor response as expected. Some objects in our
-     * database keep responses like this:
-     *
-     * response:
-     * { statusCode: 200,
-     *   body: 'response=1&responsetext=SUCCESS&authcode=123456&transactionid=4010302956'
-     * }
-     *
-     * This method attempts to retrieve a transaction id from such response, and attach it to the top level response object.
-     */
-	ensureTransactionId(refund) {
-
-		du.debug('Ensure Transaction Id');
-
-		let result = refund.transaction.processor_response.result;
-
-		if (objectutilities.hasRecursive(result, 'response.body') && _.isString(result.response.body)) {
-			let parsed_response = querystring.parse(result.response.body);
-
-			if (_.has(parsed_response, 'transactionid')) {
-				result.transactionid = parsed_response.transactionid;
-			}
-		}
-
-		return refund;
 	}
 
 	createProcessingParameters(){
