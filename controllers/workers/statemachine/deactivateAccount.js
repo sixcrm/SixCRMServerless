661cb9aff8267a0b5cf705fb4283df46533e035e
Nick Acker <acker2@me.com>
2018-08-31 16:12:41 -0700

    Create deactivateaccount Lambda

---
+++ controllers/workers/statemachine/deactivateAccount.js
@@ -0,0 +1,38 @@
+const workerController = global.SixCRM.routes.include('controllers', 'workers/components/worker.js');
+const AccountController = global.SixCRM.routes.include('entities', 'Account');
+const SessionController = global.SixCRM.routes.include('entities', 'Session');
+const accountController = new AccountController();
+const sessionController = new SessionController();
+
+module.exports = class DeactivateAccountController extends workerController {
+	async execute(account_id) {
+		const account = await accountController.get({ id: account_id });
+		await Promise.all([
+			this.removeBilling(account),
+			this.cancelSessions(account)
+		]);
+	}
+
+	async removeBilling(account) {
+		delete account.billing;
+		await accountController.update({
+			entity: account,
+			allow_billing_overwrite: true
+		});
+	}
+
+	async cancelSessions(account) {
+		const {sessions} = await sessionController.listByAccount({
+			query_parameters: {
+				FilterExpression: 'not_exists(#cancelled) AND not_exists(#concluded)',
+				ExpressionAttributeNames: {
+					'#cancelled': 'cancelled',
+					'#concluded': 'concluded'
+				}
+			},
+			account
+		});
+
+		await Promise.all(sessions.map(entity => sessionController.cancelSession({entity})));
+	}
+}
