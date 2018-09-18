ff3adb7235a18bdfdaad4cdc4381af2372e2bb56
Ljubomir Simin <ljubomir@coingcs.com>
2018-09-18 18:08:37 +0200

    Tweaks to email fail notification.

--- controllers/helpers/notifications/notificationtypes/email_fail.js
+++ controllers/helpers/notifications/notificationtypes/email_fail.js
@@ -9,8 +9,8 @@ module.exports = class EmailFailNotification extends BaseNotification {
 		super();
 
 		this.name = 'email_fail';
-		this.context_required = ['smtp_provider.name', 'smtp_provider.id'];
-		this.category = 'transaction';
+		this.context_required = ['smtpprovider.name', 'smtpprovider.id'];
+		this.category = 'warning';
 
 		this.account_wide = true;
 
