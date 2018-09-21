41d21a72ff6a1b706d0c536b25d691a1abe1ae7d
Entroper <entroper@gmail.com>
2018-09-21 18:11:32 -0400

    tests with updates for orders and subscriptions

--- test/functional/analytics/events/tests/subscription/validate.js
+++ test/functional/analytics/events/tests/subscription/validate.js
@@ -2,7 +2,10 @@ const expect = require('chai').expect;
 
 module.exports = async (connection) => {
 
-	const result = await connection.query(`SELECT * FROM analytics.f_subscription WHERE session = '034087fc-73a1-4594-86de-8490c4db2025'`);
+	const result = await connection.query(`SELECT * FROM analytics.f_subscription WHERE status = 'active'`);
 	expect(result.rowCount).to.be.equal(2);
+
+	const inactiveResult = await connection.query(`SELECT * FROM analytics.f_subscription WHERE status = 'inactive'`);
+	expect(inactiveResult.rowCount).to.be.equal(1);
 
 }
