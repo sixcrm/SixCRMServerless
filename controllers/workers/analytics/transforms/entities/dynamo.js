8caf791719f42d05e35b22a0f99dde9415aa9201
Entroper <entroper@gmail.com>
2018-09-28 16:14:31 -0400

    start removing entity controller dependencies from analytics

---
+++ controllers/workers/analytics/transforms/entities/dynamo.js
@@ -0,0 +1,29 @@
+const du = require('@6crm/sixcrmcore/util/debug-utilities').default;
+const aws = require('aws-sdk');
+
+module.exports = class DynamoClient {
+
+	constructor() {
+
+		du.debug('DynamoClient Constructor');
+
+		const dynamoConfig = global.SixCRM.configuration.site_config.dynamodb;
+		let parameters = {
+			region: dynamoConfig && dynamoConfig.region || global.SixCRM.configuration.site_config.aws.region
+		};
+
+		if (dynamoConfig && dynamoConfig.endpoint) {
+			parameters.endpoint = dynamoConfig.endpoint;
+		}
+
+		this.client = new aws.DynamoDB(parameters);
+
+	}
+
+	get(table, key) {
+
+		return this.client.getItem({ TableName: table, Key: key }).promise;
+
+	}
+
+}
