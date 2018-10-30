2c11989702940bcdce1dd84dfd9d98f0784e9d08
Ljubomir Simin <ljubomir@coingcs.com>
2018-10-30 15:07:13 +0100

    Update tests.

--- test/unit/controllers/vendors/merchantproviders/NMI.spec.js
+++ test/unit/controllers/vendors/merchantproviders/NMI.spec.js
@@ -384,8 +384,8 @@ describe('vendors/merchantproviders/NMI.js', () => {
 				expect(result.getResult()).to.have.property('code');
 				expect(result.getResult()).to.have.property('message');
 				expect(result.getResult()).to.have.property('response');
-				expect(result.getResult().code).to.equal('softdecline');
-				expect(result.getResult().message).to.equal('Soft decline');
+				expect(result.getResult().code).to.equal('decline');
+				expect(result.getResult().message).to.equal('Declined');
 
 			});
 
