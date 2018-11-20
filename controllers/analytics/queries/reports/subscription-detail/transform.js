dcaab94e6d9c9dd8ae1c753c14d2acfbe780349f
Entroper <entroper@gmail.com>
2018-11-20 15:39:35 -0500

    fix date on subscriptions report to indicate next billing

--- controllers/analytics/queries/reports/subscription-detail/transform.js
+++ controllers/analytics/queries/reports/subscription-detail/transform.js
@@ -1,3 +1,4 @@
+const moment = require('moment-timezone');
 const du = require('@6crm/sixcrmcore/util/debug-utilities').default;
 
 module.exports = async (results) => {
@@ -9,6 +10,19 @@ module.exports = async (results) => {
 			count: results.length
 		},
 		records: results.map(r => {
+
+			const datetime = moment(r.datetime).utc();
+			if (r.interval === 'monthly') {
+
+				r.datetime = datetime.add(1, 'months').format();
+
+			}
+			else {
+
+				const days = r.interval.substring(0, r.interval.indexOf(' '));
+				r.datetime = datetime.add(days, 'days').format();
+
+			}
 
 			return Object.keys(r).map(k => {
 
