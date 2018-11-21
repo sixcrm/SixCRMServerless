b17a8a21fe3be6887565cd86b5527684d2472cf1
Entroper <entroper@gmail.com>
2018-11-21 14:12:19 -0500

    store next billing date in subscriptions report

--- controllers/analytics/queries/reports/subscription-detail/transform.js
+++ controllers/analytics/queries/reports/subscription-detail/transform.js
@@ -1,4 +1,3 @@
-const moment = require('moment-timezone');
 const du = require('@6crm/sixcrmcore/util/debug-utilities').default;
 
 module.exports = async (results) => {
@@ -10,19 +9,6 @@ module.exports = async (results) => {
 			count: results.length
 		},
 		records: results.map(r => {
-
-			const datetime = moment(r.datetime).utc();
-			if (r.interval === 'monthly') {
-
-				r.datetime = datetime.add(1, 'months').format();
-
-			}
-			else {
-
-				const days = r.interval.substring(0, r.interval.indexOf(' '));
-				r.datetime = datetime.add(days, 'days').format();
-
-			}
 
 			return Object.keys(r).map(k => {
 
