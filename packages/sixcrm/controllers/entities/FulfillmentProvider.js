4b71e51a4c684162884099b99852af611c2dc0eb
Ljubomir Simin <ljubomir@coingcs.com>
2019-01-23 16:48:24 +0100

    Move code that handles update of censored fields to EntityUtilities.

--- packages/sixcrm/controllers/entities/FulfillmentProvider.js
+++ packages/sixcrm/controllers/entities/FulfillmentProvider.js
@@ -4,8 +4,6 @@ const arrayutilities = require('@6crm/sixcrmcore/util/array-utilities').default;
 
 var entityController = global.SixCRM.routes.include('controllers', 'entities/Entity.js');
 
-const CENSORED_VALUE = '****';
-
 module.exports = class FulfillmentProviderController extends entityController {
 
 	constructor(){
@@ -43,15 +41,7 @@ module.exports = class FulfillmentProviderController extends entityController {
 	}
 
 	async update({entity}) {
-		const sanitization = this.sanitization;
-		this.sanitization = false;
-		const original = await this.get({id: entity.id, fatal: true});
-		this.sanitization = sanitization;
-		for (const path of this.encrypted_attribute_paths) {
-			if (_(entity).get(path) === CENSORED_VALUE) {
-				_.set(entity, path, _(original).get(path));
-			}
-		}
+		await this.handleCensoredValues(entity);
 
 		return super.update({entity, ignore_updated_at: true});
 	}
