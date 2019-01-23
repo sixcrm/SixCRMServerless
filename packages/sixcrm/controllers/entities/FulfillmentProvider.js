6471036218c166e5625f3574e2fc6e3dfadbad45
Ljubomir Simin <ljubomir@coingcs.com>
2019-01-23 16:18:35 +0100

    Fix update for censored values on FulfillmentProvider and SMTPProvider.

--- packages/sixcrm/controllers/entities/FulfillmentProvider.js
+++ packages/sixcrm/controllers/entities/FulfillmentProvider.js
@@ -4,6 +4,8 @@ const arrayutilities = require('@6crm/sixcrmcore/util/array-utilities').default;
 
 var entityController = global.SixCRM.routes.include('controllers', 'entities/Entity.js');
 
+const CENSORED_VALUE = '****';
+
 module.exports = class FulfillmentProviderController extends entityController {
 
 	constructor(){
@@ -39,6 +41,21 @@ module.exports = class FulfillmentProviderController extends entityController {
 		});
 
 	}
+
+	async update({entity}) {
+		const sanitization = this.sanitization;
+		this.sanitization = false;
+		const original = await this.get({id: entity.id, fatal: true});
+		this.sanitization = sanitization;
+		for (const path of this.encrypted_attribute_paths) {
+			if (_(entity).get(path) === CENSORED_VALUE) {
+				_.set(entity, path, _(original).get(path));
+			}
+		}
+
+		return super.update({entity, ignore_updated_at: true});
+	}
+
 
 }
 
