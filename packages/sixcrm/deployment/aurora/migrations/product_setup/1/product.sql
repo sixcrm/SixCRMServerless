8d5d2885d6eabbb8be79f91dd09a555857d3ff9f
Entroper <entroper@gmail.com>
2019-01-30 11:40:14 -0500

    product_setup schema

---
+++ packages/sixcrm/deployment/aurora/migrations/product_setup/1/product.sql
@@ -0,0 +1,19 @@
+CREATE TABLE product_setup.product (
+	id UUID NOT NULL PRIMARY KEY,
+	account_id UUID NOT NULL,
+	name VARCHAR(55) NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	price NUMERIC(19,2) NOT NULL CHECK (price >= 0),
+	is_shippable BOOLEAN NOT NULL,
+	shipping_price NUMERIC(19,2) CHECK (shipping_price IS NULL OR shipping_price >= 0),
+	shipping_delay INTERVAL,
+	fulfillment_provider_id UUID,
+	description TEXT,
+	sku VARCHAR(36),
+	image_urls TEXT[] NOT NULL,
+	merchant_provider_group_id UUID
+	CHECK (is_shippable = FALSE OR (shipping_price IS NOT NULL AND shipping_delay IS NOT NULL AND fulfillment_provider_id IS NOT NULL))
+);
+
+CREATE INDEX ix_product_account_id ON product_setup.product(account_id);
