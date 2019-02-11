f286a25a1a2a3e9e325ced72b613fc927dfc94ab
Ljubomir Simin <ljubomir@coingcs.com>
2019-02-11 16:26:50 +0100

    More debug info just in case.

--- packages/sixcrm/controllers/entities/SMSProvider.js
+++ packages/sixcrm/controllers/entities/SMSProvider.js
@@ -1,5 +1,6 @@
 require('module-alias/register');
 const eu = require('@6crm/sixcrmcore/lib/util/error-utilities').default;
+const du = require('@6crm/sixcrmcore/lib/util/debug-utilities').default;
 const entityController = global.SixCRM.routes.include('controllers', 'entities/Entity.js');
 const TwilioProvider = require('@lib/controllers/providers/twilio-provider.js').default;
 
@@ -14,14 +15,18 @@ module.exports = class SMTPProviderController extends entityController {
 	}
 
 	async validate({recipient_phone, smtpprovider_id}) {
+		du.debug('Validate SMS provider', recipient_phone, smtpprovider_id);
+
 		const configuration = await this.get({id: smtpprovider_id});
 
 		if(!configuration) {
+			du.error(`Can't find sms provider with ID ${smtpprovider_id}`);
 			throw eu.getError('notfound', 	`Can't find sms provider with ID ${smtpprovider_id}`);
 		}
 
 		const implementation = providerMapping[configuration.type];
 		if (!implementation) {
+			du.error(`No implementation for SMS provider type ${configuration.type}`);
 			throw eu.getError('notfound', `No implementation for SMS provider type ${configuration.type}`);
 		}
 
