7d9020277c81c24c1e189c64f1b4a9d9ae4e263a
Ljubomir Simin <ljubomir@coingcs.com>
2019-02-12 12:21:24 +0100

    Error handling in Twilio provider.

--- packages/sixcrm/controllers/providers/twilio-provider.ts
+++ packages/sixcrm/controllers/providers/twilio-provider.ts
@@ -1,7 +1,11 @@
 require('module-alias/register');
 
 const du = require('@6crm/sixcrmcore/lib/util/debug-utilities').default;
-const eu = require('@6crm/sixcrmcore/lib/util/error-utilities').default;
+
+interface SMSResponse {
+	message: any,
+	success: boolean
+}
 
 export default class TwilioProvider {
 
@@ -15,18 +19,41 @@ export default class TwilioProvider {
 		this.from_number = configuration.from_number
 	}
 
-	async sendSMS(text: string, phoneNumber: string) {
+	// https://www.twilio.com/docs/sms/send-messages
+	async sendSMS(text: string, phoneNumber: string): Promise<SMSResponse> {
 
 		du.info(`Sending SMS to ${phoneNumber}`);
 
 		const client = require('twilio')(this.api_account, this.api_token);
 
-		return client.messages
-			.create({
-				body: text,
-				from: this.from_number,
-				to: phoneNumber
-			})
+		try {
+			const twilioResponse = await client.messages
+				.create({
+					body: text,
+					from: this.from_number,
+					to: phoneNumber
+				});
+
+			if (!twilioResponse.error_code) {
+				return TwilioProvider.respond(twilioResponse);
+			}
+
+			du.error(`Twilio response error code ${twilioResponse.error_code}`);
+			du.error(`Twilio response error message ${twilioResponse.error_message}`);
+
+			return TwilioProvider.respond(twilioResponse.error_message, false);
+
+		} catch (error) {
+			return TwilioProvider.respond(error, false)
+		}
+
+	}
+
+	private static respond(message: any, success: boolean = true): SMSResponse {
+		const level = success ? 'debug' : 'info';
+		du[level](`Twilio response success: ${success}, message: '${message}'`);
+
+		return { message, success }
 	}
 
 
