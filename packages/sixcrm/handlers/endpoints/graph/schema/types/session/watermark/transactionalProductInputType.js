71b9f7f3907a1526ed30eb3441752080a407bb60
Evan Oxfeld <eoxfeld@gmail.com>
2019-02-15 11:44:34 -0500

    Revert "graph: Handle transactionalProduct deprecated fields"

--- packages/sixcrm/handlers/endpoints/graph/schema/types/session/watermark/transactionalProductInputType.js
+++ packages/sixcrm/handlers/endpoints/graph/schema/types/session/watermark/transactionalProductInputType.js
@@ -4,10 +4,8 @@ const {
 	GraphQLFloat,
 	GraphQLInt,
 	GraphQLBoolean,
-	GraphQLString,
-	GraphQLList
+	GraphQLString
 } = require('graphql');
-const productAttributesInputType = require('./../../product/components/attributesInputType');
 
 module.exports.graphObj = new GraphQLInputObjectType({
 	name: 'TransactionalProductInput',
@@ -31,55 +29,24 @@ module.exports.graphObj = new GraphQLInputObjectType({
 		},
 		ship: {
 			type: GraphQLBoolean,
-			deprecationReason: 'The `ship` field is deprecated and will be removed soon.'
+			description: 'The product ship, no-ship status.',
 		},
-		is_shippable:	{ type: GraphQLBoolean },
 		shipping_delay: {
 			type: GraphQLInt,
 			description: 'The number of seconds to delay shipping after a transaction.',
 		},
-		price: {
-			type: GraphQLFloat,
-		},
 		default_price: {
 			type: GraphQLFloat,
-			deprecationReason: 'The `default_price` field is deprecated and will be removed soon.',
+			description: 'A default price for product.',
 		},
-		shipping_price: { type: GraphQLFloat },
 		merchantprovidergroup:{
 			type: GraphQLString,
 			description: 'The merchant provider group associated with the product schedule.'
 		},
 		fulfillment_provider: {
 			type: GraphQLString,
-			deprecationReason: 'The `fulfillment_provider` field is deprecated and will be removed soon.',
-		},
-		fulfillment_provider_id: 	{ type: GraphQLString },
-		attributes: {
-			type: productAttributesInputType.graphObj,
-			deprecationReason: 'The `ProductAttributesInput` type is deprecated and will be removed soon.',
-		},
-		image_urls: { type: new GraphQLList(GraphQLString), defaultValue: [] },
+			description: 'The session associated with the transaction.'
+		}
 	}),
 	interfaces: []
-});
-
-module.exports.toTransactionalProductInput = ({
-	attributes: {
-		images =[]
-	} = {},
-	default_price,
-	fulfillment_provider,
-	fulfillment_provider_id,
-	image_urls,
-	is_shippable,
-	price,
-	ship,
-	...productInput
-}) => ({
-	...productInput,
-	fulfillment_provider_id: fulfillment_provider_id || fulfillment_provider,
-	is_shippable: is_shippable || ship,
-	price: price || default_price,
-	image_urls: image_urls.length ? image_urls : images.map(image => image.path)
 });
