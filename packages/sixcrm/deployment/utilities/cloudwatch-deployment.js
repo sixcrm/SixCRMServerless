0efc91bca0af675ae29a5c8a5b05a42d3f2566fe
Entroper <entroper@gmail.com>
2019-02-22 11:53:59 -0500

    revert to old logger for now

--- packages/sixcrm/deployment/utilities/cloudwatch-deployment.js
+++ packages/sixcrm/deployment/utilities/cloudwatch-deployment.js
@@ -17,7 +17,7 @@ module.exports = class CloudwatchDeployment extends AWSDeploymentUtilities{
 		this.cloudwatchprovider = new CloudwatchProvider();
 		this.lambdaprovider = new LambdaProvider();
 
-		this.logger_lambda_name = 'ElasticSearchStream';
+		this.logger_lambda_name = 'logger';
 
 	}
 
