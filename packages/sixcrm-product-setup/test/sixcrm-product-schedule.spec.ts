bb96561784977e929a27f575a63250196cb4b9fc
Evan Oxfeld <evan@adexchangegrp.com>
2019-02-28 13:00:34 -0500

    product-setup: Uncomment out ProductScheduleService tests

--- packages/sixcrm-product-setup/test/sixcrm-product-schedule.spec.ts
+++ packages/sixcrm-product-setup/test/sixcrm-product-schedule.spec.ts
@@ -1,51 +1,55 @@
-// 'use strict';
-//
-// import { v4 } from 'uuid';
-// import * as chai from 'chai';
-// import * as chaiAsPromised from 'chai-as-promised';
-// chai.use(chaiAsPromised);
-// const expect = chai.expect;
-//
-// import {createProductScheduleService} from '../src';
-// import {disconnect} from "../src/connect";
-// import ProductScheduleService from "../src/ProductScheduleService";
-//
-// let getValidProductSchedule = function(accountId) {
-// 	return {id: '123'};
-// };
-//
-// describe('@6crm/sixcrm-product-schedule', () => {
-// 	let productScheduleService: ProductScheduleService;
-// 	let accountId = v4();
-//
-// 	before(async () => {
-// 		productScheduleService = await createProductScheduleService({
-// 			accountId,
-// 			host: 'localhost',
-// 			username: 'postgres',
-// 			password: '',
-// 			schema: 'public',
-// 			logging: ['error']
-// 		});
-// 	});
-//
-// 	after(async () => {
-// 		await disconnect();
-// 	});
-//
-// 	describe('createProductSchedule', () => {
-// 		it('creates a product in the account', async () => {
-// 			// given
-// 			const aProductSchedule = getValidProductSchedule(accountId);
-// 			await productScheduleService.create(aProductSchedule);
-//
-// 			// when
-// 			const productFromDb = await productScheduleService.get(aProductSchedule.id);
-//
-// 			// then
-// 			expect(productFromDb.id).to.equal(aProductSchedule.id);
-// 		});
-//
-//
-// 	});
-// });
+'use strict';
+
+import { v4 } from 'uuid';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+import {createProductScheduleService} from '../src';
+import {disconnect} from "../src/connect";
+import ProductScheduleService from "../src/ProductScheduleService";
+
+let getValidProductSchedule = function(accountId) {
+	return {id: '123'};
+};
+
+describe('@6crm/sixcrm-product-schedule', () => {
+	let productScheduleService: ProductScheduleService;
+	let accountId = v4();
+
+	before(async () => {
+        console.error('Start create PS service');
+		productScheduleService = await createProductScheduleService({
+			accountId,
+			host: 'localhost',
+			username: 'postgres',
+			password: '',
+			schema: 'public',
+			logging: ['error']
+		});
+        console.error('End create PS service');
+	});
+
+	after(async () => {
+        console.error('Start disconect PS service');
+		await disconnect();
+        console.error('End disconect PS service');
+	});
+
+	describe('createProductSchedule', () => {
+		it('creates a product in the account', async () => {
+			// given
+			const aProductSchedule = getValidProductSchedule(accountId);
+			await productScheduleService.create(aProductSchedule);
+
+			// when
+			const productFromDb = await productScheduleService.get(aProductSchedule.id);
+
+			// then
+			expect(productFromDb.id).to.equal(aProductSchedule.id);
+		});
+
+
+	});
+});
