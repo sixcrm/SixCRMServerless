b8a7eeb8c551eee8aa1a4aea475a298e9b1dd5b0
Entroper <entroper@gmail.com>
2019-03-08 18:13:56 -0500

    remote analytics migrations (done in SixCRMAnalytics now)

--- packages/sixcrm/deployment/aurora/migrations/analytics/1/tables/d_processor_result.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS analytics.d_processor_result (
-    id VARCHAR(50) NOT NULL,
-    CONSTRAINT pk_d_processor_result PRIMARY KEY(id)
-);
--- packages/sixcrm/deployment/aurora/migrations/analytics/7/update-subscription-provider.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE analytics.f_subscription
-	ADD COLUMN product_schedule_name VARCHAR(64),
-	ADD COLUMN product_schedule VARCHAR(36),
-	ADD COLUMN merchant_provider_name VARCHAR(64),
-	ADD COLUMN merchant_provider VARCHAR(36);
