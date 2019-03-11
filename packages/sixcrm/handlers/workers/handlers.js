c4f245a13de1bc61342e6d9b4bd320e8859157cd
Entroper <entroper@gmail.com>
2019-03-11 16:29:08 -0400

    remove analytics handler

--- packages/sixcrm/handlers/workers/handlers.js
+++ packages/sixcrm/handlers/workers/handlers.js
@@ -1,4 +1,3 @@
-const AnalyticsHandler = require('./analytics-handler');
 const EventHandler = require('./event-handler');
 
 const SelectRebillsController = global.SixCRM.routes.include('controllers', 'workers/selectRebills.js');
@@ -6,15 +5,10 @@ const ReIndexingHelperController = global.SixCRM.routes.include('helpers', 'inde
 const IndexDynamoRecordsController = global.SixCRM.routes.include('controllers', 'workers/indexDynamoRecords.js');
 
 module.exports = {
-	analyticseventhandler: handleAnalytics(),
 	selectrebills: handleEvent((event) => new SelectRebillsController().execute(event)),
 	reindex: handleEvent(() => new ReIndexingHelperController().execute()),
 	indexdynamorecords: handleEvent((event) => new IndexDynamoRecordsController().execute(event))
 };
-
-function handleAnalytics() {
-	return (event, context, callback) => new AnalyticsHandler().handle(event, context, callback);
-}
 
 function handleEvent(delegate) {
 	return (event, context, callback) => new EventHandler().handle(event, context, callback, delegate);
