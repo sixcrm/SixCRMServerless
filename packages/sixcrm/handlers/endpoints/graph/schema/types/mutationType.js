2601d5fd96b198dc568d3f3d8fc73af0afa64201
Ljubomir Simin <ljubomir@coingcs.com>
2019-03-15 13:58:25 +0100

    Remove unused import.

--- packages/sixcrm/handlers/endpoints/graph/schema/types/mutationType.js
+++ packages/sixcrm/handlers/endpoints/graph/schema/types/mutationType.js
@@ -158,7 +158,6 @@ const UserACLController = global.SixCRM.routes.include('controllers', 'entities/
 const UserDeviceTokenController = global.SixCRM.routes.include('controllers', 'entities/UserDeviceToken');
 const UserSettingController = global.SixCRM.routes.include('controllers', 'entities/UserSetting');
 const UserSigningStringController = global.SixCRM.routes.include('controllers', 'entities/UserSigningString');
-const ProductScheduleController = global.SixCRM.routes.include('controllers', 'entities/ProductSchedule.js');
 const RebillController = global.SixCRM.routes.include('controllers', 'entities/Rebill.js');
 const ReturnController = global.SixCRM.routes.include('controllers', 'entities/Return.js');
 const RoleController = global.SixCRM.routes.include('controllers', 'entities/Role.js');
