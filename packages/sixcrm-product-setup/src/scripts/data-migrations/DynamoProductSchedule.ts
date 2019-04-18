cb58d5fd081ac6236e7645aa086b6234bb8acced
Ljubomir Simin <ljubomir@coingcs.com>
2019-04-18 17:21:23 +0200

    Watermark migration

--- packages/sixcrm-product-setup/src/scripts/data-migrations/DynamoProductSchedule.ts
+++ packages/sixcrm-product-setup/src/scripts/data-migrations/DynamoProductSchedule.ts
@@ -77,14 +77,7 @@ export class DynamoProductSchedule {
 	}
 
 	toProductSchedule() : ProductSchedule {
-		let cycles = _.sortBy(this.schedule, 'start').reduce(sortedScheduleReducer, []);
-		cycles = cycles.map(({ cycle_products, ...cycle}) => ({
-			...cycle,
-			cycle_products: cycle_products.map(({ product, ...cycleProduct }) => ({
-				...cycleProduct,
-				product: { id: product }
-			}))
-		}));
+		let cycles = DynamoProductSchedule.cyclesFromSchedule(this.schedule);
 
 		return Object.assign(new ProductSchedule(this.id, this.account, this.name.substring(0, 55).trim(), !!this.trial_required),{
 			created_at: new Date(this.created_at),
@@ -93,6 +86,18 @@ export class DynamoProductSchedule {
 			cycles
 
 		});
+	}
+
+	static cyclesFromSchedule(schedule) {
+		let cycles = _.sortBy(schedule, 'start').reduce(sortedScheduleReducer, []);
+		cycles = cycles.map(({cycle_products, ...cycle}) => ({
+			...cycle,
+			cycle_products: cycle_products.map(({product, ...cycleProduct}) => ({
+				...cycleProduct,
+				product: {id: product}
+			}))
+		}));
+		return cycles;
 	}
 
 	private get(object: any, path: string): any {
