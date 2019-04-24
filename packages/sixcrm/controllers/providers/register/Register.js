fb9dd8cca5caccadd983eac4eee6be7dbc9aa17e
Evan Oxfeld <evan@adexchangegrp.com>
2019-04-24 15:45:47 -0400

    Register: Fix subscription event cycle number

--- packages/sixcrm/controllers/providers/register/Register.js
+++ packages/sixcrm/controllers/providers/register/Register.js
@@ -199,7 +199,7 @@ module.exports = class Register extends RegisterUtilities {
 			status,
 			amount: cycle.price,
 			item_count: quantity,
-			cycle: rebill.cycle,
+			cycle: rebill.cycle + 1,
 			interval: cycle.length.months ? 'monthly' : `${cycle.length.days} days`,
 			account: session.account,
 			campaign: session.campaign,
